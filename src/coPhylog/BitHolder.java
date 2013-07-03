68e3bb52db49893bd2c8e9a1feff577ea18e608e
afodor <anthony.fodor@gmail.com>
2013-07-03 15:14:16 -0400

    Working on filling in the stub...

--- src/coPhylog/BitHolder.java
+++ src/coPhylog/BitHolder.java
@@ -6,16 +6,31 @@ public class BitHolder
 	// the rightmost 32 bits hold the right context
 	private long bits = 0x0000;
 	
-	private char c; // the character in the middle
+	private char middleChar; 
 	
 	// the index of the current string
 	private int index =0;
 	
 	private int numValidChars=0;
 	
-	private final int stopPoint;
+	private int stopPoint;
+	private final int contextSize;
 	
-	public BitHolder(String s, int contextSize)
+
+	private static final Long A_LONG = new Long(0x0000);
+	private static final Long C_LONG = new Long(0x0001);
+	private static final Long G_LONG = new Long(0x0002);
+	private static final Long T_LONG = new Long(0x0003);
+	
+	
+	private final Long A_LONG_SHIFT;
+	private final Long C_LONG_SHIFT;
+	private final Long G_LONG_SHIFT;
+	private final Long T_LONG_SHIFT;
+	
+	private final int shiftSize;
+	
+	public void setToString(String s)
 	{
 		int targetChars = contextSize*2 + 1;
 		
@@ -25,13 +40,29 @@ public class BitHolder
 		{
 			
 		}
+		
 	}
 	
-	private final Long A_LONG = new Long(0x0000);
-	private final Long C_LONG = new Long(0x0001);
-	private final Long G_LONG = new Long(0x0002);
-	private final Long T_LONG = new Long(0x0003);
+	public BitHolder(int contextSize) throws Exception
+	{
+		this.contextSize = contextSize;
+		shiftSize = 32 - contextSize*2;
+		
+		if(shiftSize < 0 )
+			throw new Exception("Maximum supported context size is 16");
+		
+		this.A_LONG_SHIFT = A_LONG << shiftSize;
+		this.C_LONG_SHIFT = C_LONG << shiftSize;
+		this.G_LONG_SHIFT = G_LONG << shiftSize;
+		this.T_LONG_SHIFT = T_LONG << shiftSize;
+		
+		
+	}
 	
+	private void add(char newChar)
+	{
+		
+	}
 	
 	private Long getMaskOrNull(char c)
 	{
