e444c895ea0963601b8fa0406232c0f41f17a3a2
afodor <anthony.fodor@gmail.com>
2013-07-15 14:33:49 -0400

    First step (not yet tested) towards pivot table

--- src/fileAbstractions/FileUtils.java
+++ src/fileAbstractions/FileUtils.java
@@ -17,9 +17,32 @@ import java.io.File;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.StringTokenizer;
 
 public class FileUtils
 {
+	public static String getCommonNameForPairs( PairedReads pr)
+		throws Exception
+	{
+		String s1 = getShortName(pr.getFirstFileName().getName());
+		String s2 = getShortName(pr.getSecondFileName().getName());
+	
+		if( ! s1.equals(s2))
+				throw new Exception("Unequal file names");
+			
+		return s1;
+	}
+	
+	private static String getShortName(String s)
+	{
+		if( s.startsWith("AS130-2"))
+			return "AS_130_2";
+		
+		StringTokenizer sToken = new StringTokenizer(s, "_");
+		
+		return sToken.nextToken() + "_" + sToken.nextToken();
+	}
+	
 	public static File getCountsFile(File inFile) throws Exception
 	{
 		return new File( inFile.getParentFile() + File.separator + "results" + File.separator + 
--- src/scripts/sequenceScripts/CreateSNPPivotTable.java
+++ src/scripts/sequenceScripts/CreateSNPPivotTable.java
@@ -14,12 +14,21 @@
 package scripts.sequenceScripts;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileReader;
+import java.io.FileWriter;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
 
+
+import coPhylog.CoPhylogBinaryFileReader;
+import coPhylog.ContextCount;
+
+import fileAbstractions.FileUtils;
 import fileAbstractions.PairedReads;
 
 import utils.ConfigReader;
@@ -29,12 +38,60 @@ import utils.ConfigReader;
  */
 public class CreateSNPPivotTable
 {
+	
 	public static void main(String[] args) throws Exception
 	{
 		HashSet<Long> ids = getSnpIds();
 		System.out.println(ids.size());
 		
+		HashMap<Long,HashMap<String,Holder>> map = getPivotMap(ids);
+		writeResults(map);
 		
+		
+	}
+	
+	private static void writeResults(HashMap<Long,HashMap<String,Holder>> map) throws Exception
+	{
+		BufferedWriter writer = new BufferedWriter(new FileWriter(new File(ConfigReader.getBurkholderiaDir()+
+				File.separator + "summary" + File.separator + "pivotedSNPS.txt")));
+		
+		HashSet<String> strains= new HashSet<>();
+		
+		for( Long aLong : map.keySet() )
+		{
+			HashMap<String, Holder> innerMap = map.get(aLong);
+			
+			for(String s : innerMap.keySet())
+				strains.add(s);
+		}
+		
+		List<String> strainList = new ArrayList<>();
+		
+		for(String s : strains)
+			strainList.add(s);
+		
+		Collections.sort(strainList);
+		
+		writer.write("key");
+		
+		for(String s : strainList)
+			writer.write("\t" + s);
+		
+		writer.write("\n");
+		
+		for( Long aLong : map.keySet() )
+		{
+			HashMap<String, Holder> innerMap = map.get(aLong);
+			
+			writer.write("" + aLong);
+			
+			for(String s: strainList)
+				writer.write("\t"+ innerMap.get(s));
+			
+			writer.write("\n");
+		}
+		
+		writer.flush();  writer.close();
 	}
 	
 	private static HashMap<Long,HashMap<String,Holder>> getPivotMap(HashSet<Long> ids)
@@ -46,11 +103,48 @@ public class CreateSNPPivotTable
 		
 		for(PairedReads pr : pairedList)
 		{
-			System.out.println(pr.getFirstFileName());
-			System.out.println(pr.getSecondFileName());
+			String strainName = FileUtils.getCommonNameForPairs(pr);
+			System.out.println(strainName + " "+ pr.getFirstFileName() + " " + pr.getSecondFileName());
+			addToMap(map, pr.getFirstFileName(), strainName);
+			addToMap(map, pr.getSecondFileName(), strainName);
 		}		
 		
 		return map;
+	}
+	
+	private static void addToMap(HashMap<Long,HashMap<String,Holder>> map, File sequenceFile, 
+			String strainName) throws Exception
+	{
+		HashMap<Long, ContextCount> fileMap = 
+				CoPhylogBinaryFileReader.readBinaryFile(FileUtils.getCountsFile(sequenceFile));
+		
+		for(Long aLong : fileMap.keySet())
+		{
+			ContextCount cc = fileMap.get(aLong);
+			
+			HashMap<String, Holder> innerMap = map.get(aLong);
+			
+			if( innerMap == null)
+			{
+				innerMap = new HashMap<String,Holder>();
+				
+				map.put( aLong, innerMap);
+			}
+			
+			Holder h = innerMap.get(strainName);
+			
+			if( h == null)
+			{
+				h = new Holder();
+				innerMap.put(strainName, h);
+			}
+			
+			h.a += cc.getNumA();
+			h.c += cc.getNumC();
+			h.g += cc.getNumG();
+			h.t+= cc.getNumT();
+		}
+		
 	}
 	
 	private static class Holder
