473f364b0fc48c1906c96f188efc000ff65a329c
afodor <anthony.fodor@gmail.com>
2013-11-18 20:27:51 -0500

    stubs for mOTU

---
+++ src/eTree/ENode.java
@@ -0,0 +1,54 @@
+/** 
+ * Author:  anthony.fodor@gmail.com
+ * 
+ * This code is free software; you can redistribute it and/or
+* modify it under the terms of the GNU General Public License
+* as published by the Free Software Foundation; either version 2
+* of the License, or (at your option) any later version,
+* provided that any use properly credits the author.
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+* GNU General Public License for more details at http://www.gnu.org * * */
+
+package eTree;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import probabilisticNW.ProbSequence;
+
+public class ENode
+{
+	private ProbSequence probSequence;
+	private ENode parent;
+	private float level;
+	private List<ENode> daughters =new ArrayList<ENode>();
+	
+	public ProbSequence getProbSequence()
+	{
+		return probSequence;
+	}
+
+	public ENode getParent()
+	{
+		return parent;
+	}
+
+	public float getLevel()
+	{
+		return level;
+	}
+
+	public List<ENode> getDaughters()
+	{
+		return daughters;
+	}
+
+	public ENode( String sequence, float level, ENode parent )
+	{
+		this.probSequence = new ProbSequence(sequence);
+		this.parent = parent;
+		this.level = level;
+	}
+}
---
+++ src/eTree/ETree.java
@@ -0,0 +1,35 @@
+/** 
+ * Author:  anthony.fodor@gmail.com
+ * 
+ * This code is free software; you can redistribute it and/or
+* modify it under the terms of the GNU General Public License
+* as published by the Free Software Foundation; either version 2
+* of the License, or (at your option) any later version,
+* provided that any use properly credits the author.
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+* GNU General Public License for more details at http://www.gnu.org * * */
+
+package eTree;
+
+public class ETree
+{
+	public static final float[] LEVELS = { 0.1f, 0.07f, 0.04f, 0.03f, 0.02f, 0.01f };
+	
+	private final ENode topNode;
+	
+	public ETree(String starterSequence)
+	{
+		this.topNode = new ENode(starterSequence, LEVELS[0], null);
+		ENode lastNode = topNode;
+		
+		for( int x=1; x < LEVELS.length; x++)
+		{
+			ENode nextNode = new ENode(starterSequence, LEVELS[x], lastNode);
+			lastNode = nextNode;
+		}
+	}
+	
+	
+}
