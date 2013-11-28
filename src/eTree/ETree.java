448e89a2cc1043c363e184e6dfb5f8bd7966b92c
afodor <anthony.fodor@gmail.com>
2013-11-28 13:30:40 -0500

    5 level tree...

--- src/eTree/ETree.java
+++ src/eTree/ETree.java
@@ -45,7 +45,7 @@ public class ETree implements Serializable
 	private static final long serialVersionUID = 8463272194826212918L;
 	public static final String ROOT_NAME = "root";
 	
-	public static final double[] LEVELS = {0.0, 0.20,0.19,0.18,0.17,0.16,0.15, 0.14, 0.13, 0.12, 0.11,0.10,0.09,0.08,0.07,0.06,0.05,0.04,0.03,0.02,0.01};
+	public static final double[] LEVELS = {0.0, 0.05,0.04,0.03,0.02,0.01};
 	private int node_number =1;
 	public static final int RDP_THRESHOLD = 80;
 	
