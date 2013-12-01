22ed930b03721f25df170e2958306ae2aa9d2d8b
afodor <anthony.fodor@gmail.com>
2013-11-30 21:55:21 -0500

    make the bat files

---
+++ src/bottomUpTree/MakeSHFiles.java
@@ -0,0 +1,56 @@
+/** 
+ * Author:  anthony.fodor@gmail.com    
+ * This code is free software; you can redistribute it and/or
+* modify it under the terms of the GNU General Public License
+* as published by the Free Software Foundation; either version 2
+* of the License, or (at your option) any later version,
+* provided that any use properly credits the author.
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+* GNU General Public License for more details at http://www.gnu.org * * */
+
+
+package bottomUpTree;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.util.ArrayList;
+import java.util.List;
+
+import utils.ConfigReader;
+import dereplicate.DereplicateBySample;
+
+public class MakeSHFiles
+{
+	public static void main(String[] args) throws Exception
+	{
+		File dir = new File(ConfigReader.getETreeTestDir() + File.separator + 
+				"gastro454DataSet" + File.separator );
+		
+		List<String> fileNames = new ArrayList<String>();
+		for( String s : dir.list())
+			fileNames.add(s);
+		
+		BufferedWriter mainBatFile = new BufferedWriter(new FileWriter(new File( 
+				ConfigReader.getETreeTestDir() + File.separator + 
+				"gastro454DataSet" + File.separator + "runAll.sh")));
+		
+		for(String s : fileNames)
+			if( s.startsWith(DereplicateBySample.DEREP_PREFIX))
+			{
+				BufferedWriter aSHWriter = new BufferedWriter(new FileWriter(new File( 
+						ConfigReader.getETreeTestDir() + File.separator + 
+						"gastro454DataSet" + File.separator )));
+				
+				aSHWriter.write("java -mx3000m /users/afodor/metagenomicsTools/bin/bottomUpTree/RunOne " + 
+				dir.getAbsolutePath() + File.separator + s + " " + s +"_CLUST.clust");
+				
+				aSHWriter.flush();  aSHWriter.close();
+			}
+		
+		mainBatFile.flush();  mainBatFile.close();
+	}
+}
