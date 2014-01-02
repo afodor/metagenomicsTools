b06274d7b553c0eb72c025ef7811176963d2272b
afodor <anthony.fodor@gmail.com>
2014-01-02 16:31:37 -0500

    towards RDP to JSON tree...

--- src/bottomUpTree/rdp/RdpToJson.java
+++ src/bottomUpTree/rdp/RdpToJson.java
@@ -16,14 +16,22 @@ package bottomUpTree.rdp;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.StringTokenizer;
+
+import parsers.NewRDPNode;
+import parsers.NewRDPParserFileLine;
+import parsers.OtuWrapper;
 
 import utils.ConfigReader;
 import utils.TabReader;
 
 public class RdpToJson
 {
+	public static final String ROOT="root";
+	
 	private static HashMap<String, Integer> getCaseControlMap() throws Exception
 	{
 		HashMap<String, Integer> map = new HashMap<String, Integer>();
@@ -83,37 +91,145 @@ public class RdpToJson
 	{
 		HashMap<String, Integer> caseControlMap = getCaseControlMap();
 		
-		File rdpDir = new File(ConfigReader.getNinaWithDuplicatesDir() + File.separator + "rdp" );
-		
-		String[] names = rdpDir.list();
-	
 		Holder root = new Holder();
 		
-		root.taxonomicLevel = "root";
+		root.taxonomicLevel = ROOT;
+		root.taxonomicName= ROOT;
 		
-		for( String s : names)
-		{
-			//System.out.println(s);
-			String key =getKeyFromFilename(s);
-			Integer caseControl = caseControlMap.get(key);
-			
-			if( caseControl == null)
-				throw new Exception("Could not find " + key);
-		}
+		List<NewRDPParserFileLine> allRDPLines = NewRDPParserFileLine.getRdpListSingleThread(ConfigReader.getNinaWithDuplicatesDir() + File.separator + 
+				"rdp" + File.separator + "allRdpOut.txt");
+		
+		for(int x=1; x < NewRDPParserFileLine.TAXA_ARRAY.length; x++)
+			addALevel(caseControlMap, x, root, allRDPLines);
 		
 		
 	}
 	
-	private static void addAFile( File file, Holder root, int caseControl )
+	private static String findParentTaxa( int rdpLevel, List<NewRDPParserFileLine> allRDPLines, String taxaName )
+		throws Exception
 	{
+		for( NewRDPParserFileLine fileLine : allRDPLines)
+		{
+			NewRDPNode aNode = fileLine.getTaxaMap().get(NewRDPParserFileLine.TAXA_ARRAY[rdpLevel]);
+			
+			if( aNode != null && aNode.getTaxaName().equals(taxaName))
+			{
+				//System.out.println(taxaName + " " + fileLine.getSummaryString());
+				NewRDPNode parentNode = fileLine.getTaxaMap().get(NewRDPParserFileLine.TAXA_ARRAY[rdpLevel-1]);
+				
+				if( parentNode != null)
+					return parentNode.getTaxaName();
+			}
+		}
 		
+		return null;
+	}
+	
+	private static void addALevel( HashMap<String, Integer> caseControlMap, 
+										int rdpDepth, 
+										Holder root,
+										List<NewRDPParserFileLine> allRDPLines)
+		throws Exception
+	{
+		System.out.println("Trying depth " + rdpDepth);
+		OtuWrapper wrapper = new OtuWrapper(ConfigReader.getNinaWithDuplicatesDir() + File.separator + 
+				"rdp" + File.separator + NewRDPParserFileLine.TAXA_ARRAY[rdpDepth]
+									+"_taxaAsColumns.txt");
+		List<List<Double>> logNormData = wrapper.getDataPointsNormalizedThenLogged();
+		List<List<Double>> unloggedData= wrapper.getDataPointsNormalizedThenLogged();
+		String parentLevel = NewRDPParserFileLine.TAXA_ARRAY[rdpDepth-1];
+		
+		if( rdpDepth -1 == 0 )
+			parentLevel = ROOT;
+		
+		HashMap<String, Holder> parentLevelMap = getMapForLevel(parentLevel, root);
+		
+		for(int x=0; x < wrapper.getOtuNames().size(); x++ )
+		{
+			String childTaxa = wrapper.getOtuNames().get(x);
+			String parentTaxa = ROOT;
+			
+			if( rdpDepth >= 2 )
+			{
+				parentTaxa = findParentTaxa(rdpDepth, allRDPLines, childTaxa);
+			}
+			
+			if( parentTaxa != null)
+			{
+				Holder parentHolder = parentLevelMap.get(parentTaxa);
+				
+				if( parentHolder != null)
+				{
+					if( parentHolder.children.containsKey(childTaxa))
+						throw new Exception("Duplicate child " + childTaxa);
+					
+					Holder childHolder = new Holder();
+					parentHolder.children.put(childTaxa, childHolder);
+					childHolder.taxonomicLevel = NewRDPParserFileLine.TAXA_ARRAY[rdpDepth];
+					childHolder.taxonomicName = childTaxa;
+					
+					for( int y=0; y < wrapper.getSampleNames().size(); y++)
+					{
+						Integer caseControlStatus = caseControlMap.get(wrapper.getSampleNames().get(y));
+						
+						if( caseControlStatus == null)
+							throw new Exception("Could not find case/control");
+						
+						if( caseControlStatus.equals(1))
+						{
+							childHolder.caseCounts+= unloggedData.get(y).get(x);
+							childHolder.caseSamplesLogNormalized.add(logNormData.get(y).get(x));
+						}
+						else if ( caseControlStatus.equals(0))
+						{
+							childHolder.controlCounts += unloggedData.get(y).get(x);
+							childHolder.controlSamplesLogNormalized.add(logNormData.get(y).get(x));
+						}
+						else throw new Exception("Parsing error");
+					}
+				}
+			}
+		}
+		
+	}
+	
+	private static void flatten( Holder aHolder, List<Holder> list )
+	{
+		list.add(aHolder);
+		
+		for( Holder child : aHolder.children.values() )
+			flatten(child, list);
+	}
+	
+	private static HashMap<String, Holder> getMapForLevel(String level, Holder root)
+		throws Exception
+	{
+		List<Holder> aList = new ArrayList<Holder>();
+		flatten(root,aList);
+		
+		HashMap<String, Holder> returnMap = new HashMap<String, Holder>();
+		
+		for(Holder h : aList)
+			if( h.taxonomicLevel.equals(level))
+			{
+				if( returnMap.containsKey(h.taxonomicName))
+					throw new Exception("Unexpected duplicate " + h.taxonomicName);
+				
+				returnMap.put(h.taxonomicName, h);
+			}
+				
+		
+		return returnMap;
 	}
 	
 	private static class Holder
 	{
+		String taxonomicName;
 		String taxonomicLevel;
-		int countsCase=0;
-		int countsControl=0;
+		double caseCounts=0;
+		double controlCounts=0;
+		List<Double> caseSamplesLogNormalized =new ArrayList<Double>();
+		List<Double> controlSamplesLogNormalized =new ArrayList<Double>();
 		double pValueFromTTest=1;
 		double pValueFromWilcoxon=1;
 		HashMap<String, Holder> children = new HashMap<String, Holder>();
