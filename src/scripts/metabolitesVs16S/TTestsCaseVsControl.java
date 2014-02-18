b664c47f1432d413dfe158da0683ab8730b859db
afodor <anthony.fodor@gmail.com>
2014-02-18 16:20:32 -0500

    towards script for metabolimics/microbiome analysis

--- src/scripts/metabolitesVs16S/TTestsCaseVsControl.java
+++ src/scripts/metabolitesVs16S/TTestsCaseVsControl.java
@@ -1,22 +1,35 @@
 package scripts.metabolitesVs16S;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.StringTokenizer;
 
+import utils.Avevar;
 import utils.ConfigReader;
+import utils.TTest;
 
 public class TTestsCaseVsControl
 {
 	public static void main(String[] args) throws Exception
 	{
+		String[] vals = { "fam" , "gen", "ord", "phy" , "cls" };
+		
+		for(String s : vals)
+			runALevel(s);
+		
+	}
+	
+	public static void runALevel(String level) throws Exception
+	{
 		BufferedReader reader = new BufferedReader(new FileReader(new File(ConfigReader.getMetabolitesCaseControl() + 
-				File.separator + "topeFeb2014_logNorm_gen.txt")));
+				File.separator + "topeFeb2014_logNorm_" + level + ".txt")));
 		
 		List<String> taxaNames= (getTaxaNames(reader.readLine()));
 		HashMap<String, Holder> taxaMap = new LinkedHashMap<String, Holder>();
@@ -29,9 +42,12 @@ public class TTestsCaseVsControl
 		}
 		
 		populateMap(taxaNames, taxaMap, reader);
+		writeResults(taxaMap, taxaNames,level);
 		
 		reader.close();
 	}
+	
+	
 	
 	private static void populateMap( List<String> taxaNames, HashMap<String, Holder> map, BufferedReader reader )
 		throws Exception
@@ -78,6 +94,50 @@ public class TTestsCaseVsControl
 		String taxaName;
 		HashMap<String, Double> caseMap = new HashMap<String,Double>();
 		HashMap<String, Double> controlMap = new HashMap<String,Double>();
+	}
+	
+	private static void writeResults( HashMap<String, Holder> map , List<String> taxaName, String level ) throws Exception
+	{
+		BufferedWriter writer = new BufferedWriter(new FileWriter(new File(ConfigReader.getMetabolitesCaseControl() +
+				File.separator +  level + "TTest.txt")));
+		
+		writer.write(level + "\tcaseAverage\tcontrolAverage\tcaseControlRatio\tpValue\n");
+		
+		for( String s : taxaName ) 
+		{
+			Holder h = map.get(s);
+			writer.write(h.taxaName + "\t");
+			List<Double> caseList = new ArrayList<Double>();
+			List<Double> controlList = new ArrayList<Double>();
+			
+			for( String s2 : h.caseMap.keySet() )
+				caseList.add(h.caseMap.get(s2));
+			
+			for( String s2 : h.controlMap.keySet())
+				controlList.add(h.controlMap.get(s2));
+			
+			double caseAvg = new Avevar(caseList).getAve() ;
+			writer.write( caseAvg+ "\t");
+			
+			double controlAvg = new Avevar(controlList).getAve();
+			writer.write(controlAvg+ "\t");
+			writer.write( caseAvg / controlAvg + "\t");
+			
+			double pValue =1 ;
+			
+			try
+			{
+				pValue = TTest.ttestFromNumber(caseList, controlList).getPValue();
+			}
+			catch(Exception ex)
+			{
+				
+			}
+			
+			writer.write(pValue + "\n");
+		} 
+		
+		writer.flush();  writer.close();
 	}
 	
 	private static List<String> getTaxaNames(String headerLine) throws Exception
