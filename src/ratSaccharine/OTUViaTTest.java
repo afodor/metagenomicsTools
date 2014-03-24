dd21174648032f1aac75e8978d54a7e05bc10b72
afodor <anthony.fodor@gmail.com>
2014-03-24 19:51:20 -0400

    stub for t-test p-values

--- src/ratSaccharine/OTUViaTTest.java
+++ src/ratSaccharine/OTUViaTTest.java
@@ -12,7 +12,14 @@
 
 package ratSaccharine;
 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
 import java.util.HashMap;
+import java.util.List;
+
+import parsers.OtuWrapper;
+import utils.ConfigReader;
 
 public class OTUViaTTest
 {
@@ -21,8 +28,34 @@ public class OTUViaTTest
 	{
 		HashMap<String, MetadataFileLine> map = MetadataFileLine.getMap();
 		
+		OtuWrapper wrapper = new OtuWrapper(ConfigReader.getSaccharineRatDir() + File.separator + "otuCounts.txt");
+		
+		
+		
 		System.out.println(map);
 	}
+	
+	private static class Holder
+	{
+		// the key is the cage
+		// the outer list has one entry for each OTU
+		// the inner list has one entry for each cage
+		HashMap<String, List<List<Double>>> lowSachData = new HashMap<String, List<List<Double>>>();
+		HashMap<String, List<List<Double>>> highSachData = new HashMap<String, List<List<Double>>>();
+	}
+	
+	/*
+	private static void writeTTestFile(String tissue, HashMap<String, MetadataFileLine> metaMap, OtuWrapper wrapper)
+		throws Exception
+	{
+		BufferedWriter writer = new BufferedWriter(new FileWriter(new File(ConfigReader.getSaccharineRatDir() + 
+				File.separator + "otuTTests"  + tissue + ".txt")));
+		
+		for( int x=0; x < wrapper.
+		
+		writer.flush();  writer.close();
+	}
+	*/
 	
 	
 }
