1574378ac8d8f9f903e9ab0266abcdd4e98fdb07
afodor <anthony.fodor@gmail.com>
2014-06-19 13:20:17 -0400

    Attempt at Java PCA (will probably be much too slow)

---
+++ src/bigDataScalingFactors/PCA_Pivot.java
@@ -0,0 +1,45 @@
+package bigDataScalingFactors;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
+import parsers.OtuWrapper;
+import pca.PCA;
+import utils.ConfigReader;
+
+public class PCA_Pivot
+{
+	public static void main(String[] args) throws Exception
+	{
+		OtuWrapper wrapper = new OtuWrapper(ConfigReader.getBigDataScalingFactorsDir() + File.separator + "risk" 
+				+ File.separator + 
+			"riskRawTaxaAsColumn.txt");
+		
+		System.out.println(wrapper.getSampleNames().size() + " " + wrapper.getOtuNames().size());
+			
+		double[][] d=  wrapper.getAsArray();
+			
+		List<String> numSequences = new ArrayList<String>();
+		List<String> sampleIDs = new ArrayList<String>();
+		for(String s : wrapper.getSampleNames())
+		{
+			System.out.println(s);
+			sampleIDs.add(s);
+			numSequences.add("" + wrapper.getCountsForSample(s));
+		}
+			
+		List<String> catHeaders = new ArrayList<String>();
+		catHeaders.add("NumSequences"); 
+			
+		List<List<String>> categories = new ArrayList<List<String>>();
+			
+		categories.add(numSequences); 
+		
+		File outFile = new File(ConfigReader.getBigDataScalingFactorsDir() + File.separator + "risk" 
+				+ File.separator + 
+			"PCA_NotNormalized.txt");
+			
+		PCA.writePCAFile(sampleIDs, catHeaders, categories,d, outFile);
+	}
+}
