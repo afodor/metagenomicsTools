ba22adcb5b622bc9027eec2883494c810c370c12
afodor <anthony.fodor@gmail.com>
2014-06-28 14:20:31 -0400

    update R code to do the PCA...

--- src/bigDataScalingFactors/PCA_Pivot.java
+++ src/bigDataScalingFactors/PCA_Pivot.java
@@ -6,7 +6,6 @@ import java.io.File;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.util.ArrayList;
-import java.util.HashSet;
 import java.util.List;
 
 import parsers.OtuWrapper;
@@ -20,14 +19,15 @@ public class PCA_Pivot
 {
 	public static void main(String[] args) throws Exception
 	{
-		BufferedWriter writer = new BufferedWriter(new FileWriter(new File(ConfigReader.getBigDataScalingFactorsDir() + 
-				File.separator + "risk" + File.separator + "pcoaLogPercentile.txt")));
+		BufferedWriter writer = new BufferedWriter(new FileWriter(new File(ConfigReader.getBigDataScalingFactorsDir() + File.separator + "June24_risk" 
+				+ File.separator + 
+			"raw_100_taxaAsColumnsRanked_PCA.txt")));
 		
 		writer.write("taxaRemoved\ttaxaLeft\tsamplesRemoved\tsamplesLeft\trSquared\tpValue\n");
 		
-		OtuWrapper wrapper = new OtuWrapper(ConfigReader.getBigDataScalingFactorsDir() + File.separator + "risk" 
+		OtuWrapper wrapper = new OtuWrapper(ConfigReader.getBigDataScalingFactorsDir() + File.separator + "June24_risk" 
 				+ File.separator + 
-			"riskRawTaxaAsColumn.txt");
+			"raw_100_taxaAsColumnsRanked.txt"  );
 		
 		writePCOA(writer, wrapper);
 		
@@ -39,6 +39,7 @@ public class PCA_Pivot
 	{	
 		System.out.println(wrapper.getSampleNames().size() + " " + wrapper.getOtuNames().size());
 		
+		/*
 		HashSet<String> excludedTaxa = new HashSet<String>();
 		
 		/*
@@ -49,6 +50,7 @@ public class PCA_Pivot
 				excludedTaxa.add(wrapper.getOtuNames().get(x));
 		}*/
 		
+		/*
 		HashSet<String> excludedSamples = new HashSet<String>();
 		
 		for( int x=0; x < wrapper.getSampleNames().size(); x++)
@@ -65,8 +67,9 @@ public class PCA_Pivot
 		System.out.println("EXCLUDING " + excludedTaxa);
 		System.out.println("EXCLUDING " + excludedSamples);
 		System.out.println("New wrapper " + wrapper.getOtuNames().size() + " " + wrapper.getSampleNames().size());
+			*/
 			
-		double[][] d=  wrapper.getNormalizedAsArray();
+		double[][] d=  wrapper.getUnnorlalizedAsArray();
 			
 		List<String> numSequences = new ArrayList<String>();
 		List<String> sampleIDs = new ArrayList<String>();
@@ -90,9 +93,11 @@ public class PCA_Pivot
 			
 		PCA.writePCAFile(sampleIDs, catHeaders, categories,d, outFile, new CorrelationMatrixDistanceMeasure());
 		
-		writer.write(excludedTaxa.size() + "\t");
+		writer.write( "0\t");
+		//writer.write(excludedTaxa.size() + "\t");
 		writer.write(wrapper.getOtuNames().size() + "\t");
-		writer.write(excludedSamples.size() + "\t");
+		//writer.write(excludedSamples.size() + "\t");
+		writer.write("0\t");
 		writer.write(wrapper.getSampleNames().size() + "\t");
 		
 		BufferedReader reader = new BufferedReader(new FileReader(outFile));
