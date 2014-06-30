7a499f77a802cf3c133d4c0f0ac5070248834e7d
afodor <anthony.fodor@gmail.com>
2014-06-30 12:14:26 -0400

    Just take the top 10...

--- src/bigDataScalingFactors/AddTotalCountsAndDiversity.java
+++ src/bigDataScalingFactors/AddTotalCountsAndDiversity.java
@@ -3,24 +3,41 @@ package bigDataScalingFactors;
 import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileWriter;
+import java.util.HashSet;
 
 import parsers.OtuWrapper;
 import utils.ConfigReader;
 
 public class AddTotalCountsAndDiversity
 {
+	private static HashSet<String> getIncluded()
+	{
+		HashSet<String> set = new HashSet<String>();
+
+		
+		String[] a = {"469709","199293","190511","198251","181330","326977","331820","130663","158660","470239"
+				};
+		
+		for(String s :a )
+			set.add(s);
+		
+		return set;
+	}
+	
 	public static void main(String[] args) throws Exception
 	{
+		HashSet<String> included = getIncluded();
+		
 		OtuWrapper wrapper = new OtuWrapper(ConfigReader.getBigDataScalingFactorsDir() + File.separator + 
-				"June24_risk" + File.separator +"raw_100_taxaAsColumns.txt" );
+				"June24_risk" + File.separator +"RLE_100_taxaAsColumn.txt" );
 		
 		BufferedWriter writer = new BufferedWriter(new FileWriter(new File(ConfigReader.getBigDataScalingFactorsDir() + File.separator + 
-				"June24_risk" + File.separator +"raw_100_taxaAsColumnsWithMeta.txt" )));
+				"June24_risk" + File.separator +"rle_100_taxaAsColumnsWithMeta.txt" )));
 		
 		writer.write("sample\tshannonDiversity\ttotalSeqs");
 		
 		for(String s : wrapper.getOtuNames())
-			if( wrapper.getCountsForTaxa(s) > 30000 )
+			if( included.contains(s))
 			writer.write("\t" + s);
 		
 		writer.write("\n");
@@ -32,7 +49,7 @@ public class AddTotalCountsAndDiversity
 			writer.write(wrapper.getCountsForSample(x) + "");
 			
 			for( int y=0; y < wrapper.getOtuNames().size(); y++)
-				if( wrapper.getCountsForTaxa(y) > 30000)
+				if( included.contains( wrapper.getOtuNames().get(y)))
 				{
 					writer.write("\t" + wrapper.getDataPointsUnnormalized().get(x).get(y));
 				}
