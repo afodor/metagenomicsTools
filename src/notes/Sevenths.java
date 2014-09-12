6d875157ee6b604f731e8d7d39486b8297247cb1
afodor <anthony.fodor@gmail.com>
2014-09-12 17:54:42 -0400

    sevenths code (for fun...)

---
+++ src/notes/Sevenths.java
@@ -0,0 +1,62 @@
+package notes;
+
+public class Sevenths
+{
+	public static final String[] NOTES = { "C", "C#", "D", "D#", "E", "F", "F#", "G", "G#",
+																"A", "Bb"};
+	
+	public static void main(String[] args)
+	{
+		
+
+		int[] diminished = new int[4];
+		diminished[0] = 0;
+		diminished[1] =3;
+		diminished[2] = 6;
+		diminished[3] = 8;
+		
+		for(int x=0; x < NOTES.length; x++)
+		{
+			int[] halfDiminished = new int[4];
+			halfDiminished[0] = diminished[0];
+			halfDiminished[1] = diminished[1];
+			halfDiminished[2] = diminished[2];
+			halfDiminished[3] = addOne(diminished[3]);
+			printResults(diminished, halfDiminished);
+		}
+		
+	}
+	
+	private static void printResults( int[] diminished, int[] halfDiminished )
+	{
+		System.out.println( " " + NOTES[ diminished[0]] + "\t" + NOTES[halfDiminished[0]] );
+
+		System.out.println( " " + NOTES[ diminished[1]] + "\t" + NOTES[halfDiminished[1]] );
+		System.out.println( " " + NOTES[ diminished[2]] + "\t" + NOTES[halfDiminished[2]] );
+		System.out.println( " " + NOTES[ diminished[3]] + "  " + (char)(94) +  "\t" + NOTES[halfDiminished[3]] );
+		
+		System.out.println("dim\thalfDim\t");
+		System.out.println("\n\n");
+	}
+	
+	private static int addOne(int i)
+	{
+		i++;
+		
+		if( i > NOTES.length -1)
+			i =0;
+		
+		return i;
+	}
+	
+	private static int subtractOne(int i)
+	{
+		i--;
+		
+		if( i < 0)
+			i = NOTES.length -1;
+		
+		return i;
+	}
+	
+}
