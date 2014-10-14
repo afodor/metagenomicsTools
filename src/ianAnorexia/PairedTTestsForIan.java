19c53f5f1f4de32b18e1203d94a48de868bed486
afodor <anthony.fodor@gmail.com>
2014-10-14 14:51:14 -0400

    paired t-test...

--- src/ianAnorexia/PairedTTestsForIan.java
+++ src/ianAnorexia/PairedTTestsForIan.java
@@ -1,12 +1,18 @@
 package ianAnorexia;
 
+import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileWriter;
+import java.util.Collections;
 import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.ArrayList;
 
 import parsers.OtuWrapper;
+import utils.Avevar;
 import utils.ConfigReader;
+import utils.TTest;
 
 public class PairedTTestsForIan
 {
@@ -16,8 +22,30 @@ public class PairedTTestsForIan
 		
 		 HashMap<Integer, Holder> map = getHolders(wrapper);
 		
-		for(Integer key : map.keySet())
-			System.out.println(key + " " + map.get(key).ad + " " + map.get(key).dis);
+		 List<OtuHolder> list = getOTUList(map, wrapper);
+		 writeResults(list);
+	}
+	
+	private static void writeResults(List<OtuHolder> list) throws Exception
+	{
+		BufferedWriter writer = new BufferedWriter(new FileWriter(new File(ConfigReader.getIanAnorexiaDir() + File.separator + 
+				"pairedTTestByTime.txt")));
+		
+		writer.write("otuID\tavgAD\tavdDS\tprevelance\tpValue\tbhFDR\n");
+		
+		int rank =1;
+		for(OtuHolder otu : list )
+		{
+			writer.write(otu.otuName + "\t");
+			writer.write(new Avevar(otu.adSamples).getAve() + "\t");
+			writer.write(new Avevar(otu.disSamples).getAve() + "\t");
+			writer.write(otu.getPrevelance() + "\t");
+			writer.write(otu.pValue + "\t");
+			writer.write( (otu.pValue * list.size() / rank) + "\n");
+			rank++;
+		}
+		
+		writer.flush();  writer.close();
 	}
 	
 	private static class Holder
@@ -39,7 +67,75 @@ public class PairedTTestsForIan
 		{
 			return Double.compare(this.pValue, arg0.pValue);
 		}
+		
+		public double getPrevelance()
+		{
+			double num=0;
+			
+			for( Double d : adSamples )
+				if(d > 0)
+					num++;
+			
+			for( Double d : disSamples)
+				if( d > 0 )
+					num++;
+			
+			return num/ (adSamples.size() + disSamples.size());
+		}
 	}
+	
+	private static List<OtuHolder> getOTUList(HashMap<Integer, Holder> map, OtuWrapper wrapper) throws Exception
+	{
+		List<OtuHolder> list = new ArrayList<OtuHolder>();
+		
+		List<Integer> keys = new ArrayList<Integer>();
+
+		int numOTUS =wrapper.getOtuNames().size();
+		
+		
+		for( Integer key : map.keySet())
+		{
+			Holder h = map.get(key);
+			
+			if( h.ad != null && h.dis != null)
+			{
+				keys.add(key);
+				
+				if( h.ad.size() != numOTUS || h.dis.size() != numOTUS)
+					throw new Exception("Logic error");
+			}
+				
+		}
+		
+		Collections.sort(keys);
+		
+		for( int x=0; x <numOTUS; x++)
+		{
+			OtuHolder o =  new OtuHolder();
+			list.add(o);
+			
+			for( Integer i : keys)
+			{
+				Holder h = map.get(i);
+				o.adSamples.add(h.ad.get(x));
+				o.disSamples.add(h.dis.get(x));
+			}
+			
+			try
+			{
+				o.pValue = TTest.pairedTTest(o.adSamples, o.disSamples).getPValue();
+			}
+			catch(Exception ex)
+			{
+				
+			}
+		}
+		
+		Collections.sort(list);
+		return list;
+	}
+	
+	
 	
 	private static Holder getOrCreate(int id, HashMap<Integer, Holder> map) throws Exception
 	{
@@ -64,7 +160,7 @@ public class PairedTTestsForIan
 	
 	private static HashMap<Integer, Holder> getHolders( OtuWrapper wrapper ) throws Exception
 	{
-		HashMap<Integer, Holder> map = new HashMap<Integer, Holder>();
+		HashMap<Integer, Holder> map = new LinkedHashMap<Integer, Holder>();
 		
 		for(String s : wrapper.getSampleNames())
 		{
