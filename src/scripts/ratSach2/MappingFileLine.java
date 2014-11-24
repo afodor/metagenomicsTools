642439a7a070e41a4838c6dccbb23e85eccec2f8
Anthony Fodor <anthony.fodor@gmail.com>
2014-11-23 20:08:35 -0800

    code to pivot spreadsheets

---
+++ src/scripts/ratSach2/MappingFileLine.java
@@ -0,0 +1,77 @@
+package scripts.ratSach2;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.util.HashMap;
+
+import utils.ConfigReader;
+
+public class MappingFileLine
+{
+	private final String sampleID;
+	private final String condition;
+	private final String ratID;
+	private final String tissue;
+	
+	public String getSampleID()
+	{
+		return sampleID;
+	}
+
+	public String getCondition()
+	{
+		return condition;
+	}
+
+	public String getRatID()
+	{
+		return ratID;
+	}
+
+	public String getTissue()
+	{
+		return tissue;
+	}
+
+	public static HashMap<String, MappingFileLine> getMap() throws Exception
+	{
+		HashMap<String, MappingFileLine> map =new HashMap<String, MappingFileLine>();
+		
+		BufferedReader reader = new BufferedReader(new FileReader(ConfigReader.getRachSachReanalysisDir() + 
+				File.separator + "Dess150_05262014_try02_mapping.txt"));
+		
+		reader.readLine();
+		
+		for( String s= reader.readLine() ; s != null; s= reader.readLine())
+		{
+			MappingFileLine mfl = new MappingFileLine(s);
+			
+			if(map.containsKey(mfl.sampleID))
+				throw new Exception("No");
+			
+			map.put(mfl.sampleID, mfl);
+			
+			
+		}
+		
+		reader.close();
+		return map;
+	}
+	
+	public MappingFileLine(String s)
+	{
+		String[] splits = s.split("\t");
+		this.tissue = splits[4];
+		this.condition = splits[7];
+		this.ratID = splits[8];
+		this.sampleID = splits[0];
+		
+	}
+	
+	public static void main(String[] args) throws Exception
+	{
+		
+	}
+	
+}
---
+++ src/scripts/ratSach2/WriteSpreadsheetsForR.java
@@ -0,0 +1,101 @@
+package scripts.ratSach2;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileReader;
+
+
+import java.io.FileWriter;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+import parsers.OtuWrapper;
+import utils.ConfigReader;
+
+public class WriteSpreadsheetsForR
+{
+	public static void main(String[] args) throws Exception
+	{
+		writeAndNormalize("Cecal Content");
+		//writeForATissue("Colon content");
+		//writeForATissue("Fecal content");
+	}
+	
+	private static void writeAndNormalize(String tissue) throws Exception
+	{
+		File aFile = writeForATissue(tissue);
+		File countFile = new File(
+				ConfigReader.getRachSachReanalysisDir() + File.separator + "otu_" + tissue + "_taxaAsCols.txt" );
+		OtuWrapper.transpose(aFile.getAbsolutePath(),countFile.getAbsolutePath(), false);
+		
+		OtuWrapper wrapper = new OtuWrapper(countFile);
+		
+		File logNormFile = new File(
+				ConfigReader.getRachSachReanalysisDir() + File.separator + "otu_" + tissue + "_taxaAsColsLogNorm.txt" );
+		
+		wrapper.writeNormalizedLoggedDataToFile(logNormFile);
+	}
+	
+	private static File writeForATissue(String tissue) throws Exception
+	{
+		File pivotFile = new File(ConfigReader.getRachSachReanalysisDir() + File.separator + "otu_" + tissue + ".txt");
+		
+		BufferedWriter writer = new BufferedWriter(new FileWriter(pivotFile));
+		
+		File otuFile = new File(
+				ConfigReader.getRachSachReanalysisDir() + File.separator + 
+				"otu_table.txt");
+		BufferedReader reader = new BufferedReader(new FileReader(otuFile));
+		
+		List<Boolean> includeList = new ArrayList<Boolean>();
+		
+		HashMap<String, MappingFileLine> metaMap = MappingFileLine.getMap();
+		
+		String[] splits = reader.readLine().split("\t");
+		
+		for( int x=1; x < splits.length-1; x++)
+		{
+			String sample = splits[x];
+			
+			System.out.println(sample);
+			MappingFileLine mfl = metaMap.get(sample);
+			
+			boolean include =false;
+			
+			if( mfl.getCondition().equals("Low") || mfl.getCondition().equals("High"))
+			{
+				if( mfl.getTissue().equals(tissue))
+					include= true;
+			}
+			
+			includeList.add(include);
+		}
+		
+		writer.write("sampleID");
+		
+		for( int x=1; x < splits.length-1; x++)
+			if( includeList.get(x-1))
+				writer.write("\t" + splits[x]);
+		
+		writer.write("\n");
+		
+		for(String s= reader.readLine() ; s != null; s = reader.readLine())
+		{
+			splits = s.split("\t");
+			
+			writer.write(splits[0]);
+			
+			for( int x=1; x < splits.length-1; x++)
+				if( includeList.get(x-1))
+					writer.write("\t" + splits[x]);
+			
+			writer.write("\n");
+		}
+		
+		writer.flush();  writer.close();
+		
+		return pivotFile;
+	}
+}
