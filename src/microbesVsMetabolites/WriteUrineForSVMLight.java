04f4aca41f42277c9f2800666765d54c2f253a4a
afodor <anthony.fodor@gmail.com>
2014-11-26 19:40:05 -0500

    write files for svm light

--- src/microbesVsMetabolites/WriteUrineForSVMLight.java
+++ src/microbesVsMetabolites/WriteUrineForSVMLight.java
@@ -1,12 +1,16 @@
 package microbesVsMetabolites;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Random;
 
 import utils.ConfigReader;
 import utils.TabReader;
@@ -43,6 +47,49 @@ public class WriteUrineForSVMLight
 		HashMap<Integer,Double> pcoaMap = getPCOA();
 		HashMap<Integer, List<Double>> metaboliteMap = getMetabolites();
 		
+		List<Integer> keys = new ArrayList<Integer>(pcoaMap.keySet());
+		int halfPoint = keys.size() / 2;
+		Random random = new Random(324234);
+		Collections.shuffle(keys, random);
+		
+		BufferedWriter writer = new BufferedWriter(new FileWriter(
+				new File(ConfigReader.getMicrboesVsMetabolitesDir() + File.separator + 
+						"trainingSet.txt"
+				)));
+		
+		for( int x=0; x < halfPoint; x++)
+		{
+			writer.write( pcoaMap.get(keys.get(x)) + " "  );
+			
+			List<Double> list = metaboliteMap.get(keys.get(x));
+			
+			for(  int y=0; y < list.size(); y++ )
+				writer.write( (y+1) + ":" + list.get(y) + " " );
+			
+			writer.write("\n");
+		}
+		
+		writer.flush();  writer.close();
+		// train with svm_learn.exe -z r trainingSet.txt regressModel
+		
+		writer = new BufferedWriter(new FileWriter(new File( 
+			ConfigReader.getMicrboesVsMetabolitesDir() + File.separator + 
+					"setToClassify.txt"	)));
+		
+		for( int x=halfPoint; x < keys.size(); x++ ) 
+		{
+			writer.write("sample" + keys.get(x) + " " );
+			
+			List<Double> list = metaboliteMap.get(keys.get(x));
+			
+			for(  int y=0; y < list.size(); y++ )
+				writer.write( (y+1) + ":" + list.get(y) + " " );
+			
+			writer.write("\n");
+		}
+		// classify with svm_classify setToClassify.txt regressModelsvmOut.txt
+		
+		writer.flush(); writer.close();
 	}
 	
 	private static HashMap<Integer, List<Double>> getMetabolites() throws Exception
