fe8d8fc0e8cf87bad63e87430044d0705d922611
Anthony Fodor <anthony.fodor@gmail.com>
2014-11-27 20:10:40 -0800

    summary file..

--- src/microbesVsMetabolites/WriteSummaryFile.java
+++ src/microbesVsMetabolites/WriteSummaryFile.java
@@ -17,31 +17,42 @@ public class WriteSummaryFile
 	{
 		BufferedWriter writer = new BufferedWriter(new FileWriter(
 				new File(ConfigReader.getMicrboesVsMetabolitesDir() + File.separator + 
-					"summary.txt"	)));
+					"summary.txt")));
 		
-		writer.write("component\tplasmaAvgR\tplasmaSDR\tplasmaScrambedAvgR\tplasmaScrambledSDR");
+		writer.write("component\tplasmaAvgR\tplasmaSDR\tplasmaScrambedAvgR\tplasmaScrambledSDR\t");
+		writer.write("urineAvgR\turineSDR\turineScrambedAvgR\turineScrambledSDR\t");
+		writer.write("metadataAvgR\tmetadataSDR\tmetadataScrambedAvgR\tmetadataScrambledSDR\n");
 		
-		for( int x=1; x <= 4; x++)
+		
+		for( int x=1; x <= 14; x++)
 		{
-			System.out.println(x);
-			File unscrambled = new File(ConfigReader.getMicrboesVsMetabolitesDir() + File.separator + 
-												"trials_Comp"+  x + ".txt");
+			writer.write(x+ "\t");
+			writeASet(writer, x, 1, false);
+			writeASet(writer, x, 3, false);
+			writeASet(writer, x, 7, true);
 			
-			File scrambed = new File(ConfigReader.getMicrboesVsMetabolitesDir() + File.separator + 
-										"trials_comp" + x+ "scramble.txt");
-			
-			writer.write(x + "\t");
-			
-			Avevar plasma = getAveVar(unscrambled, 1);
-			writer.write(plasma.getAve() + "\t");
-			writer.write(plasma.getSD() + "\t");
-			
-			Avevar scrambledPlasma = getAveVar(scrambed, 1);
-			writer.write(scrambledPlasma.getAve() + "\t");
-			writer.write(scrambledPlasma.getSD() + "\n");
 		}
 		writer.flush(); writer.close();
 		
+	}
+	
+	private static void writeASet( BufferedWriter writer, int compNum, int colNum,  boolean endLine)
+		throws Exception
+	{
+		System.out.println(compNum);
+		File unscrambled = new File(ConfigReader.getMicrboesVsMetabolitesDir() + File.separator + 
+											"trials_Comp"+  compNum + ".txt");
+		
+		File scrambed = new File(ConfigReader.getMicrboesVsMetabolitesDir() + File.separator + 
+									"trials_comp" + compNum+ "scramble.txt");
+		
+		Avevar plasma = getAveVar(unscrambled, colNum);
+		writer.write(plasma.getAve() + "\t");
+		writer.write(plasma.getSD() + "\t");
+		
+		Avevar scrambledPlasma = getAveVar(scrambed, colNum);
+		writer.write(scrambledPlasma.getAve() + "\t");
+		writer.write(scrambledPlasma.getSD() + (endLine ?  "\n" : "\t") );
 	}
 	
 	private static Avevar getAveVar(File file , int column) throws Exception
