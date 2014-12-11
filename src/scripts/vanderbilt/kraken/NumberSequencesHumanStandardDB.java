6dca3f20c607d917a2d01ae6d124a28f145832ef
afodor <anthony.fodor@gmail.com>
2014-12-11 14:06:14 -0500

    attempt to count human vs. non -human

--- src/scripts/vanderbilt/kraken/NumberSequencesHumanStandardDB.java
+++ src/scripts/vanderbilt/kraken/NumberSequencesHumanStandardDB.java
@@ -1,10 +1,13 @@
 package scripts.vanderbilt.kraken;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileWriter;
 import java.io.InputStreamReader;
 import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.StringTokenizer;
 import java.util.zip.GZIPInputStream;
 
@@ -16,6 +19,34 @@ public class NumberSequencesHumanStandardDB
 	{
 		HashMap<String, Boolean> foundInHuman = new HashMap<String, Boolean>();
 		HashMap<String, Boolean> foundInReference = new HashMap<String, Boolean>();
+		
+		int numFoundInBoth() throws Exception
+		{
+			int x=0;
+			
+			if( ! foundInHuman.keySet().equals(foundInReference.keySet()))
+				throw new Exception("No");
+			
+			for(String s : foundInHuman.keySet())
+				if( foundInHuman.get(s) && foundInReference.get(s) )
+					x++;
+			
+			return x;
+		}
+		
+		int numFoundInNeither() throws Exception
+		{
+			int x=0;
+			
+			if( ! foundInHuman.keySet().equals(foundInReference.keySet()))
+				throw new Exception("No");
+			
+			for(String s : foundInHuman.keySet())
+				if( !foundInHuman.get(s) && !foundInReference.get(s) )
+					x++;
+			
+			return x;
+		}
 	}
 	
 	private static void addToMap(File file, HashMap<String, Holder> map) throws Exception
@@ -67,15 +98,71 @@ public class NumberSequencesHumanStandardDB
 		
 	}
 	
+	private static int getNumTrue(HashMap<String, Boolean> map ) 
+	{
+		int x=0;
+		
+		for(Boolean b : map.values())
+			if( b)
+				x++;
+		
+		return x;
+	}
+	
+	private static void writeResults(HashMap<String, Holder> map) throws Exception
+	{
+		BufferedWriter writer = new BufferedWriter(new FileWriter(new File(ConfigReader.getVanderbiltDir() + File.separator + 
+				"humanVsReferenceFromKraken.txt")));
+		
+		writer.write("fullSampleName\tsampleID\tstoolVsSwab\trunNum\tnumClassifiedRef\tnumNotClassifiedRef\t" + 
+						"totalRef\tnumClassifiedHuman\tnumNotClassifeidHuman\ttotalHuman\tnumNeither\tnumBoth\n");
+		
+		for(String s : map.keySet())
+		{
+			writer.write(s + "\t");
+			String[] splits = s.split("_");
+			writer.write( splits[0] + "\t" );
+			if( s.startsWith("ST"))
+					writer.write("stool\t");
+			else if (s.startsWith("SW"))
+					writer.write("swab\t");
+			else throw new Exception("No");
+			
+			Holder h = map.get(s);
+			
+			
+			writer.write(splits[1] + "\t");
+
+			int numClassifiedRef = getNumTrue(h.foundInReference);
+			writer.write(numClassifiedRef + "\t");
+			writer.write((h.foundInReference.size() - numClassifiedRef) + "\t");
+			writer.write(h.foundInReference.size() + "\t");
+			
+			int numClassifiedHuman = getNumTrue(h.foundInHuman);
+			writer.write(numClassifiedHuman + "\t");
+			writer.write((h.foundInHuman.size() - numClassifiedHuman) + "\t");
+			writer.write(h.foundInHuman.size()+"\t");
+			writer.write(h.numFoundInBoth() + "\t");
+			writer.write(h.numFoundInNeither() + "\n");
+		}
+		
+		
+		
+		
+		writer.flush();  writer.close();
+	}
+	
 	public static void main(String[] args) throws Exception
 	{
-		HashMap<String, Holder> map = new HashMap<String, Holder>();
+		HashMap<String, Holder> map = new LinkedHashMap<String, Holder>();
 		
 		File topDir = new File(ConfigReader.getVanderbiltDir() + File.separator + "krakenOut");
 		
 		for(String s: topDir.list())
 			if( s.startsWith("Sample"))
 				addToMap(new File(topDir.getAbsoluteFile() + File.separator + s), map);
+		
+		writeResults(map);
 		
 		
 	}
