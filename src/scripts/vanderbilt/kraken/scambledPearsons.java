d34a9474509a0fb2ddd6b8862d7583a2b213b73c
afodor <anthony.fodor@gmail.com>
2014-12-15 15:10:14 -0500

    stub for scrambling Pearson's

--- src/scripts/vanderbilt/kraken/scambledPearsons.java
+++ src/scripts/vanderbilt/kraken/scambledPearsons.java
@@ -5,12 +5,18 @@ import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileReader;
 import java.io.FileWriter;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashMap;
+import java.util.List;
+import java.util.Random;
 
 import utils.ConfigReader;
 
 public class scambledPearsons
 {
+	private static final Random RANDOM = new Random(3423423);
+	
 	public static void main(String[] args) 
 	{
 		
@@ -68,6 +74,25 @@ public class scambledPearsons
 		return map;
 	}
 	
+	private double getAPearson(int sampleID, List<String> samples, 
+							boolean shuffle, HashMap<String, HashMap<String, Holder>>  dataMap)
+	{
+		List<Double> list1 = new ArrayList<Double>();
+		List<Double> list2 = new ArrayList<Double>();
+		
+		HashMap<String, Holder> realMap = dataMap.get(samples.get(sampleID));
+		
+		int randomIndex= RANDOM.nextInt( samples.size() );
+		
+		while(randomIndex== sampleID)
+			randomIndex= RANDOM.nextInt( samples.size() );
+		
+		//HashMap<String, Holder> randomMap= dataMap.get(sampleID.get(randomIndex));
+		
+		return 0;
+		
+	}
+	
 	private static void writePivotsForALevel(String level) throws Exception
 	{
 		BufferedWriter writer = new BufferedWriter(new FileWriter(new File(
@@ -77,6 +102,9 @@ public class scambledPearsons
 		writer.write("sample\trealR\tscambledRAvg\tscrambedRSD\n");
 		
 		HashMap<String, HashMap<String, Holder>>  map = getMap(level);
+		List<String> samples = new ArrayList<String>(map.keySet());
+		Collections.sort(samples);
+		
 		
 		writer.flush();  writer.close();
 	}
