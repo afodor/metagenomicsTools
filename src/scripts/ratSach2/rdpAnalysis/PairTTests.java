e84eab595742a21ed0a840525c88c2936908dce0
afodor <anthony.fodor@gmail.com>
2015-01-05 16:28:38 -0500

    stub for paired t-test...

---
+++ src/scripts/ratSach2/rdpAnalysis/PairTTests.java
@@ -0,0 +1,15 @@
+package scripts.ratSach2.rdpAnalysis;
+
+public class PairTTests
+{
+	private static class Holder
+	{
+		double pValueCecum;
+		double pValueColon;
+	}
+	
+	public static void main(String[] args)
+	{
+		
+	}
+}
