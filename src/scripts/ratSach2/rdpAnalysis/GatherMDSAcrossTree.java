1a6265d8eb658c9546c353295bb42ccf776b1ae2
afodor <anthony.fodor@gmail.com>
2015-01-06 17:12:20 -0500

    prettier formating for gather across taxonomic levels...

--- src/scripts/ratSach2/rdpAnalysis/GatherMDSAcrossTree.java
+++ src/scripts/ratSach2/rdpAnalysis/GatherMDSAcrossTree.java
@@ -5,6 +5,7 @@ import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileReader;
 import java.io.FileWriter;
+import java.text.NumberFormat;
 
 import utils.ConfigReader;
 
@@ -12,6 +13,10 @@ public class GatherMDSAcrossTree
 {
 	public static void main(String[] args) throws Exception
 	{
+
+		NumberFormat nf = NumberFormat.getInstance();
+		nf.setMaximumFractionDigits(4);
+		
 		BufferedWriter writer = new BufferedWriter(new FileWriter(new File(
 				ConfigReader.getRachSachReanalysisDir()
 				+ File.separator + "rdpAnalysis" 
@@ -24,8 +29,8 @@ public class GatherMDSAcrossTree
 		for(String level : levels )
 		{
 			writer.write(level + "\t");
-			writer.write(getVal(level, "Cecal Content")  + "\t");
-			writer.write(getVal(level, "Colon content")  + "\n");
+			writer.write( nf.format(getVal(level, "Cecal Content"))  + "\t");
+			writer.write( nf.format(getVal(level, "Colon content")) + "\n");
 		}
 		
 		writer.flush();  writer.close();
@@ -34,6 +39,7 @@ public class GatherMDSAcrossTree
 	private static double getVal(String level, String tissue)
 		throws Exception
 	{
+		
 		BufferedReader reader = new BufferedReader(new FileReader( new File(
 				ConfigReader.getRachSachReanalysisDir()
 				+ File.separator + "rdpAnalysis" 
