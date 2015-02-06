7db7976d1d840d0ca4d6e4d6bc2d8278b254286a
afodor <anthony.fodor@gmail.com>
2015-02-06 15:02:51 -0500

    add counts info...

--- src/scripts/KylieAge/AddMetadata.java
+++ src/scripts/KylieAge/AddMetadata.java
@@ -14,13 +14,33 @@ import utils.ConfigReader;
 
 public class AddMetadata
 {
+	public static HashMap<String, Long> getSequenceCounts() throws Exception
+	{
+		HashMap<String, Long> map = new HashMap<String, Long>();
+		BufferedReader reader = new BufferedReader(new FileReader(new File(ConfigReader.getKylieAgeDir() + File.separator + "counts.txt")));
+		reader.readLine();
+		
+		for(String s = reader.readLine(); s != null; s= reader.readLine())
+		{
+			String[] splits = s.split("\t");
+			String key = splits[0].replace(".fastq.gz", "");
+			if( map.containsKey(key))
+				throw new Exception("No");
+			
+			map.put(key, Long.parseLong(splits[1]));
+		}
+		
+		return map;
+	}
+	
 	public static void main(String[] args) throws Exception
 	{
 		HashMap<String, MetadataParser> metaMap = MetadataParser.getMetaMap();
+		HashMap<String, Long> countMap = getSequenceCounts();
+		System.out.println(countMap.keySet());
 		
 		for(int x=1; x < NewRDPParserFileLine.TAXA_ARRAY.length; x++)
 		{
-
 			String level = NewRDPParserFileLine.TAXA_ARRAY[x];
 			
 			OtuWrapper wrapper = new OtuWrapper( 
@@ -35,7 +55,7 @@ public class AddMetadata
 			BufferedWriter writer = new BufferedWriter(new FileWriter(new File(ConfigReader.getKylieAgeDir()+
 					File.separator + "rdp" + File.separator + "pcoa_" + level  + "PlusMetadata.txt")));
 			
-			writer.write("sampleID\tread1_2\tnumSequences\tshannonEvenness\tshannonDiversity\tunrarifiedRichness\t" + 
+			writer.write("sampleID\tread1_2\tnumRawSequences\tfractionCalledSequences\tnumCalledSequences\tshannonEvenness\tshannonDiversity\tunrarifiedRichness\t" + 
 			"monkey\toldYoung\tlocation\toldShare\tcohab_neighbor\t" + reader.readLine() + "\n");
 			
 			for( String s = reader.readLine() ; s != null; s= reader.readLine())
@@ -44,15 +64,17 @@ public class AddMetadata
 				String key = new StringTokenizer(splits[0].replaceAll("\"", ""), "_").nextToken();
 				
 				MetadataParser mp = metaMap.get(key);
+				Long rawCounts = countMap.get(splits[0].replaceAll("\"", ""));
 				
 				if( mp == null)
 					throw new Exception("Could not find " + key);
 				
-
 				String sampleID = splits[0].replaceAll("\"", "");
+				System.out.println(splits[0]);
 				writer.write( sampleID+ "\t");
 				writer.write( sampleID.split("_")[3] + "\t" );
-				
+				writer.write( rawCounts + "\t");
+				writer.write( wrapper.getCountsForSample(sampleID)/ (((double)rawCounts)) + "\t");
 				writer.write( wrapper.getCountsForSample(sampleID) + "\t" );
 				writer.write( wrapper.getEvenness(sampleID) + "\t" );
 				writer.write( wrapper.getShannonEntropy(sampleID) + "\t" );
