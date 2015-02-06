7b178a8db3bc1336cf1caeeb5ca7ac9b895ebd85
afodor <anthony.fodor@gmail.com>
2015-02-06 10:10:23 -0500

    pivot three column for Kylie data...

--- src/utils/ConfigReader.java
+++ src/utils/ConfigReader.java
@@ -74,6 +74,7 @@ public class ConfigReader
 	public static final String RAT_SACH_REANALYSIS_DIR = "RAT_SACH_REANALYSIS_DIR";
 	public static final String MICROBES_VS_METABOLITES_DIR = "MICROBES_VS_METABOLITES_DIR";
 	public static final String VANDERBILT_DIR = "VANDERBILT_DIR";
+	public static final String KYLIE_AGE_DIR = "KYLIE_AGE";
 	
 	public static final String TANYA_DIR="TANYA_DIR";
 	
@@ -115,6 +116,11 @@ public class ConfigReader
 	public static String getMetabolitesCaseControl() throws Exception
 	{
 		return getConfigReader().getAProperty(METABOLITES_CASE_CONTROL);
+	}
+	
+	public static String getKylieAgeDir() throws Exception
+	{
+		return getConfigReader().getAProperty(KYLIE_AGE_DIR);
 	}
 	
 	public static String getChinaDir() throws Exception
