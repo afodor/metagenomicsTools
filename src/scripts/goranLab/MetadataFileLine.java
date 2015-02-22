e209591e4e57036490b828bb85adb3e24e9a6d62
afodor <anthony.fodor@gmail.com>
2015-02-22 14:08:11 -0500

    towards metadata parser

--- src/scripts/goranLab/MetadataFileLine.java
+++ src/scripts/goranLab/MetadataFileLine.java
@@ -9,10 +9,47 @@ import utils.ConfigReader;
 
 public class MetadataFileLine
 {
-	private String sanVsSol;
-	private String plq3Orplq4;
-	private String rNumber;
+	private final String sanVsSol;
+	private final String plq3Orplq4;
+	private final String rNumber;
 	
+	private MetadataFileLine(String firstLineKey, String secondLineKey)
+		throws Exception
+	{
+		//System.out.println(firstLineKey + " " + secondLineKey);
+		String rNumber = firstLineKey.split("-")[0];
+		
+		if( ! rNumber.startsWith("R"))
+			throw new Exception("No");
+		
+		this.rNumber = rNumber;
+		
+		this.plq3Orplq4 = secondLineKey.split("_")[1];
+		
+		if( ! this.plq3Orplq4.startsWith("plq"))
+			throw new Exception("No");
+		
+		this.sanVsSol = secondLineKey.split("_")[0];
+		
+		//if( ! this.sanVsSol.equals(firstLineKey.split("-")[1]))
+			//	throw new Exception("No");
+	}
+	
+	public String getSanVsSol()
+	{
+		return sanVsSol;
+	}
+
+	public String getPlq3Orplq4()
+	{
+		return plq3Orplq4;
+	}
+
+	public String getrNumber()
+	{
+		return rNumber;
+	}
+
 	private static HashMap<String, MetadataFileLine> getMetaMap() throws Exception
 	{
 		HashMap<String, MetadataFileLine> map = new HashMap<String, MetadataFileLine>();
@@ -25,7 +62,10 @@ public class MetadataFileLine
 		
 		for( int x=1; x < firstSplits.length; x++)
 		{
-			System.out.println(firstSplits[x] + " " + secondSplits[x]);
+			if( map.containsKey(firstSplits[x]))
+				throw new Exception("Duplicate key");
+			
+			map.put(firstSplits[x], new MetadataFileLine(firstSplits[x],secondSplits[x]));
 		}
 		
 		reader.close();
@@ -36,5 +76,11 @@ public class MetadataFileLine
 	public static void main(String[] args) throws Exception
 	{
 		HashMap<String, MetadataFileLine> map = getMetaMap();
+		
+		for(String s: map.keySet())
+		{
+			MetadataFileLine mfl = map.get(s);
+			System.out.println(s + " " +mfl.getPlq3Orplq4() + " " + mfl.getSanVsSol() + " " + mfl.getrNumber());
+		}
 	}
 }
