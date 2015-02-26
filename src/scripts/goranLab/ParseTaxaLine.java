574dd35c6f7bcce7ab637534e92e8848be90d487
Anthony Fodor <anthony.fodor@gmail.com>
2015-02-25 21:06:36 -0500

    towards writing levels...

---
+++ src/scripts/goranLab/ParseTaxaLine.java
@@ -0,0 +1,39 @@
+package scripts.goranLab;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.util.HashMap;
+import java.util.StringTokenizer;
+
+import utils.ConfigReader;
+
+public class ParseTaxaLine
+{
+	public static HashMap<String, String> getTaxaMap(int level) throws Exception
+	{
+		HashMap<String, String> map = new HashMap<String, String>();
+		
+		BufferedReader reader = new BufferedReader(new FileReader(new File(
+				ConfigReader.getGoranTrialDir() + File.separator 
+				+ "OTU_Front_DATA Serum Solar Sano 02-20-2015.txt")));
+		
+		reader.readLine();  reader.readLine();
+		
+		for(String s = reader.readLine(); s != null; s = reader.readLine())
+		{
+			String[] splits = s.split("\t");
+		
+			//System.out.println(splits[2]);
+			String familyString =  splits[2].split(";")[level];
+			familyString = new StringTokenizer(familyString, "(").nextToken().replaceAll("\"", "").trim();
+			
+			if( map.containsKey(splits[0]))
+				throw new Exception("No");
+			
+			map.put(splits[0], familyString);
+		}
+		
+		return map;
+	}
+}
