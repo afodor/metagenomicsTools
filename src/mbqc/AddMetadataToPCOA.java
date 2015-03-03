c76e9dbe079d8c71ef1583a9fb17455ee3bd550f
afodor <anthony.fodor@gmail.com>
2015-03-03 17:59:49 -0500

    attempt to merge metadata into PCOA

--- src/mbqc/AddMetadataToPCOA.java
+++ src/mbqc/AddMetadataToPCOA.java
@@ -1,9 +1,12 @@
 package mbqc;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.util.HashMap;
+import java.util.StringTokenizer;
 
 import utils.ConfigReader;
 
@@ -20,19 +23,49 @@ public class AddMetadataToPCOA
 				"alpha-beta-div" + File.separator +  "beta-div" + File.separator + "attic" + 
 						File.separator + "pcoa-qiime.txt")));
 		
+		BufferedWriter writer = new BufferedWriter(new FileWriter(new File(
+				ConfigReader.getMbqcDir() + File.separator + 
+				 File.separator +  "dropbox" + File.separator + 
+				"alpha-beta-div" + File.separator +  "beta-div" + File.separator + "attic" + 
+						File.separator + "pcoa-qiimeWithMetadata.txt"
+					)));
+		
+		writer.write("fullID\tinformaticsToken\tobscuredToken\tnumberToken\textractionWetlab\tsequencingWetlab\t");
+		writer.write(reader.readLine() + "\n");
+		
 		int numFound =0;
 		int numMissed=0;
 		
 		for(String s= reader.readLine();  s !=null; s = reader.readLine())
 		{
-			System.out.println(s);
 			String[] splits = s.split("\t");
 			
 			if( metaMap.containsKey(splits[0]))
+			{
 				numFound++;
+				writer.write(splits[0] + "\t");
+				
+				StringTokenizer innerSplits = new StringTokenizer(splits[0], ".");
+				writer.write(innerSplits.nextToken() + "\t" + innerSplits.nextToken() + "\t" + 
+						innerSplits.nextToken()  + "\t");
+				
+				if( innerSplits.hasMoreTokens())
+					throw new Exception("No");
+				
+				RawDesignMatrixParser rdmp = metaMap.get(splits[0]);
+				writer.write(rdmp.getExtractionWetlab() + "\t" + rdmp.getSequecingWetlab() + "\t");
+				writer.write(splits[1] + "\t" + splits[2] + "\n");
+				
+				if( splits.length != 3)
+					throw new Exception("No");
+			}
 			else
+			{
 				numMissed++;
+			}
 		}
+		
+		writer.flush();  writer.close();
 		
 		System.out.println(numFound + " " + numMissed);
 	}
