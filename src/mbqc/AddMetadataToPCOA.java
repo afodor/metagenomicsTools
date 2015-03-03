410a087f5b2638eb97c601ad363d4d08803a9632
afodor <anthony.fodor@gmail.com>
2015-03-03 17:40:46 -0500

    stub for writing metadata to PCOA

--- src/mbqc/AddMetadataToPCOA.java
+++ src/mbqc/AddMetadataToPCOA.java
@@ -3,6 +3,7 @@ package mbqc;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
+import java.util.HashMap;
 
 import utils.ConfigReader;
 
@@ -10,15 +11,29 @@ public class AddMetadataToPCOA
 {
 	public static void main(String[] args) throws Exception
 	{
+		HashMap<String, RawDesignMatrixParser> metaMap = 
+				RawDesignMatrixParser.getByFullId();
+		
 		BufferedReader reader = new BufferedReader(new FileReader(new File(
 				ConfigReader.getMbqcDir() + File.separator + 
 				 File.separator +  "dropbox" + File.separator + 
 				"alpha-beta-div" + File.separator +  "beta-div" + File.separator + "attic" + 
 						File.separator + "pcoa-qiime.txt")));
 		
+		int numFound =0;
+		int numMissed=0;
+		
 		for(String s= reader.readLine();  s !=null; s = reader.readLine())
 		{
 			System.out.println(s);
+			String[] splits = s.split("\t");
+			
+			if( metaMap.containsKey(splits[0]))
+				numFound++;
+			else
+				numMissed++;
 		}
+		
+		System.out.println(numFound + " " + numMissed);
 	}
 }
