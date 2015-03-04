6fd22f33d24760b482925dbff5705902321f6e3a
afodor <anthony.fodor@gmail.com>
2015-03-04 15:32:48 -0500

    point metadata code to mds1 and mds 2

--- src/mbqc/AddMetadataToPCOA.java
+++ src/mbqc/AddMetadataToPCOA.java
@@ -20,18 +20,18 @@ public class AddMetadataToPCOA
 		BufferedReader reader = new BufferedReader(new FileReader(new File(
 				ConfigReader.getMbqcDir() + File.separator + 
 				 File.separator +  "dropbox" + File.separator + 
-				"alpha-beta-div" + File.separator +  "beta-div" + File.separator + "attic" + 
-						File.separator + "pcoa-qiime.txt")));
+				"alpha-beta-div" + File.separator +  "beta-div" +
+						File.separator + "mdsOut.txt")));
 		
 		BufferedWriter writer = new BufferedWriter(new FileWriter(new File(
 				ConfigReader.getMbqcDir() + File.separator + 
 				 File.separator +  "dropbox" + File.separator + 
-				"alpha-beta-div" + File.separator +  "beta-div" + File.separator + "attic" + 
-						File.separator + "pcoa-qiimeWithMetadata.txt"
+				"alpha-beta-div" + File.separator +  "beta-div" +
+				 File.separator + "mdsOutWithMetadata.txt"
 					)));
 		
-		writer.write("fullID\tinformaticsToken\tobscuredToken\tnumberToken\textractionWetlab\tsequencingWetlab\t");
-		writer.write("pcoa1\tpcoa2\n");
+		writer.write("fullID\tinformaticsToken\tobscuredToken\tnumberToken\textractionWetlab\tsequencingWetlab\tmbqcID\t");
+		writer.write("mds1\tmds2\n");
 		
 		int numFound =0;
 		int numMissed=0;
@@ -53,7 +53,7 @@ public class AddMetadataToPCOA
 					throw new Exception("No");
 				
 				RawDesignMatrixParser rdmp = metaMap.get(splits[0]);
-				writer.write(rdmp.getExtractionWetlab() + "\t" + rdmp.getSequecingWetlab() + "\t");
+				writer.write(rdmp.getExtractionWetlab() + "\t" + rdmp.getSequecingWetlab() + "\t" + rdmp.getMbqcID() + "\t");
 				writer.write(splits[1] + "\t" + splits[2] + "\n");
 				
 				if( splits.length != 3)
