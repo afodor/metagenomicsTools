8723627e4713cdca7162688c4b1a0fc89d1480c0
afodor <anthony.fodor@gmail.com>
2015-03-24 13:13:54 -0400

    pair by diversity...

--- src/mbqc/PairByDiversity.java
+++ src/mbqc/PairByDiversity.java
@@ -1,13 +1,16 @@
 package mbqc;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.List;
 
+import utils.Avevar;
 import utils.ConfigReader;
 
 public class PairByDiversity
@@ -48,8 +51,38 @@ public class PairByDiversity
 		return map;
 	}
 	
+	private static void writePairs( HashMap<String, List<Double>> map) throws Exception
+	{
+		BufferedWriter writer = new BufferedWriter(new FileWriter(new File(		
+				ConfigReader.getMbqcDir() + File.separator + 
+		 File.separator +  "fromGaleb" + File.separator + "simpsonsPaired.txt")));
+		
+		writer.write("informaticsID\textractionID\tsequencingID\tmbqcID\textractionsimpson_reciprocal\tshippedsimpson_reciprocal\n");
+		
+		for(String s : map.keySet())
+		{
+			String[] splits = s.split("_");
+			//informatics_extraction_sequencing_mbqcID
+			
+			if( !splits[1].equals("NA"))
+			{
+				// find the pair
+				List<Double> match = map.get(splits[0] + "_NA_" + splits[2] +"_" + splits[3]);
+				
+				if( match != null)
+				{
+					writer.write(splits[0] + "\t" + splits[1] + "\t" + splits[2] + "\t" + splits[3] + "\t" + 
+								new Avevar(map.get(s)).getAve() + "\t" + new Avevar(match).getAve() + "\n");
+				}
+			}
+		}
+		
+		writer.flush();  writer.close();
+	}
+	
 	public static void main(String[] args) throws Exception
 	{
 		 HashMap<String, List<Double>> map = getDiversities();
+		 writePairs(map);
 	}
 }
