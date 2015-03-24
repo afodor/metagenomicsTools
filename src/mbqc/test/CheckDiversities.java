b92b8d0d1c0c29452a1f9156d233c56f39c9f53a
afodor <anthony.fodor@gmail.com>
2015-03-24 12:25:32 -0400

    diversities passing...

--- src/mbqc/test/CheckDiversities.java
+++ src/mbqc/test/CheckDiversities.java
@@ -11,13 +11,45 @@ public class CheckDiversities
 {
 	public static void main(String[] args) throws Exception
 	{
-		HashMap<String, Double> divMap = getDiversities();
+		HashMap<String, String> divMap = getDiversities();
 		
+		BufferedReader reader = new BufferedReader(new FileReader(new File(
+				ConfigReader.getMbqcDir() + File.separator + 
+				 File.separator +  "fromGaleb" + File.separator + 
+				 "merged-final-unrarefiedplusMetadata.txt")));
+		
+		reader.readLine();
+		
+		for(String s= reader.readLine(); s != null; s= reader.readLine())
+		{
+			String[] splits = s.split("\t");
+			
+			String expected = divMap.get(splits[0]);
+			
+			if( expected == null)
+				throw new Exception("No " + splits[0]);
+			
+			if( expected.equals("nan"))
+			{
+				if( !expected.equals(splits[10]))
+					throw new Exception("No");
+			}
+			else
+			{
+				double diff = Math.abs( Double.parseDouble(expected) - Double.parseDouble(splits[10]));
+				if( diff > 0.00000000000000001)
+						throw new Exception("No");
+			}
+			
+
+		}
+		
+		System.out.println("Passed");
 	}
 	
-	private static HashMap<String, Double> getDiversities() throws Exception
+	private static HashMap<String, String> getDiversities() throws Exception
 	{
-		HashMap<String, Double> map = new HashMap<String, Double>();
+		HashMap<String, String> map = new HashMap<String, String>();
 		
 		BufferedReader reader = new BufferedReader(new FileReader(new File(
 				ConfigReader.getMbqcDir() + File.separator + 
@@ -29,12 +61,7 @@ public class CheckDiversities
 		for(String s= reader.readLine(); s != null; s= reader.readLine())
 		{
 			String[] splits = s.split("\t");
-			
-			if( map.containsKey(splits[0]))
-				throw new Exception("No");
-			
-			if( ! splits[1].equals("nan"))
-				map.put(splits[0], Double.parseDouble(splits[1]));
+			map.put(splits[0], splits[1]);
 		}
 		
 		return map;
