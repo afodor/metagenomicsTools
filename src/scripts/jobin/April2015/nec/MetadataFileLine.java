f2c98387df0b4090fea5280bb62bb05acef59487
Anthony Fodor <anthony.fodor@gmail.com>
2015-05-14 21:02:00 -0400

    add case/control string...

---
+++ src/scripts/jobin/April2015/nec/MetadataFileLine.java
@@ -0,0 +1,62 @@
+package scripts.jobin.April2015.nec;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.util.HashMap;
+
+import utils.ConfigReader;
+
+public class MetadataFileLine
+{
+	private final String rgSampleID;
+	private final String caseControlString;
+	
+	public String getRgSampleID()
+	{
+		return rgSampleID;
+	}
+
+	public String getCaseControlString()
+	{
+		return caseControlString;
+	}
+	
+	private MetadataFileLine(String s) throws Exception
+	{
+		String[] splits = s.split("\t");
+		
+		this.rgSampleID = splits[0];
+		this.caseControlString = splits[5];
+	}
+	
+	public static HashMap<String, MetadataFileLine> getMap() throws Exception
+	{
+		HashMap<String, MetadataFileLine> map= new HashMap<String, MetadataFileLine>();
+		
+		BufferedReader reader = new BufferedReader(new FileReader(new File(
+			ConfigReader.getJobinApril2015Dir() + File.separator + "nec_map.txt"	)));
+		
+		reader.readLine();
+		
+		for(String s= reader.readLine(); s != null; s =reader.readLine())
+		{
+			MetadataFileLine mfl = new MetadataFileLine(s);
+			
+			if( map.containsKey(mfl.rgSampleID) )
+				throw new Exception("No");
+			
+			map.put(mfl.rgSampleID, mfl);
+		}
+		
+		return map;
+	}
+	
+	public static void main(String[] args) throws Exception
+	{
+		HashMap<String, MetadataFileLine> map =getMap();
+		
+		for(String s : map.keySet())
+			System.out.println(s + " " + map.get(s).caseControlString);
+	}
+}
