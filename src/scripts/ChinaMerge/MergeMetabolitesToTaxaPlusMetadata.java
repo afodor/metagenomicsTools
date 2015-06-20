d67bd4951cbdb6b2e683d035efc02c5dceca4a2d
afodor <anthony.fodor@gmail.com>
2015-06-20 13:23:25 -0400

    towards merging metabolites and microbes

--- src/scripts/ChinaMerge/MergeMetabolitesToTaxaPlusMetadata.java
+++ src/scripts/ChinaMerge/MergeMetabolitesToTaxaPlusMetadata.java
@@ -4,6 +4,7 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 import utils.ConfigReader;
@@ -22,6 +23,28 @@ public class MergeMetabolitesToTaxaPlusMetadata
 		
 		for(int x=2; x < topSplits.length; x++)
 			metaboliteNames.add(topSplits[x]);
+		
+		HashMap<Integer, List<Double>> map = new HashMap<Integer, List<Double>>();
+		
+		for(String s = reader.readLine(); s != null; s = reader.readLine())
+		{
+			String[] splits = s.split("\t");
+			
+			Integer key = Integer.parseInt(splits[0]);
+			
+			if( map.containsKey(key))
+				throw new Exception("No");
+			
+			List<Double> list = new ArrayList<Double>();
+			
+			map.put(key,list);
+			
+			for(int x=2; x < splits.length; x++)
+				list.add(Double.parseDouble(splits[x]));
+			
+			if( list.size() != metaboliteNames.size())
+				throw new Exception("No");
+		}
 		
 		reader.close();
 	}
