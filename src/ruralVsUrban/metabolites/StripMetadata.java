8447aa7bca2f7b447eed159851ed92532977d815
afodor <anthony.fodor@gmail.com>
2015-06-22 15:47:28 -0400

    towards adding subject metadata...

--- src/ruralVsUrban/metabolites/StripMetadata.java
+++ src/ruralVsUrban/metabolites/StripMetadata.java
@@ -12,6 +12,28 @@ import utils.TabReader;
 
 public class StripMetadata
 {
+	public static List<String> getNames() throws Exception
+	{
+		BufferedReader reader = new BufferedReader(new FileReader(
+				new File(ConfigReader.getChinaDir() + File.separator + 
+						"metabolites" + File.separator + 
+						"for_anthony.txt")));
+		
+		String[] splits = reader.readLine().split("\t");
+		
+		int startIndex = getIndex(splits, "As_water");
+		int endIndex = getIndex( splits, "As_Toenail");
+		
+		List<String> list = new ArrayList<String>();
+	
+		for( int x=startIndex; x <= endIndex; x++)
+			list.add(splits[x]);
+		
+		reader.close();
+		
+		return list;
+	}
+	
 	public static HashMap<Integer, List<Double>> getMap() throws Exception
 	{
 		HashMap<Integer, List<Double>> map = new HashMap<Integer, List<Double>>();
@@ -65,6 +87,7 @@ public class StripMetadata
 			}
 		}
 		
+		reader.close();
 		return map;
 	}
 	
@@ -80,6 +103,16 @@ public class StripMetadata
 	
 	public static void main(String[] args) throws Exception
 	{
-		getMap();
+		List<String> list = getNames();
+		System.out.println(list);
+		HashMap<Integer, List<Double>> map=  getMap();
+		System.out.println(map);
+		
+		for(Integer i : map.keySet())
+		{
+			if( map.get(i).size() != list.size())
+				throw new Exception("No");
+		}
+		
 	}
 }
