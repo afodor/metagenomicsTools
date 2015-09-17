c91657d888c4f007b841937b61c03affd72b375e
afodor <anthony.fodor@gmail.com>
2015-09-17 15:16:42 -0400

    towards splitting files

--- src/scripts/TopeSeptember2015Run/CompareForwardBackwardPrimers.java
+++ src/scripts/TopeSeptember2015Run/CompareForwardBackwardPrimers.java
@@ -2,10 +2,15 @@ package scripts.TopeSeptember2015Run;
 
 import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileReader;
+import java.io.InputStreamReader;
 import java.util.HashSet;
+import java.util.zip.GZIPInputStream;
 
+import parsers.FastQ;
 import utils.ConfigReader;
+import utils.Translate;
 
 public class CompareForwardBackwardPrimers
 {
@@ -24,14 +29,57 @@ public class CompareForwardBackwardPrimers
 		{
 			String[] splits = s.split("\t");
 			
-			set1.add( splits[4] );
-			set2.add( splits[9]);
+			set1.add( Translate.reverseTranscribe(splits[4]));
+			set2.add( splits[9]);	
 		}
+		
+		reader.close();
 		
 		System.out.println(set1);
 		System.out.println(set2);
 		
+		File file1 = new File(ConfigReader.getTopeSep2015Dir() + File.separator + 
+				"Humphries_R2_001.fastq.gz" );
+		findHits(set1, file1);
+		
+		System.out.println("FILE 2");
+		
+		File file2 = new File(ConfigReader.getTopeSep2015Dir() + File.separator + 
+				"Humphries_R3_001.fastq.gz" );
+		findHits(set2, file2);
+		
 		set1.retainAll(set2);
-		System.out.println(set1.size());
+		System.out.println("COMMON " + set1.size());
+	}
+	
+	private static void findHits( HashSet<String> primerSet, File file ) throws Exception
+	{
+		BufferedReader reader =
+				new BufferedReader(new InputStreamReader( 
+						new GZIPInputStream( new FileInputStream( file))));
+		long numHits =0;
+		long numSeqs=0;
+		
+		for( FastQ fastQ = FastQ.readOneOrNull(reader); fastQ != null;
+						fastQ = FastQ.readOneOrNull(reader))
+		{
+			numSeqs++;
+			
+			if( primerSet.contains(fastQ.getSequence()))
+			{
+				//System.out.println(fastQ.getSequence());
+				numHits++;
+				
+			}
+				
+			if( numSeqs % 1000000 == 0 )
+				System.out.println( numHits + " " + numSeqs + " " + ((double)numHits / numSeqs));
+				
+		}
+		
+		System.out.println( numHits + " " + numSeqs + " " + ((double)numHits / numSeqs));
+		
+		reader.close();
+		
 	}
 }
