d820b0d34031d55a346b372a1eecff516111c08e
afodor <anthony.fodor@gmail.com>
2015-09-17 16:10:48 -0400

    code to split

--- src/scripts/TopeSeptember2015Run/SplitSequences.java
+++ src/scripts/TopeSeptember2015Run/SplitSequences.java
@@ -1,12 +1,16 @@
 package scripts.TopeSeptember2015Run;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.FileReader;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.util.HashMap;
 import java.util.zip.GZIPInputStream;
+import java.util.zip.GZIPOutputStream;
 
 import parsers.FastQ;
 import utils.ConfigReader;
@@ -16,6 +20,7 @@ public class SplitSequences
 	public static void main(String[] args) throws Exception
 	{
 		HashMap<String, String> map = SampleMap.getPrimersToSampleMap();
+		HashMap<String, BufferedWriter> writerMap = new HashMap<String, BufferedWriter>();
 		
 		BufferedReader read1 = new BufferedReader(new InputStreamReader( 
 				new GZIPInputStream( new FileInputStream( ConfigReader.getTopeSep2015Dir() + 
@@ -32,10 +37,10 @@ public class SplitSequences
 						File.separator + 
 						"Humphries_R3_001.fastq.gz"))));
 		
-		BufferedReader read2 = new BufferedReader(new InputStreamReader( 
+		BufferedReader read4 = new BufferedReader(new InputStreamReader( 
 				new GZIPInputStream( new FileInputStream(ConfigReader.getTopeSep2015Dir() + 
 						File.separator + 
-						"Humphries_R1_001.fastq.gz"))));
+						"Humphries_R4_001.fastq.gz"))));
 		
 		long numRead =0;
 		long numAssigned =0;
@@ -45,7 +50,7 @@ public class SplitSequences
 			numRead++;
 			FastQ tag2FastQ = FastQ.readOneOrNull(tag2);
 			FastQ tag3FastQ = FastQ.readOneOrNull(tag3);
-			FastQ read2FastQ = FastQ.readOneOrNull(read2);
+			FastQ read4FastQ = FastQ.readOneOrNull(read4);
 			
 			if( ! read1FastQ.getFirstTokenOfHeader().equals(tag2FastQ.getFirstTokenOfHeader() ))
 					throw new Exception("No");
@@ -53,7 +58,7 @@ public class SplitSequences
 			if( ! read1FastQ.getFirstTokenOfHeader().equals(tag3FastQ.getFirstTokenOfHeader() ))
 					throw new Exception("No");
 			
-			if( ! read1FastQ.getFirstTokenOfHeader().equals(read2FastQ.getFirstTokenOfHeader() ))
+			if( ! read1FastQ.getFirstTokenOfHeader().equals(read4FastQ.getFirstTokenOfHeader() ))
 					throw new Exception("No");
 			
 			String key = tag2FastQ.getSequence() + "@" + tag3FastQ.getSequence();
@@ -61,10 +66,27 @@ public class SplitSequences
 			if( map.containsKey(key))
 			{
 				numAssigned++;
+				
+				BufferedWriter writer1 = getOrCreate(map.get(key) + "_1", writerMap);
+				BufferedWriter writer4 = getOrCreate(map.get(key) + "_4", writerMap);
+				
+				writer1.write(">" + read1FastQ.getFirstTokenOfHeader() + "\n");
+				writer1.write(">" + read1FastQ.getSequence() + "\n");
+				writer1.flush();
+
+				writer4.write(">" + read4FastQ.getFirstTokenOfHeader() + "\n");
+				writer4.write(">" + read4FastQ.getSequence() + "\n");
+				writer4.flush();
 			}
 			
-			if( numRead % 1000 == 0 )
+			if( numRead % 10000 == 0 )
 				System.out.println(numRead + " " + numAssigned + " " + ((double)numAssigned/ numRead));
+		}
+		
+		for( String s : writerMap.keySet())
+		{
+			BufferedWriter writer = writerMap.get(s);
+			writer.flush();  writer.close();
 		}
 	
 		if( FastQ.readOneOrNull(tag2) != null)
@@ -74,8 +96,30 @@ public class SplitSequences
 		if( FastQ.readOneOrNull(tag3) != null)
 			throw new Exception("No");
 		
-
-		if( FastQ.readOneOrNull(read2) != null)
+		if( FastQ.readOneOrNull(read4) != null)
 			throw new Exception("No");
+		
+		System.out.println("finshed");
+		System.out.println(numRead + " " + numAssigned + " " + ((double)numAssigned/ numRead));
+		
+	}
+	
+	private static BufferedWriter getOrCreate( String name, HashMap<String, BufferedWriter> map )
+		throws Exception
+	{
+		BufferedWriter writer = map.get(name);
+		
+		if( writer == null)
+		{
+			writer = new BufferedWriter(
+					new OutputStreamWriter(new GZIPOutputStream(new FileOutputStream(
+						ConfigReader.getTopeSep2015Dir() + File.separator + 
+						"splitSamples" + File.separator + name+ ".fasta.gz"))));
+			
+			map.put(name, writer);
+		
+		}
+		
+		return writer;
 	}
 }
