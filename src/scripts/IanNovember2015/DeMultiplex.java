4360d48bdaa893c629f429dccb23e72713ff642e
Anthony Fodor <anthony.fodor@gmail.com>
2015-11-12 13:30:54 -0500

    barcode parser for 97 samples

--- src/scripts/IanNovember2015/DeMultiplex.java
+++ src/scripts/IanNovember2015/DeMultiplex.java
@@ -9,14 +9,39 @@ import utils.ConfigReader;
 
 public class DeMultiplex
 {
-	public static HashMap<String, String> getSampleID() throws Exception
+	private final String barcodeSequence;
+	private final int sampleID;
+	private final String description;
+	
+	private DeMultiplex(String s) throws Exception
 	{
-		HashMap<String, String> map = new HashMap<String,String>();
+		String[] splits = s.split("\t");
+		if(splits.length != 4)
+			throw new Exception("No");
+		
+		this.sampleID = Integer.parseInt(splits[0]);
+		this.barcodeSequence = splits[1];
+		this.description = splits[3];
+	}
+	
+	public static HashMap<String, DeMultiplex> getSampleID() throws Exception
+	{
+		HashMap<String, DeMultiplex> map = new HashMap<String,DeMultiplex>();
 		
 		BufferedReader reader = new BufferedReader(new FileReader(new File(
 					ConfigReader.getIanNov2015Dir() + File.separator + 
 						"MAP_HC_R1.txt")));
 		
+		reader.readLine();
+		for(String s = reader.readLine(); s != null; s = reader.readLine())
+		{
+			DeMultiplex dm = new DeMultiplex(s);
+			
+			if( map.containsKey(dm.barcodeSequence))
+				throw new Exception("No");
+			
+			map.put(dm.barcodeSequence, dm);
+		}
 		
 		reader.close();
 		
@@ -25,6 +50,7 @@ public class DeMultiplex
 	
 	public static void main(String[] args) throws Exception
 	{
-		HashMap<String, String> sampleIDs = getSampleID();
+		HashMap<String, DeMultiplex> sampleIDs = getSampleID();
+		System.out.println(sampleIDs.size());
 	}
 }
