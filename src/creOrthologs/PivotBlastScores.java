ff1f72d84c42d7d1607e302ded1e9042c7a27c9a
Anthony Fodor <anthony.fodor@gmail.com>
2015-11-22 20:42:32 -0500

    towards parser...

--- src/creOrthologs/PivotBlastScores.java
+++ src/creOrthologs/PivotBlastScores.java
@@ -2,8 +2,11 @@ package creOrthologs;
 
 import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileReader;
+import java.io.InputStreamReader;
 import java.util.HashMap;
+import java.util.zip.GZIPInputStream;
 
 import utils.ConfigReader;
 
@@ -14,31 +17,40 @@ public class PivotBlastScores
 		HashMap<String, Integer> counts = getCounts();
 	}
 	
+	private static String getOrthologKey(String filepath)
+	{
+		return filepath.substring(filepath.lastIndexOf("_")+1, filepath.indexOf(".txt.gz"));
+	}
+	
 	private static HashMap<String, Integer> getCounts() throws Exception
 	{
 		HashMap<String, Integer> map = new HashMap<String, Integer>();
 		
-		BufferedReader reader = new BufferedReader(new FileReader(new File(
-			ConfigReader.getCREOrthologsDir() + File.separator + "blastResults.txt"	)));
-		
+		BufferedReader reader = 
+			new BufferedReader(new InputStreamReader( 
+				new GZIPInputStream( new FileInputStream( 
+						ConfigReader.getCREOrthologsDir() + File.separator + "blastResults.txt.gz"))));
+						
+						
 		reader.readLine();
 		
 		int x=0;
 		
 		for(String s= reader.readLine() ; s != null; s = reader.readLine())
 		{
-			System.out.println(s);
+			//System.out.println(s);
 			String[] splits = s.split("\t");
+			String orthologKey = getOrthologKey(splits[0]);
 			
-			if( splits[1].indexOf("@") != -1 || splits[2].indexOf("@") != -1)
+			if( orthologKey.indexOf("@") != -1 || splits[3].indexOf("@") != -1)
 				throw new Exception("No");
 		
-			String key = splits[1] + "@" + splits[2];
+			String key = orthologKey + "@" + splits[3];
 			
 			if( map.containsKey(key))
 				throw new Exception("Duplicate " + key);
 			
-			String aVal = splits[3].substring(0, splits[3].indexOf("."));
+			String aVal = splits[4].substring(0, splits[4].indexOf("."));
 			
 			map.put(key, Integer.parseInt(aVal));
 			
