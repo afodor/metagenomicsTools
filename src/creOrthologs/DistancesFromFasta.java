4c49d2c6c7275c86c79621be1fa43f193ccc5432
afodor <anthony.fodor@gmail.com>
2015-11-25 21:48:51 -0500

    self to self...

--- src/creOrthologs/DistancesFromFasta.java
+++ src/creOrthologs/DistancesFromFasta.java
@@ -3,6 +3,8 @@ package creOrthologs;
 import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileWriter;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 import parsers.FastaSequence;
@@ -10,6 +12,27 @@ import utils.ConfigReader;
 
 public class DistancesFromFasta
 {
+	private static String getMergedLocation(String s1, String s2)
+	{
+		List<String> list = new ArrayList<String>();
+		list.add(s1);
+		list.add(s2);
+		Collections.sort(list);
+		
+		return list.get(0) + "_" + list.get(1);
+	}
+	
+	private static String getLocation(String s) throws Exception
+	{
+		String[] splits = s.split("_");
+		
+		for(int x=1; x < splits.length; x++)
+			if( Character.isDigit( splits[x].charAt(0)))
+				return splits[x-1];
+		
+		throw new Exception("Could not find " + s);
+	}
+	
 	public static void main(String[] args) throws Exception
 	{
 		List<FastaSequence> list = FastaSequence.readFastaFile(ConfigReader.getCREOrthologsDir() + File.separator + 
@@ -18,14 +41,18 @@ public class DistancesFromFasta
 		BufferedWriter writer = new BufferedWriter(new FileWriter(new File(ConfigReader.getCREOrthologsDir() + File.separator + 
 				"contig_7000000220927531_distances.txt")));
 		
-		writer.write("xFasta\tyFasta\tmismatchDistance\tgapToNonGap\tsumDistance\n");
+		writer.write("xFasta\tyFasta\txLocation\tyLocation\txLocation_yLocaiton\tmismatchDistance\tgapToNonGap\tsumDistance\n");
 		
 		for(int x=0;x < list.size() -1 ; x++)
+		{
+			System.out.println(x );
+			FastaSequence xSeq = list.get(x);
+			String xLocation = getLocation(xSeq.getHeader());
+			
 			for( int y=x+1; y < list.size(); y++)
 			{
-				System.out.println(x + " " + y);
-				FastaSequence xSeq = list.get(x);
 				FastaSequence ySeq = list.get(y);
+				String yLocation= getLocation(ySeq.getHeader());
 				
 				int numDiffs =0;
 				int numGapDiffs = 0;
@@ -50,10 +77,12 @@ public class DistancesFromFasta
 					}
 				}
 				
-				writer.write(xSeq.getHeader() + "\t" + ySeq.getHeader() + "\t" + numDiffs + "\t" + numGapDiffs + "\t" +
+				writer.write(xSeq.getHeader() + "\t" + ySeq.getHeader() + "\t" +
+						xLocation + "\t" + yLocation + "\t" + getMergedLocation(xLocation, yLocation)+ "\t" + 
+						numDiffs + "\t" + numGapDiffs + "\t" +
 									(numDiffs + numGapDiffs) + "\n");	
 			}
-		
+		}
 		
 		writer.flush();  writer.close();
 	}
