87d113613c22b4bf2bb398c4f10e2d0481269138
Anthony Fodor <anthony.fodor@gmail.com>
2015-12-28 19:35:29 -0500

    mds for new Goran data...

---
+++ src/scripts/GoranOct2015/MergePCRData.java
@@ -0,0 +1,15 @@
+package scripts.GoranOct2015;
+
+public class MergePCRData
+{
+	// 0 is OTU
+		// 1 is phylum
+		// 2 is class
+		// 3 is order
+		// 4 is family
+		// 5 is genus
+		public static void writeForALevel(int level) throws Exception
+		{
+			
+		}
+}
