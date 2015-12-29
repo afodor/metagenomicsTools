732d8ad774dac0a90ab00a7832d4dd292d389412
Anthony Fodor <anthony.fodor@gmail.com>
2015-12-29 04:34:01 -0500

    add metadata to taxa level data

--- src/scripts/GoranOct2015/MergePCRData.java
+++ src/scripts/GoranOct2015/MergePCRData.java
@@ -12,6 +12,58 @@ import utils.ConfigReader;
 
 public class MergePCRData
 {
+	private static void addALevel(String inFile, String outFile, boolean fromR,
+			HashMap<String, Double> pcrMap ) throws Exception
+	{
+
+		BufferedReader reader = new BufferedReader(new FileReader(new File(
+			inFile)));
+		
+		String[] topSplits = reader.readLine().split("\t");
+		
+		BufferedWriter writer = new BufferedWriter(new FileWriter(new File(
+				outFile)));
+		
+		writer.write("fullKey\tpartialKey\tcategory\tmeanCD");
+		
+		int startPos = fromR ? 0 : 1;
+		
+		for(int x= startPos; x < topSplits.length; x++)
+			writer.write("\t" + topSplits[x]);
+		
+		writer.write("\n");
+		
+		for(String s2 = reader.readLine(); s2 != null; s2 = reader.readLine())
+		{
+			String[] splits = s2.split("\t");
+			
+			String firstToken =splits[0].replaceAll("\"", ""); 
+			System.out.println(firstToken);
+			
+			StringTokenizer sToken = new StringTokenizer(firstToken,"_");
+			
+			String key = sToken.nextToken() + "_" + sToken.nextToken();
+			String condition = sToken.nextToken();
+			
+			Double val = pcrMap.get(key);
+			
+			if( val == null)
+				throw new Exception("Could not find " + key);
+			
+			writer.write(firstToken + "\t" + key  + "\t" + condition + "\t" + 
+							pcrMap.get(key));
+			
+			for( int x=1; x < splits.length; x++)
+				writer.write("\t" + splits[x]);
+			
+			writer.write("\n");
+		}
+		
+		writer.flush();  writer.close();
+		
+		reader.close();
+	}
+	
 	public static void main(String[] args) throws Exception
 	{
 		String[] levels = {"phylum","class","order","family","genus", "otu"};
@@ -20,52 +72,24 @@ public class MergePCRData
 		
 		for(String s : levels)
 		{
-			BufferedReader reader = new BufferedReader(new FileReader(new File(
-				ConfigReader.getGoranOct2015Dir() + File.separator + 
-					"mds_" + s + ".txt")));
+			String inFile = ConfigReader.getGoranOct2015Dir() + File.separator + 
+					"mds_" + s + ".txt";
 			
-			String[] topSplits = reader.readLine().split("\t");
+			String outFile = ConfigReader.getGoranOct2015Dir() + File.separator + 
+					"mds_" + s + "withMedata.txt";
 			
-			BufferedWriter writer = new BufferedWriter(new FileWriter(new File(
-					ConfigReader.getGoranOct2015Dir() + File.separator + 
-					"mds_" + s + "withMedata.txt")));
+			addALevel(inFile, outFile, true, pcrMap);
+		}
+		
+		for(String s : levels)
+		{
+			String inFile = ConfigReader.getGoranOct2015Dir() + File.separator + 
+					s+ "_asColumnsLogNorm.txt";
 			
-			writer.write("fullKey\tpartialKey\tcategory\tmeanCD");
+			String outFile = ConfigReader.getGoranOct2015Dir() + File.separator + 
+					s+ "_asColumnsLogNormWithMetadata.txt";
 			
-			for(String s2 : topSplits)
-				writer.write("\t" + s2);
-			
-			writer.write("\n");
-			
-			for(String s2 = reader.readLine(); s2 != null; s2 = reader.readLine())
-			{
-				String[] splits = s2.split("\t");
-				
-				String firstToken =splits[0].replaceAll("\"", ""); 
-				System.out.println(firstToken);
-				
-				StringTokenizer sToken = new StringTokenizer(firstToken,"_");
-				
-				String key = sToken.nextToken() + "_" + sToken.nextToken();
-				String condition = sToken.nextToken();
-				
-				Double val = pcrMap.get(key);
-				
-				if( val == null)
-					throw new Exception("Could not find " + key);
-				
-				writer.write(firstToken + "\t" + key  + "\t" + condition + "\t" + 
-								pcrMap.get(key));
-				
-				for( int x=1; x < splits.length; x++)
-					writer.write("\t" + splits[x]);
-				
-				writer.write("\n");
-			}
-			
-			writer.flush();  writer.close();
-			
-			reader.close();
+			addALevel(inFile, outFile, false, pcrMap);
 		}
 	}
 }
