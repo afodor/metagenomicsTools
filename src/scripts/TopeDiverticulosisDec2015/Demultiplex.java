4fd583bb5aaaa370dbfc93c07352d1f1b16e08f2
Anthony Fodor <anthony.fodor@gmail.com>
2015-12-29 21:35:59 -0500

    demutliplexing away...

--- src/scripts/TopeDiverticulosisDec2015/Demultiplex.java
+++ src/scripts/TopeDiverticulosisDec2015/Demultiplex.java
@@ -1,9 +1,11 @@
 package scripts.TopeDiverticulosisDec2015;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.InputStreamReader;
 import java.util.HashMap;
 import java.util.HashSet;
@@ -16,6 +18,23 @@ import utils.Translate;
 
 public class Demultiplex
 {
+	private static BufferedWriter getFromMap(HashMap<String, BufferedWriter> map, String key) throws Exception
+	{
+		BufferedWriter writer = map.get(key);
+		
+		if( writer == null)
+		{
+			writer = new BufferedWriter(new FileWriter(new File( 
+					ConfigReader.getTopeDiverticulosisDec2015Dir() + File.separator + 
+						"fastaOut" + File.separator + 
+					key + ".fasta")));
+			
+			map.put(key, writer);
+		}
+		
+		return writer;
+	}
+	
 	// forward@reverse is the key
 	private static HashMap<String, String> getBarcodeToSampleMap() throws Exception
 	{
@@ -74,6 +93,16 @@ public class Demultiplex
 			primer2Seqs.add(splits[1]);
 		}
 		
+		HashMap<String, BufferedWriter> writerMap = new HashMap<String, BufferedWriter>();
+
+		BufferedReader reader1 = 
+				new BufferedReader(new InputStreamReader( 
+						new GZIPInputStream( new FileInputStream(
+						ConfigReader.getTopeDiverticulosisDec2015Dir() + File.separator + 
+						"local" + File.separator +  "scratch" + File.separator  + "mhumphrys" + 
+								File.separator + 
+									"APJR3_20151123_M01994_IL100064365_NoIndex_L001_R1.fastq.gz"))));
+		
 		BufferedReader reader2 = 
 				new BufferedReader(new InputStreamReader( 
 						new GZIPInputStream( new FileInputStream(
@@ -90,6 +119,14 @@ public class Demultiplex
 								File.separator + 
 									"APJR3_20151123_M01994_IL100064365_NoIndex_L001_R3.fastq.gz"))));
 		
+		BufferedReader reader4 = 
+				new BufferedReader(new InputStreamReader( 
+						new GZIPInputStream( new FileInputStream(
+						ConfigReader.getTopeDiverticulosisDec2015Dir() + File.separator + 
+						"local" + File.separator +  "scratch" + File.separator  + "mhumphrys" + 
+								File.separator + 
+									"APJR3_20151123_M01994_IL100064365_NoIndex_L001_R1.fastq.gz"))));
+		
 		long numDone = 0;
 		long numMatched = 0;
 		for( FastQ fastq2 = FastQ.readOneOrNull(reader2); fastq2 != null;
@@ -97,13 +134,28 @@ public class Demultiplex
 		{
 			boolean gotOne = false;
 			
+			FastQ fastq1 = FastQ.readOneOrNull(reader1);
 			FastQ fastq3 =  FastQ.readOneOrNull(reader3);
+			FastQ fastq4 = FastQ.readOneOrNull(reader4);
 			
+
+			if( fastq1 == null)
+				throw new Exception("No");
+
 			if( fastq3 == null)
+				throw new Exception("No");
+			
+			if( fastq4 == null)
 				throw new Exception("No");
 			
 			if( ! fastq2.getFirstTokenOfHeader().equals(fastq3.getFirstTokenOfHeader()))
 					throw new Exception("No");
+			
+			if( ! fastq2.getFirstTokenOfHeader().equals(fastq1.getFirstTokenOfHeader()))
+				throw new Exception("No");
+			
+			if( ! fastq2.getFirstTokenOfHeader().equals(fastq4.getFirstTokenOfHeader()))
+				throw new Exception("No");
 			
 			for( String p3 : primer3Seqs)
 				if( fastq3.getSequence().startsWith( p3 ))
@@ -120,6 +172,21 @@ public class Demultiplex
 							if( id != null)
 							{
 								gotOne = true;
+								
+								BufferedWriter forwardWriter = 
+										getFromMap(writerMap, id +"_1");
+								
+								forwardWriter.write(fastq1.getFirstTokenOfHeader() + "\n");
+								forwardWriter.write(fastq1.getSequence() + "\n");
+								forwardWriter.flush();
+								
+								BufferedWriter backwardsWriter = 
+										getFromMap(writerMap, id+ "_4");
+								
+								backwardsWriter.write(fastq4.getFirstTokenOfHeader() + "\n");
+								backwardsWriter.write(fastq4.getSequence() + "\n");
+								backwardsWriter.flush();
+								
 							}
 								
 						}
@@ -138,7 +205,29 @@ public class Demultiplex
 						((double)numMatched / numDone));
 		}
 		
+		if( numDone % 10000 == 0)
+			System.out.println( numMatched + " " +  numDone + " " + 
+					((double)numMatched / numDone));
+		
 		if(  FastQ.readOneOrNull(reader3) != null )
 			throw new Exception("No");
+
+		if(  FastQ.readOneOrNull(reader1) != null )
+			throw new Exception("No");
+
+		if(  FastQ.readOneOrNull(reader4) != null )
+			throw new Exception("No");
+		
+		for( BufferedWriter writer : writerMap.values())
+		{
+			writer.flush();  writer.close();
+		}
+		
+		reader1.close();
+		reader2.close();
+		reader3.close();
+		reader4.close();
+		
+		System.out.println("Finished");
 	}
 }
