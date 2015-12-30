2f32250072a72507debfd370918d93e66be29785
Anthony Fodor <anthony.fodor@gmail.com>
2015-12-29 21:03:39 -0500

    towards demultiplex for Tope...

--- src/scripts/TopeDiverticulosisDec2015/Demultiplex.java
+++ src/scripts/TopeDiverticulosisDec2015/Demultiplex.java
@@ -3,7 +3,9 @@ package scripts.TopeDiverticulosisDec2015;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileReader;
 import java.io.InputStreamReader;
+import java.util.HashMap;
 import java.util.zip.GZIPInputStream;
 
 import parsers.FastQ;
@@ -11,8 +13,47 @@ import utils.ConfigReader;
 
 public class Demultiplex
 {
+	// forward@reverse is the key
+	private static HashMap<String, String> getBarcodeToSampleMap() throws Exception
+	{
+		HashMap<String, String> map = new HashMap<String,String>();
+		
+		BufferedReader reader = new BufferedReader(new FileReader(new File(
+			ConfigReader.getTopeDiverticulosisDec2015Dir() + File.separator + 
+						"Mapping file for Demux.txt")));
+		
+		reader.readLine();
+		
+		for(String s= reader.readLine(); s != null; s =reader.readLine())
+		{
+			String[] splits = s.split("\t");
+			
+			if( splits.length != 13)
+				throw new Exception("No");
+			
+			String id = splits[2];
+			
+			if(map.containsValue(id))
+				throw new Exception("No");
+			
+			String primerKey = splits[11] + "@" + splits[12];
+			
+			if( map.containsKey(primerKey))
+				throw new Exception("No");
+			
+			map.put(primerKey,id);
+		}
+		
+		return map;
+	}
+	
 	public static void main(String[] args) throws Exception
 	{
+		HashMap<String, String> primerMap = getBarcodeToSampleMap();
+		
+		for(String s : primerMap.keySet())
+			System.out.println(s + " "+ primerMap.get(s));
+		
 		BufferedReader reader2 = 
 				new BufferedReader(new InputStreamReader( 
 						new GZIPInputStream( new FileInputStream(
