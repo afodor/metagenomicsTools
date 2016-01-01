9104d1604337a373428757cd390dade0aa5e00c4
Anthony Fodor <anthony.fodor@gmail.com>
2016-01-01 11:50:34 -0500

    test demultiplex

--- src/scripts/IanNovember2015/test/TestDemultiplex.java
+++ src/scripts/IanNovember2015/test/TestDemultiplex.java
@@ -1,6 +1,8 @@
 package scripts.IanNovember2015.test;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
 import java.util.HashMap;
 import java.util.List;
 import java.util.StringTokenizer;
@@ -10,10 +12,76 @@ import utils.ConfigReader;
 
 public class TestDemultiplex
 {
+	private static HashMap<String, Integer> getBarcodeToSampleMap() throws Exception
+	{
+		HashMap<String, Integer> map = new HashMap<String,Integer>();
+		BufferedReader reader = new BufferedReader(new FileReader(new File(
+			ConfigReader.getIanNov2015Dir() + File.separator + 
+				"MAP_HC_R1.txt")));
+		
+		reader.readLine();
+		
+		for(String s = reader.readLine(); s != null; s = reader.readLine())
+		{
+			String[] splits = s.split("\t");
+			
+			if( map.containsKey(splits[1]))
+				throw new Exception("No");
+			
+			map.put(splits[1], Integer.parseInt(splits[0]));
+		}
+		
+		return map;
+	}
+	
 	public static void main(String[] args) throws Exception
 	{
+		HashMap<String, Integer> barcodeMap = getBarcodeToSampleMap();
 		HashMap<String, Integer> expectedMap = getExpectedSubjectID();
 		System.out.println("got " + expectedMap.size());
+		
+		BufferedReader reader = new BufferedReader(new FileReader(new File(
+			ConfigReader.getIanNov2015Dir() + File.separator + 
+			"behavbugs_noindex_l001_r2_001" + File.separator +  "mcsmm" + 
+					File.separator + "mcsmm.behavbugsn"	)));
+		
+		int numPassed = 0;
+		int numNotFound =0;
+		for(String s= reader.readLine(); s != null; s= reader.readLine())
+		{
+			String key = new StringTokenizer(reader.readLine()).nextToken();
+			String sequence = reader.readLine();
+			
+			Integer expectedFromBarcode = barcodeMap.get(sequence);
+			
+			if( expectedFromBarcode != null)
+			{
+				for( int x=0; x < 2; x++)
+					if( reader.readLine() == null)
+						throw new Exception("No");
+				
+				Integer expectedFromSeq = expectedMap.get(key);
+				
+				if(expectedFromSeq == null)
+					throw new Exception("Could not find " + key);
+				
+				if( ! expectedFromBarcode.equals(expectedFromSeq))
+					throw new Exception("Mismatch " + key + " " + expectedFromBarcode + " " + expectedFromSeq);
+				
+				numPassed++;
+				
+			}
+			else
+			{
+				numNotFound++;
+			}
+			
+			if( numPassed + numNotFound % 1000== 0)
+				System.out.println("passed " + numPassed + " skipped "+ numNotFound);
+		}
+				
+		System.out.println("passed " + numPassed + " skipped "+ numNotFound);
+		System.out.println("global pass");
 	}
 	
 	private static HashMap<String, Integer> getExpectedSubjectID() throws Exception
