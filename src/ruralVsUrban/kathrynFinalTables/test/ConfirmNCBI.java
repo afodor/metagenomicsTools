30c97da0394d80c51beeb0f80b5b30a80a01ce3f
Anthony Fodor <anthony.fodor@gmail.com>
2016-02-09 16:19:44 -0500

    ncbi table passing

--- src/ruralVsUrban/kathrynFinalTables/test/ConfirmNCBI.java
+++ src/ruralVsUrban/kathrynFinalTables/test/ConfirmNCBI.java
@@ -5,6 +5,7 @@ import java.io.File;
 import java.io.FileReader;
 import java.util.HashMap;
 
+
 import utils.ConfigReader;
 
 public class ConfirmNCBI
@@ -21,12 +22,90 @@ public class ConfirmNCBI
 				throw new Exception("Failed");
 		
 		System.out.println("Map comparison passed");
+		
+		checkColumns(8, 5);
 	}
 	
 	private static class Holder
 	{
 		String target;
 		double bitScore;
+	}
+	
+	private static void checkColumns(int kwColumns, int outputColumn) throws Exception
+	{
+		HashMap<String, Double> kwVals = new HashMap<String, Double>();
+		
+		BufferedReader reader = new BufferedReader(new FileReader(new File(
+				ConfigReader.getChinaDir() + File.separator + 
+				"Kathryn_update_NCBI_MostWanted" + File.separator + "otuModel_pValues_otu.txt"
+				)));
+		
+		reader.readLine();
+		
+		for(String s = reader.readLine(); s != null; s= reader.readLine())
+		{
+			String[] splits =s.split("\t");
+			
+			if( splits.length != 17)
+				throw new Exception("No");
+			
+			if( kwVals.containsKey(splits[0].replaceAll("\"", "").replace("X", "Consensus")))
+				throw new Exception("Fail");
+			
+			if( splits[0].startsWith("\"X"))
+			kwVals.put(splits[0].replaceAll("\"", "").replace("X", "Consensus"),
+					Double.parseDouble(splits[kwColumns]));
+		}
+		
+		reader.close();
+		
+		HashMap<String, Double> outputVals = new HashMap<String, Double>();
+		
+		reader = new BufferedReader(new FileReader(new File(
+				ConfigReader.getChinaDir() + File.separator + 
+				"Kathryn_update_NCBI_MostWanted" + File.separator + 
+					"ncbiRuralVsUrban.txt"
+				)));
+		
+		reader.readLine();
+		
+		for(String s = reader.readLine(); s != null; s= reader.readLine())
+		{
+			String[] splits =s.split("\t");
+			
+			if( splits.length != 8)
+				throw new Exception("No");
+			
+			if( Boolean.parseBoolean(splits[7]) && 
+					Double.parseDouble(splits[3]) > Double.parseDouble(splits[2]))
+				throw new Exception("Fail");
+			
+
+			if( !Boolean.parseBoolean(splits[7]) && 
+					Double.parseDouble(splits[2]) > Double.parseDouble(splits[3]))
+				throw new Exception("Fail");
+			
+			if(outputVals.containsKey(splits[0]))
+				throw new Exception("Fail");
+			
+			outputVals.put(splits[0],Double.parseDouble(splits[outputColumn]));
+		}
+		System.out.println("got " + kwVals.size() + " " + outputVals.size());
+		
+		if( kwVals.size() != outputVals.size())
+			throw new Exception("Fail");
+		
+		reader.close();
+		
+		if( ! kwVals.keySet().equals(outputVals.keySet()))
+			throw new Exception("Fail");
+		
+		for(String s : kwVals.keySet())
+			if( Math.abs(kwVals.get(s)- outputVals.get(s))> 0.001)
+				throw new Exception("Fail");
+		
+		System.out.println("Pass column comparison");
 	}
 	
 	private static HashMap<String, String> getFullTopHit() throws Exception
