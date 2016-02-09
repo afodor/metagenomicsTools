c34803271fa7cac920f8081e1223231cf0b66ba9
Anthony Fodor <anthony.fodor@gmail.com>
2016-02-09 15:56:37 -0500

    initial map comparison...

--- src/ruralVsUrban/kathrynFinalTables/test/ConfirmNCBI.java
+++ src/ruralVsUrban/kathrynFinalTables/test/ConfirmNCBI.java
@@ -12,6 +12,59 @@ public class ConfirmNCBI
 	public static void main(String[] args) throws Exception
 	{
 		HashMap<String, String> expectedMap = getExpectedBestVal();
+		System.out.println("Got " + expectedMap.size());
+		HashMap<String, String> reparsedMap = getFullTopHit();
+		System.out.println("Got " + reparsedMap.size());
+		
+		for(String s : expectedMap.keySet())
+			if( ! reparsedMap.get(s).equals(expectedMap.get(s)))
+				throw new Exception("Failed");
+		
+		System.out.println("Map comparison passed");
+	}
+	
+	private static class Holder
+	{
+		String target;
+		double bitScore;
+	}
+	
+	private static HashMap<String, String> getFullTopHit() throws Exception
+	{
+		HashMap<String, Holder> holderMap = new HashMap<String,Holder>();
+		
+		BufferedReader reader = new BufferedReader(new FileReader(
+				ConfigReader.getChinaDir() + File.separator + 
+		"Kathryn_update_NCBI_MostWanted" + File.separator + 
+			"allForwardToNCBI_Oct15.txt"));
+		
+		for(String s= reader.readLine(); s != null; s = reader.readLine())
+		{
+			String[] splits = s.split("\t");
+			
+			if( splits.length != 12)
+				throw new Exception("No");
+			
+			Holder h = holderMap.get(splits[0]);
+			
+			if( h == null || Double.parseDouble(splits[11].trim()) > h.bitScore)
+			{
+				h = new Holder();
+				h.target = splits[1];
+				h.bitScore = Double.parseDouble(splits[11].trim());
+				holderMap.put( splits[0], h );
+			}
+		}
+		
+		reader.close();
+		
+		HashMap<String, String> map = new HashMap<String,String>();
+		
+		for(String s : holderMap.keySet())
+			map.put(s, holderMap.get(s).target);
+			
+		
+		return map;
 	}
 	
 	private static HashMap<String, String> getExpectedBestVal() throws Exception
@@ -22,8 +75,6 @@ public class ConfirmNCBI
 				ConfigReader.getChinaDir() + File.separator + 
 				"Kathryn_update_NCBI_MostWanted" + File.separator + 
 						"ncbiRuralVsUrban.txt")));
-		
-		
 		
 		reader.readLine();
 		
@@ -36,6 +87,8 @@ public class ConfirmNCBI
 			
 			map.put(splits[0], splits[1]);
 		}
+		
+		reader.close();
 		return map;
 	}
 }
