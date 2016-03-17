d9248f127d69161390b367b86a00d51dcfa690dc
afodor <anthony.fodor@gmail.com>
2016-03-17 18:32:25 -0400

    first attempt at finding chunks..

--- src/creOrthologs/kmers/chunk/FindChunks.java
+++ src/creOrthologs/kmers/chunk/FindChunks.java
@@ -1,8 +1,10 @@
 package creOrthologs.kmers.chunk;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
@@ -11,15 +13,44 @@ import utils.ConfigReader;
 
 public class FindChunks
 {
-	//public static 
+	public static final double INITIATION_THRESHOLD = 0.85f;
+	public static final double EXTENSION_THRESHOLD = 0.9f;
 	
 	public static void main(String[] args) throws Exception
 	{
 		List<Holder> list = getList();
+		List<ChunkHolder> chunks = getChunks(list);
+		writeChunks(chunks);
+ 	}
+	
+	private static boolean positionIsInChunk(List<ChunkHolder> list, int position)
+	{
+		for(ChunkHolder ch : list)
+			if( ch.firstStart >= position && ch.lastStart <= position)
+				return true;
 		
-		for(Holder h : list)
-			System.out.println(h.startPos + " " + h.spearman);
+		return false;
 	}
+	
+	private static void writeChunks( List<ChunkHolder> list ) throws Exception
+	{
+		BufferedWriter writer = new BufferedWriter(new FileWriter(new File(
+			ConfigReader.getCREOrthologsDir() + File.separator + 
+			"pneuOnlyChunks_" + INITIATION_THRESHOLD + "_" + EXTENSION_THRESHOLD + ".txt")));
+		
+		writer.write("InitialStart\tEndStart\tlength\taverage\n");
+		
+		for(ChunkHolder ch : list)
+		{
+			writer.write(ch.firstStart + "\t");
+			writer.write(ch.lastStart + "\t");
+			writer.write(ch.n + "\t");
+			writer.write( ch.spearmanSum / ch.n + "\t");
+		}
+		
+		writer.flush();  writer.close();
+	}
+	
 	
 	private static List<Holder> getList() throws Exception
 	{
@@ -52,7 +83,7 @@ public class FindChunks
 	{
 		private int firstStart;
 		private int lastStart;
-		double pearsonSum =0;
+		double spearmanSum =0;
 		int n=0;
 	}
 	
@@ -69,11 +100,49 @@ public class FindChunks
 			Holder thisHolder = list.get(lastIndex);
 			if( currentChunk == null)
 			{
-				
+				if(thisHolder.spearman <= INITIATION_THRESHOLD)
+				{
+					currentChunk = new ChunkHolder();
+					chunks.add(currentChunk);
+					currentChunk.firstStart = thisHolder.startPos;
+					currentChunk.lastStart = thisHolder.startPos;
+					currentChunk.n = 1;
+					currentChunk.spearmanSum += thisHolder.spearman;
+					
+					int lookBack = lastIndex -1;
+					
+					boolean stop = false;
+					while(lookBack > 0 && ! stop)
+					{
+						Holder previous = list.get(lookBack);
+						
+						if( previous.spearman <= EXTENSION_THRESHOLD && 
+								! positionIsInChunk(chunks, previous.startPos))
+						{
+							currentChunk.firstStart = previous.startPos;
+							currentChunk.n = currentChunk.n + 1;
+							currentChunk.spearmanSum += thisHolder.spearman;
+							lookBack--;
+						}
+						else
+						{
+							stop = true;
+						}
+					}
+				}
 			}
 			else
 			{
-				
+				if( thisHolder.spearman <= EXTENSION_THRESHOLD)
+				{
+					currentChunk.lastStart = thisHolder.startPos;
+					currentChunk.n = currentChunk.n + 1;
+					currentChunk.spearmanSum+= thisHolder.spearman;
+				}
+				else
+				{
+					currentChunk = null;
+				}
 			}
 			
 			lastIndex++;
