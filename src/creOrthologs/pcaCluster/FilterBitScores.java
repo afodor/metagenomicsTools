58498bb3d85d88dfe8eed40ecc4aac73bfed5e75
Anthony Fodor <anthony.fodor@gmail.com>
2016-06-24 12:54:12 -0400

    r script for PCA

--- src/creOrthologs/pcaCluster/FilterBitScores.java
+++ src/creOrthologs/pcaCluster/FilterBitScores.java
@@ -6,20 +6,70 @@ import java.io.File;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
-import java.util.Random;
 
 import utils.ConfigReader;
 
 public class FilterBitScores
 {
+	public static final int MIN_NUM = 5;
+	
+	private static List<Boolean> getIncludeList( File inFile ) throws Exception
+	{
+		List<Float> list = new ArrayList<Float>();
+		
+		BufferedReader reader =new BufferedReader(new FileReader(inFile));
+		
+		String[] splits = reader.readLine().split("\t");
+		
+		for( int x=0;x  < splits.length; x++) 
+			list.add(0.0f);
+		
+		for(String s= reader.readLine(); s != null; s = reader.readLine())
+		{
+			splits = s.split("\t");
+			
+			if( splits[0].indexOf("pneu") != -1 )
+			{
+				// first token is ignored
+				for( int x=1; x < splits.length; x++)
+					if( Float.parseFloat(splits[x].replace("\"","")) > 0)
+						list.set(x, list.get(x)+ 1);
+			}
+		}
+			
+		List<Boolean> includeList = new ArrayList<Boolean>();
+		
+		int numIncluded =0;
+		
+		for( int x=0; x < list.size(); x++)
+		{
+			if( list.get(x) >= MIN_NUM )
+			{
+				includeList.add(true);
+				numIncluded++;
+			}
+			else
+			{
+				includeList.add(false);
+			}
+				
+		}
+			
+		reader.close();
+		
+		System.out.println("Returning with " + numIncluded);
+		return includeList;
+	}
+	
 	public static void main(String[] args) throws Exception
 	{
 		int numColumns = 3000;
 		
-		BufferedReader reader =new BufferedReader(new FileReader(new File(
-			ConfigReader.getCREOrthologsDir() + File.separator + "bitScoreOrthologsAsColumns.txt"	)));
+		File inFile = new File(
+				ConfigReader.getCREOrthologsDir() + File.separator + "bitScoreOrthologsAsColumns.txt");
+		
+		BufferedReader reader =new BufferedReader(new FileReader(inFile));
 		
 		BufferedWriter writer=  new BufferedWriter(new FileWriter(new File(
 			ConfigReader.getCREOrthologsDir() + File.separator + "pcaCluster" + 
@@ -27,7 +77,7 @@ public class FilterBitScores
 		
 		String[] splits = reader.readLine().split("\t");
 		
-		List<Boolean> includeList = new ArrayList<Boolean>();
+		List<Boolean> includeList = getIncludeList(inFile);
 		
 		for( int x=0; x < splits.length; x++)
 		{
@@ -36,8 +86,6 @@ public class FilterBitScores
 			else
 				includeList.add(false);
 		}
-		
-		Collections.shuffle(includeList, new Random(32341));
 		
 		writer.write("genome");
 		
