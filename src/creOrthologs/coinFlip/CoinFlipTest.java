14665bee8b0fa214a3597799a3bd36fc8ed49631
Anthony Fodor <anthony.fodor@gmail.com>
2016-07-05 14:57:02 -0400

    put chromosome into gene list...

--- src/creOrthologs/coinFlip/CoinFlipTest.java
+++ src/creOrthologs/coinFlip/CoinFlipTest.java
@@ -5,10 +5,12 @@ import java.io.File;
 import java.io.FileReader;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.StringTokenizer;
 
+import parsers.FastaSequence;
 import utils.ConfigReader;
 
 public class CoinFlipTest
@@ -16,8 +18,11 @@ public class CoinFlipTest
 	private static class GeneHolder
 	{
 		List<RangeHolder> rangeList=new ArrayList<RangeHolder>();
+		String chr;
 		int numOver;
 		int numUnder;
+		
+		HashSet<Long> includedKmers = new HashSet<Long>();
 	}
 	
 	private static class RangeHolder 
@@ -44,14 +49,31 @@ public class CoinFlipTest
 		HashMap<String, GeneHolder> map = parseGTFFile();
 		List<BinHolder> list = parseBinFile();
 		
-		for(String s : map.keySet())
+		
+			
+	}
+	
+	private static void addKmersToGenes(List<GeneHolder> geneHolderList) throws Exception
+	{
+		HashMap<String, FastaSequence> fastaMap = FastaSequence.getFirstTokenSequenceMap(
+				ConfigReader.getBioLockJDir() + File.separator + "resistantAnnotation" + 
+						File.separator +"refGenome");
+		
+		for(GeneHolder gh: geneHolderList)
 		{
-			GeneHolder gh = map.get(s);
 			
-			for(RangeHolder rh : gh.rangeList)
-				System.out.println(s + " " + rh.start + " " + rh.stop);
 		}
-			
+	}
+	
+	private static void populateHolders() throws Exception
+	{
+		BufferedReader reader = new BufferedReader(new FileReader(
+			ConfigReader.getBioLockJDir() + File.separator + "resistantAnnotation" 
+						+ File.separator + 
+					"resistantVsSuc_kneu.txt"));
+		
+		
+		reader.close();
 	}
 	
 	private static class BinHolder
@@ -106,11 +128,14 @@ public class CoinFlipTest
 			if( gh == null)
 			{
 				gh = new GeneHolder();
+				gh.chr = splits[0];
 				map.put(key,gh);
 			}
 			
 			RangeHolder h = new RangeHolder(Integer.parseInt(splits[3]), Integer.parseInt(splits[4]));
 			gh.rangeList.add(h);
+			if( ! gh.chr.equals(splits[0]))
+				throw new Exception("Parsing error");
 		}
 		
 		reader.close();
