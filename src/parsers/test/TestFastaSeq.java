b0598988bab02d887520d187fa02a792d558e7f7
Anthony Fodor <anthony.fodor@gmail.com>
2016-11-17 16:15:18 -0500

    example of a JUnit TestCase

---
+++ src/parsers/test/TestFastaSeq.java
@@ -0,0 +1,22 @@
+package parsers.test;
+
+import org.junit.Test;
+
+import junit.framework.TestCase;
+import parsers.FastaSequence;
+
+public class TestFastaSeq extends TestCase
+{
+	@Test
+	public void testBasicLengthAndIdentity() throws Exception
+	{
+		String aHeader = "Seq1";
+		String aSeq = "CCCGGGAAAA";
+		
+		FastaSequence fs = new FastaSequence(aHeader, aSeq);
+		
+		assertEquals(fs.getHeader(), aHeader);
+		assertEquals(fs.getSequence(), aSeq);
+	}
+
+}
