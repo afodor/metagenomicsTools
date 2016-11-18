e75165b8424c6ed7dd38622a7186fbdf1c18f4c5
Anthony Fodor <anthony.fodor@gmail.com>
2016-11-18 14:57:10 -0500

    Put Take Test case

---
+++ src/classExamples/boundedBuffer/PutTakeTestCase.java
@@ -0,0 +1,103 @@
+package classExamples.boundedBuffer;
+
+import java.util.concurrent.CyclicBarrier;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.atomic.AtomicInteger;
+
+import junit.framework.TestCase;
+
+/**
+ * PutTakeTest
+ * <p/>
+ * Producer-consumer test program for BoundedBuffer
+ *
+ * @author Brian Goetz and Tim Peierls
+
+	Modded from Chapter 11 of Java Concurrency in Practice
+ */
+ public class PutTakeTestCase extends TestCase {
+	 
+    
+    public void test() throws Exception 
+    {
+        PutTakeTest ptt= new PutTakeTest(10, 10, 100000);// sample parameters
+        ptt.runThisTest();
+        ptt.pool.shutdown();
+    }
+    
+    private class PutTakeTest{
+
+    protected final ExecutorService pool = Executors.newCachedThreadPool();
+    protected CyclicBarrier barrier;
+    protected final SemaphoreBoundedBuffer<Integer> bb;
+    protected final int nTrials, nPairs;
+    protected final AtomicInteger putSum = new AtomicInteger(0);
+    protected final AtomicInteger takeSum = new AtomicInteger(0);
+
+
+    int xorShift(int y) {
+        y ^= (y << 6);
+        y ^= (y >>> 21);
+        y ^= (y << 7);
+        return y;
+    }
+    
+    PutTakeTest(int capacity, int npairs, int ntrials) {
+        this.bb = new SemaphoreBoundedBuffer<Integer>(capacity);
+        this.nTrials = ntrials;
+        this.nPairs = npairs;
+        this.barrier = new CyclicBarrier(npairs * 2 + 1);
+    }
+
+    public void runThisTest() {
+        try {
+            for (int i = 0; i < nPairs; i++) {
+                pool.execute(new Producer());
+                pool.execute(new Consumer());
+            }
+            barrier.await(); // wait for all threads to be ready
+            barrier.await(); // wait for all threads to finish
+            assertEquals(putSum.get(), takeSum.get());
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+
+    class Producer implements Runnable {
+        public void run() {
+            try {
+                int seed = (this.hashCode() ^ (int) System.nanoTime());
+                int sum = 0;
+                barrier.await();
+                for (int i = nTrials; i > 0; --i) {
+                    bb.put(seed);
+                    sum += seed;
+                    seed = xorShift(seed);
+                }
+                putSum.getAndAdd(sum);
+                barrier.await();
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+
+    class Consumer implements Runnable {
+        public void run() {
+            try {
+                barrier.await();
+                int sum = 0;
+                for (int i = nTrials; i > 0; --i) {
+                    sum += bb.take();
+                }
+                takeSum.getAndAdd(sum);
+                barrier.await();
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+   }
+}
