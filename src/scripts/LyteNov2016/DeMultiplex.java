2245ae3e2ea93a6ad562d3c5dd77114c86f91f85
Anthony Fodor <anthony.fodor@gmail.com>
2016-12-08 13:10:43 -0500

    demultiplex now working

--- src/scripts/LyteNov2016/DeMultiplex.java
+++ src/scripts/LyteNov2016/DeMultiplex.java
@@ -1,80 +1,59 @@
 package scripts.LyteNov2016;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.util.HashMap;
+import java.util.StringTokenizer;
 
 import parsers.FastQ;
 import utils.ConfigReader;
-import utils.Translate;
 
 public class DeMultiplex
 {
 	
-	private static HashMap<String, String> getBarcodeMap() throws Exception
-	{
-		HashMap<String, String> map = new HashMap<String,String>();
-		
-		BufferedReader reader = new BufferedReader(new FileReader(
-				ConfigReader.getLyteNov2016Dir() + File.separator + "Lyte_mapping_11102016.txt"	));
-			
-		reader.readLine();
-		
-		for(String s= reader.readLine(); s != null; s= reader.readLine())
-		{
-			String[] splits = s.split("\t");
-			String seq = splits[1];
-			
-			if( map.containsKey(seq))
-				throw new Exception("No");
-			
-			map.put(seq,splits[0]);
-		}
-		
-		return map;
-		
-	}
-	
-	// can't seem to find the barcodes in the sequences
 	public static void main(String[] args) throws Exception
 	{
-		HashMap<String, String> demap = getBarcodeMap();
-		for(String s : demap.keySet())
-			System.out.println(s);
 		
 		BufferedReader reader = new BufferedReader(new FileReader(new File(
 			ConfigReader.getLyteNov2016Dir() + File.separator + "Lyte_seqs_R1.fastq"	)));
 		
 		int numFound =0;
-		int numSearched =0;
+		
+		HashMap<String, BufferedWriter> map = new HashMap<String, BufferedWriter>();
 		
 		for( FastQ fastq = FastQ.readOneOrNull(reader); fastq != null; fastq = FastQ.readOneOrNull(reader))
 		{
-			boolean foundOne = false;
-			numSearched++;
-			String match = null;
-			String reverse = Translate.safeReverseTranscribe(fastq.getSequence());
-			for( String s : demap.keySet())
+			String sampleID = fastq.getFirstTokenOfHeader().replace("@", "");
+			
+			sampleID = new StringTokenizer(sampleID, "_").nextToken();
+			
+			BufferedWriter writer = map.get(sampleID);
+			
+			if( writer == null)
 			{
-				if( reverse.indexOf(s) != -1)
-				{
-					foundOne = true;
-					match = s;
-				}
-					
+				writer = new BufferedWriter(new FileWriter(new File(
+					ConfigReader.getLyteNov2016Dir() + File.separator + 	
+					"forwardReadsDemultiplexed" + File.separator + 
+						sampleID + ".fasta"
+						)));
+				map.put(sampleID, writer);
 			}
 			
-			if( foundOne)
-			{
-				numFound++;
-				System.out.println(match + " " + reverse.substring("CCGGACTACHVGGGTWTCTAAT".length()));
-			}
-				
-			if( numSearched % 1000 ==0 )
-				System.out.println(numFound + " " + numSearched );
+			writer.write(">" + fastq.getFirstTokenOfHeader().replace("@", "") + "\n");
+			writer.write(fastq.getSequence() + "\n");
+			
+			numFound++;
+			
+			if( numFound % 10000 ==0)
+				System.out.println(numFound + " " + map.size());
 		}
 		
-		System.out.println(numFound + " " + numSearched);
+		for( BufferedWriter w : map.values())
+		{
+			w.flush();  w.close();
+		}
 	}
 }
