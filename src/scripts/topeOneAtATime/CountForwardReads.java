5599f98e7d2645a68dc567198cd26c2111b54d75
Anthony Fodor <anthony.fodor@gmail.com>
2017-01-16 20:57:53 -0500

    table 1 for the seed paper...

--- src/scripts/topeOneAtATime/CountForwardReads.java
+++ src/scripts/topeOneAtATime/CountForwardReads.java
@@ -22,7 +22,7 @@ public class CountForwardReads
 				ConfigReader.getTopeOneAtATimeDir() + File.separator + 
 				"countSummary.txt")));
 		
-		writer.write("level\ttotalSequences\taverage\tsd\tsampleSize\tfractionAbove1000\n");
+		writer.write("level\ttotalSequences\taverage\tsd\tnumCase\tnumControl\tfractionAbove1000\n");
 		
 		for(int x=1; x < NewRDPParserFileLine.TAXA_ARRAY.length; x++)
 		{
@@ -36,6 +36,10 @@ public class CountForwardReads
 			
 			
 			reader.readLine();
+			
+			int numCase = 0;
+			int numControl = 0;
+			
 			for(String s= reader.readLine(); s != null; s= reader.readLine())
 			{
 				
@@ -43,17 +47,24 @@ public class CountForwardReads
 				
 				int readNum = Integer.parseInt(splits[10]);
 				
-				if( readNum == 1 && splits[11].toLowerCase().equals("false"))
-				{
-					List<Double> list = new ArrayList<Double>();
-					
-					for( int y=17; y < splits.length; y++)
-						list.add(Double.parseDouble(splits[y]));
-					
-					sampleCounts.add(sum(list));
-				}
+				if( readNum == 1 && splits[11].toLowerCase().equals("false") && splits[14].length() > 0)
+						if( Integer.parseInt(splits[14]) == 0 || Integer.parseInt(splits[14]) == 1)
+					{
+						List<Double> list = new ArrayList<Double>();
+						
+						for( int y=17; y < splits.length; y++)
+							list.add(Double.parseDouble(splits[y]));
+						
+						sampleCounts.add(sum(list));
+						
+						if( Integer.parseInt(splits[14]) == 0)
+							numControl++;
+						else if (Integer.parseInt(splits[14]) == 1)
+							numCase++;
+						else throw new Exception("Parsing error");
+					}
 				else if ( readNum != 4 && readNum != 1)
-					throw new Exception("Parsing error " + readNum);
+						throw new Exception("Parsing error " + readNum);
 			}
 
 			Collections.sort(sampleCounts);
@@ -64,7 +75,7 @@ public class CountForwardReads
 			
 			writer.write(taxa + "\t" + (long)sum(sampleCounts) + "\t" + 
 						nf.format(avevar.getAve()) + "\t" + nf.format(avevar.getSD())
-						+ "\t" + sampleCounts.size() + "\t" + 
+						+ "\t" + numCase+ "\t" + numControl + "\t" + 
 					nf.format(fractionAboveThreshold(sampleCounts, 1000)) + "\n");
 			
 			writer.flush(); 
