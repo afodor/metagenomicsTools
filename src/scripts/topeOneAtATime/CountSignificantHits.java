91e7e592e1fe45f886e77c000293b575930c9ac8
Anthony Fodor <anthony.fodor@gmail.com>
2017-02-06 13:28:14 -0500

    significant hits for sex...

--- src/scripts/topeOneAtATime/CountSignificantHits.java
+++ src/scripts/topeOneAtATime/CountSignificantHits.java
@@ -1,8 +1,11 @@
 package scripts.topeOneAtATime;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileReader;
+import java.io.FileWriter;
+import java.text.NumberFormat;
 
 import parsers.NewRDPParserFileLine;
 import utils.ConfigReader;
@@ -14,13 +17,22 @@ public class CountSignificantHits
 	public static void main(String[] args) throws Exception
 	{
 		//addSignifiant("caseControl", 19, 6);
-		addSignifiant("tic", 21, 14);
+		//addSignifiant("tic", 21, 14);
+		addSignifiant("sex", 24, 12,11);
 		
 	}
 	
-	private static void addSignifiant(String text, int sigColumn, int rSquaredColumn)
+	private static void addSignifiant(String text, int sigColumn, int rSquaredColumn, int pValueUncorrected)
 		throws Exception
 	{
+		NumberFormat nf = NumberFormat.getInstance();
+		nf.setMinimumFractionDigits(3);
+		
+		BufferedWriter writer = new BufferedWriter(new FileWriter(new File(ConfigReader.getTopeOneAtATimeDir() 
+				+ File.separator + text + "sigHits.txt")));
+		
+		writer.write("level\ttaxa\tpValue\trSquared\tcorrectedPvalue\totuClassificaiton\n");
+		
 		for(int x=1; x < NewRDPParserFileLine.TAXA_ARRAY_PLUS_OTU.length; x++)
 		{
 			String taxa = NewRDPParserFileLine.TAXA_ARRAY_PLUS_OTU[x];
@@ -39,10 +51,22 @@ public class CountSignificantHits
 				if( pValue <= THRESHOLD)
 				{
 					System.out.println( text + " " + taxa + " "+  splits[0] +  " " + pValue + " " + Double.parseDouble(splits[rSquaredColumn]) );
+					
+					
+					writer.write(taxa + "\t" + nf.format(Double.parseDouble(splits[pValueUncorrected]))  + "\t" + 
+										nf.format(Double.parseDouble(splits[rSquaredColumn]))
+									+ "\t" + Double.parseDouble(nf.format(sigColumn)));
+					
+					if( taxa.equals("otu") )
+						writer.write("\t" + splits[splits.length -1]);
+					
+					writer.write("\n");
 				}
 			}
 			
 			reader.close();
 		}
+
+		writer.flush();  writer.close();
 	}
 }
