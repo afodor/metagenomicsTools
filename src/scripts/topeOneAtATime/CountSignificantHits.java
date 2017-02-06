f31dd2a5962f66dc1bc80451c8e97790c090f168
Anthony Fodor <anthony.fodor@gmail.com>
2017-02-05 22:02:01 -0500

    stub for counting significant hits...

---
+++ src/scripts/topeOneAtATime/CountSignificantHits.java
@@ -0,0 +1,23 @@
+package scripts.topeOneAtATime;
+
+import java.io.BufferedReader;
+
+import parsers.NewRDPParserFileLine;
+
+public class CountSignificantHits
+{
+	private final double THRESHOLD = 0.05;
+	
+	public static void main(String[] args) throws Exception
+	{
+		
+	}
+	
+	private static void addSignifiant(String text, int sigColumn, int rSquaredColumn)
+	{
+		for(int x=0; x < NewRDPParserFileLine.TAXA_ARRAY_PLUS_OTU.length; x++)
+		{
+			//BufferedReader reader = e
+		}
+	}
+}
