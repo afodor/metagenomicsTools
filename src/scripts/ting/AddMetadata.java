5b5f6855b3d89f8b14efcad465076312882c4c7c
Anthony Fodor <anthony.fodor@gmail.com>
2017-02-27 15:06:37 -0500

    add one more...

--- src/scripts/ting/AddMetadata.java
+++ src/scripts/ting/AddMetadata.java
@@ -42,7 +42,7 @@ public class AddMetadata
 	
 		BufferedWriter writer = new BufferedWriter(new FileWriter(outFile));
 	
-		writer.write("mouseId\tgenotype\tcage\ttime\tinitialBodyWeight\tlowestBodyWeight\tbodyWeightAtDay8");
+		writer.write("mouseId\tgenotype\tcage\ttime\tinitialBodyWeight\tlowestBodyWeight\tbodyWeightAtDay8\tmaxWeightLossPercent");
 		writer.write("\tnumberSequencesPerSample\tshannonEntropy");
 	
 		String[] firstSplits = reader.readLine().split("\t");
@@ -65,6 +65,7 @@ public class AddMetadata
 			writer.write(key+ "\t" + mp.getGenotype() + "\t" + mp.getCage() + "\t" + 
 							mp.getTime() + "\t" + mp.getInitialBodyWeight() + "\t" + 
 								mp.getLowestBodyWeight() + "\t" + mp.getBodyWeightAtDay8() + "\t"+ 
+							mp.getMaxWeightLossPercent() + "\t" + 
 				wrapper.getNumberSequences(key.toString()) 
 					+ "\t" + wrapper.getShannonEntropy(key.toString())  );
 		
