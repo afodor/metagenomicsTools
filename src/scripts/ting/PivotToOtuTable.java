1d11afeaa226b5dd6eb3f0ad5a6bb1cade6bf0f4
Anthony Fodor <anthony.fodor@gmail.com>
2017-02-27 13:12:16 -0500

    stub for Ting data...

--- src/scripts/ting/PivotToOtuTable.java
+++ src/scripts/ting/PivotToOtuTable.java
@@ -3,6 +3,7 @@ package scripts.ting;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
+import java.util.HashMap;
 
 import utils.ConfigReader;
 
@@ -13,7 +14,29 @@ public class PivotToOtuTable
 		BufferedReader reader =new BufferedReader(new FileReader(new File(ConfigReader.getTingDir() 
 				+ File.separator + "20170224_Casp11_DSS_16S_DeNovo.txt")));
 		
+		String[] topSplits = reader.readLine().split("\t");
 		
+		for(String s : topSplits)
+			System.out.println(s);
+		
+		HashMap<String, Integer> map = new HashMap<String,Integer>();
+		
+		for(int x=0; x < 15; x++)
+			reader.readLine();
+		
+		String s = reader.readLine();
+		
+		while( s.trim().length() > 0 )
+		{
+			System.out.println(s);
+			
+			String[] splits = s.split("\t");
+			
+			if(splits.length != topSplits.length)
+				throw new Exception("No");
+			
+			s= reader.readLine();
+		}
 		
 		reader.close();
 	}
