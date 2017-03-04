c49e3c5e5a20dad81fdd4f9cec1bc0f170815a8e
Anthony Fodor <anthony.fodor@gmail.com>
2017-03-04 15:41:07 -0500

    start on metadata parser...

---
+++ src/scripts/lyteBehaviorMarch2017/MetadataParser.java
@@ -0,0 +1,63 @@
+package scripts.lyteBehaviorMarch2017;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.util.HashMap;
+
+import utils.ConfigReader;
+
+public class MetadataParser
+{	
+	private static final String getTopLine() throws Exception
+	{
+		File metadataFile = new File(
+				ConfigReader.getLyteBehaviorMarch2017Dir() + File.separator + 
+					"CompiledMetadata.txt");
+		
+		BufferedReader reader =new BufferedReader(new FileReader(metadataFile));
+		
+		String returnVal = reader.readLine();
+		
+		reader.close();
+		return returnVal;
+				
+	}
+	
+	private static HashMap<String, String> getLinesAsMap() throws Exception
+	{
+		HashMap<String, String> map = new HashMap<String,String>();
+		
+		File metadataFile = new File(
+				ConfigReader.getLyteBehaviorMarch2017Dir() + File.separator + 
+					"CompiledMetadata.txt");
+		
+		BufferedReader reader =new BufferedReader(new FileReader(metadataFile));
+		
+		reader.readLine();  
+		
+		for(String s = reader.readLine(); s != null; s = reader.readLine())
+		{
+			String[] splits = s.split("\t");
+			
+			String key = splits[0].replaceAll("\"", "");
+			
+			if( map.containsKey(key))
+				throw new Exception("Duplicate key");
+			
+			map.put(key,s);
+		}
+		
+		reader.close();
+		
+		return map;
+	}
+	
+	public static void main(String[] args) throws Exception
+	{
+		HashMap<String, String> map = getLinesAsMap();
+		
+		for(String s : map.keySet())
+			System.out.println(s + " " + map.get(s));
+	}
+}
