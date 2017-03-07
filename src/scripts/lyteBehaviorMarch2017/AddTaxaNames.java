12c42b927f11a171db71a1a364d62dd7e7f1f532
Anthony Fodor <anthony.fodor@gmail.com>
2017-03-07 15:09:22 -0500

    add taxa to output

--- src/scripts/lyteBehaviorMarch2017/AddTaxaNames.java
+++ src/scripts/lyteBehaviorMarch2017/AddTaxaNames.java
@@ -1,8 +1,10 @@
 package scripts.lyteBehaviorMarch2017;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.util.HashMap;
 
 import utils.ConfigReader;
@@ -41,7 +43,47 @@ public class AddTaxaNames
 	{
 		HashMap<String, String> map = getTaxaStrings();
 		
-		for(String s : map.keySet())
-			System.out.println(s + " " + map.get(s));
+		String[] sources = {"Cecal_Content", "feces", "jej", "ileum","duo" };
+		
+		for( String t : sources)
+		{	
+			
+			File inFile = new File( ConfigReader.getLyteBehaviorMarch2017Dir() + File.separator + "rg_results" 
+					+ File.separator + 
+					 "mainEffects" + t+ ".txt");
+			
+			File outFile =new File( ConfigReader.getLyteBehaviorMarch2017Dir() + File.separator + "rg_results" 
+					+ File.separator + 
+					 "mainEffectsWithTaxaCalls_" + t+ ".txt");
+			
+			addTaxaNames(map, inFile, outFile);
+			 
+		}
+		
+	}
+	
+	private static void addTaxaNames( HashMap<String, String> map , File inFile ,File outFile  )
+		throws Exception
+	{
+		BufferedWriter writer = new BufferedWriter(new FileWriter(outFile));
+		
+		BufferedReader reader = new BufferedReader(new FileReader(inFile));
+		
+		writer.write(reader.readLine() + "\ttaxaAssignment\n");
+		
+		for(String s = reader.readLine(); s != null; s = reader.readLine())
+		{
+			String taxaAssignment = map.get(s.split("\t")[0].replaceAll("\"", ""));
+			
+			if( taxaAssignment == null)
+			{
+				System.out.println("No " + s.split("\t")[0]);
+				taxaAssignment = s.split("\t")[0].replaceAll("\"", "");
+			}
+			
+			writer.write(s + "\t" + taxaAssignment + "\n");
+		}
+		
+		writer.flush();  writer.close();
 	}
 }
