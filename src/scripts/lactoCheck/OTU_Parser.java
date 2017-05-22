946e0f38a57976758a585757f0f2d478f4591fa4
Anthony Fodor <anthony.fodor@gmail.com>
2017-05-22 11:52:36 -0400

    towards full lacto table

--- src/scripts/lactoCheck/OTU_Parser.java
+++ src/scripts/lactoCheck/OTU_Parser.java
@@ -1,9 +1,11 @@
 package scripts.lactoCheck;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileReader;
-import java.util.HashMap;
+import java.io.FileWriter;
+import java.util.ArrayList;
 import java.util.List;
 
 import utils.ConfigReader;
@@ -12,20 +14,102 @@ public class OTU_Parser
 {	
 	public static void main(String[] args) throws Exception
 	{
+		List<String> headers= getSampleHeaders();
+		
+		System.out.println(headers.size());
+		
+		List<Holder> allLacto =getAllLacto();
+		
+		System.out.println(allLacto.size());
+		
+		writeResults(headers, allLacto);
+	}
+	
+	private static void writeResults( List<String> headers, List<Holder> allLacto  )
+		throws Exception
+	{
+		BufferedWriter writer =new BufferedWriter(new FileWriter(new File(
+			ConfigReader.getLactoCheckDir() + File.separator + "merged.txt"	)));
+		
+		writer.write("id");
+		
+		for(Holder h : allLacto)
+			writer.write("\t" + h.taxaString.replace(";", "_"));
+		
+		writer.write("\n");
+		
+		for( int x=0; x < headers.size(); x++)
+		{
+			writer.write(headers.get(x));
+			
+			for( Holder h : allLacto)
+			{
+				if( h.values.size() != headers.size())
+					throw new Exception("No");
+				
+				writer.write("\t" + h.values.get(x));
+				
+			}
+			
+			writer.write("\n");
+		}
+		
+		writer.flush(); writer.close();
+	}
+	private static class Holder
+	{
+		String taxaString;
+		List<Double> values = new ArrayList<Double>();
+		
+		
+	}
+	
+	private static final List<Holder> getAllLacto() throws Exception
+	{
+		List<Holder> list = new ArrayList<Holder>();
+		
 		BufferedReader reader = new BufferedReader(new FileReader(new File(
 				ConfigReader.getLactoCheckDir() + File.separator + 
 					"gaQiimeClosedRef.txt")));
 		
-		reader.readLine();
+		reader.readLine(); reader.readLine();
 		
-		for(String s= reader.readLine(); s != null; s= reader.readLine())
+		for(String s =reader.readLine(); s != null; s= reader.readLine())
 		{
-			String[] splits =s.split("\t");
+			String[] splits = s.split("\t");
 			
 			String taxaString = splits[splits.length-1];
 			
 			if(taxaString.contains("g__Lactobacillus"))
-				System.out.println(splits[splits.length-1]);
+			{
+				Holder h = new Holder();
+				h.taxaString = taxaString;
+				
+				for( int x=1; x < splits.length-1; x++)
+					h.values.add(Double.parseDouble(splits[x]));
+				list.add(h);
+			}
+				
 		}
+		reader.close();
+		return list;
+	}
+	
+	private static final List<String> getSampleHeaders() throws Exception
+	{
+		BufferedReader reader = new BufferedReader(new FileReader(new File(
+				ConfigReader.getLactoCheckDir() + File.separator + 
+					"gaQiimeClosedRef.txt")));
+		reader.readLine();
+		
+		String[] splits = reader.readLine().split("\t");
+		
+		List<String> list = new ArrayList<String>();
+		
+		for( int x=1; x < splits.length-1; x++)
+			list.add(splits[x]);
+		
+		reader.close();
+		return list;
 	}
 }
