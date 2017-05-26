0ebda85a1bd8204975095d7666c8d096b29eb54e
Anthony Fodor <anthony.fodor@gmail.com>
2017-05-26 13:09:55 -0400

    do log scales...

--- src/scripts/lactoCheck/FindTopHits.java
+++ src/scripts/lactoCheck/FindTopHits.java
@@ -1,10 +1,14 @@
 package scripts.lactoCheck;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileReader;
+import java.io.FileWriter;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.HashSet;
+import java.util.List;
 
 import parsers.HitScores;
 import utils.ConfigReader;
@@ -53,6 +57,8 @@ public class FindTopHits
 				HitScores.getTopHitsAsQueryMap(ConfigReader.getLactoCheckDir() + File.separator + 
 						"otusToInersMatchingByBlast.txt");
 		
+		HashSet<Integer> toKeep = new HashSet<Integer>();
+		
 		for(String s : topHitsMap.keySet())
 		{
 			HitScores hs = topHitsMap.get(s);
@@ -63,8 +69,61 @@ public class FindTopHits
 						hs.getPercentIdentity());
 				
 				System.out.println(accessionToGreengenesMap.get(hs.getQueryId()));
+				toKeep.addAll(accessionToGreengenesMap.get(hs.getQueryId()));
 			}
 				
 		}
+		
+		BufferedReader reader = new BufferedReader(new FileReader(new File(
+			ConfigReader.getLactoCheckDir() + File.separator + 
+				"merged.txt")));
+		
+		BufferedWriter writer = new BufferedWriter(new FileWriter(new File(
+				ConfigReader.getLactoCheckDir() + File.separator + 
+				"mergedOnlyIners.txt")));
+		
+		List<Boolean> includeColumn = new ArrayList<Boolean>();
+		
+		for(int x=0; x< 5; x++)
+			includeColumn.add(true);
+		
+		String[] splits = reader.readLine().split("\t");
+		
+		for( int x=5; x < splits.length; x++)
+		{
+			if( toKeep.contains(Integer.parseInt(splits[x])))
+			{
+				includeColumn.add(true);
+			}
+			else
+			{
+				includeColumn.add(false);
+			}
+		}
+		
+		writer.write(splits[0]);
+		for(int x=1; x < splits.length; x++)
+			if( includeColumn.get(x))
+				writer.write("\t" + splits[x]);
+		
+		writer.write("\n");
+		
+		for( String s= reader.readLine(); s!=null; s= reader.readLine())
+		{
+			splits=  s.split("\t");
+			
+
+			writer.write(splits[0]);
+			for(int x=1; x < splits.length; x++)
+				if( includeColumn.get(x))
+					writer.write("\t" + splits[x]);
+			
+			writer.write("\n");
+			
+		}
+		
+		writer.flush();  writer.close();
+		
+		reader.close();
 	}
 }
