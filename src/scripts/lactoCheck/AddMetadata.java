e089581c4d905ac1664a9aca6ea6c561a98f7f71
Anthony Fodor <anthony.fodor@gmail.com>
2017-06-15 15:37:44 -0400

    add metadata

---
+++ src/scripts/lactoCheck/AddMetadata.java
@@ -0,0 +1,70 @@
+package scripts.lactoCheck;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.util.HashMap;
+
+import parsers.OtuWrapper;
+import utils.ConfigReader;
+
+public class AddMetadata
+{
+	public static void main(String[] args) throws Exception
+	{
+		OtuWrapper wrapper = new OtuWrapper(ConfigReader.getLactoCheckDir() + File.separator + "gaQiimeClosedRefColumnsAsTaxa.txt");
+		
+		File logNormFile = new File(ConfigReader.getLactoCheckDir() + File.separator + "gaQiimeClosedRefColumnsAsTaxaLogNorm.txt");
+		
+		File metaFile =new File(ConfigReader.getLactoCheckDir() + File.separator + "gaQiimeClosedRefColumnsAsTaxaLogNormPlusMeta.txt");
+		
+		addMetadata(logNormFile, metaFile, wrapper);
+	}
+	
+	private static void addMetadata(File inFile, File outFile, OtuWrapper originalWrapper) throws Exception
+	{
+		HashMap<String, PCR_DataParser> pcrMap = PCR_DataParser.getPCRData();
+		
+		for(String s : pcrMap.keySet())
+			System.out.println(s);
+		
+		BufferedReader reader = new BufferedReader(new FileReader(inFile));
+		
+		BufferedWriter writer = new BufferedWriter(new FileWriter(outFile));
+		
+		String[] topSplits = reader.readLine().split("\t");
+		
+		writer.write(topSplits[0] + "groupNumber\tL_crispatus\tL_iners\tbglobulin\tsequencingDepth\tshannonDiveristy");
+		
+		for( int x=1; x < topSplits.length; x++)
+			writer.write("\t" + topSplits[x]);
+		
+		writer.write("\n");
+		
+		for(String s = reader.readLine(); s != null; s = reader.readLine())
+		{
+			String[] splits = s.split("\t");
+			String[] codes = splits[0].split("\\.");
+			
+			if(codes.length != 3)
+				throw new Exception("No " + splits[0]);
+
+			if( codes[2].startsWith("G"))
+			{
+				PCR_DataParser pcr = pcrMap.get(codes[2]);
+				
+				if( pcr == null)
+					throw new Exception("No");
+			}
+			else
+			{
+				System.out.println("Could not find" + splits[0]);
+			}
+			
+		}
+		
+		reader.close();
+	}
+}
