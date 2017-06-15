f1590418fd2a45b296327fe34327d5e6857e72a6
Anthony Fodor <anthoadny.fodor@gmail.com>
2017-06-15 16:54:52 -0400

    add birth group to metadata parser...

--- src/scripts/lactoCheck/AddMetadata.java
+++ src/scripts/lactoCheck/AddMetadata.java
@@ -23,9 +23,37 @@ public class AddMetadata
 		addMetadata(logNormFile, metaFile, wrapper);
 	}
 	
+	private static HashMap<String, Integer> getBirthGroupMap() throws Exception
+	{
+		HashMap<String, Integer> map = new HashMap<String,Integer>();
+		
+		BufferedReader reader = new BufferedReader(new FileReader(new File(
+				ConfigReader.getLactoCheckDir() + File.separator + "BirthGroup.txt")));
+		
+		reader.readLine();
+		
+		for(String s = reader.readLine() ; s!= null; s= reader.readLine())
+		{
+			String[] splits = s.split("\t");
+			
+			if( splits.length != 2)
+				throw new Exception("No");
+			
+			if( map.containsKey(splits[0]))
+				throw new Exception("No");
+			
+			map.put(splits[0], Integer.parseInt(splits[1]));
+			
+		}
+		
+		reader.close();
+		return map;
+	}
+	
 	private static void addMetadata(File inFile, File outFile, OtuWrapper originalWrapper) throws Exception
 	{
 		HashMap<String, PCR_DataParser> pcrMap = PCR_DataParser.getPCRData();
+		HashMap<String, Integer> birthMap = getBirthGroupMap();
 		
 		for(String s : pcrMap.keySet())
 			System.out.println(s);
@@ -35,8 +63,8 @@ public class AddMetadata
 		BufferedWriter writer = new BufferedWriter(new FileWriter(outFile));
 		
 		String[] topSplits = reader.readLine().split("\t");
-		
-		writer.write(topSplits[0] + "\tgroupNumber\tL_crispatus\tL_iners\tbglobulin\tsequencingDepth\tshannonDiveristy");
+
+		writer.write(topSplits[0] + "\tgroupID\tgroupNumber\tL_crispatus\tL_iners\tbglobulin\tsequencingDepth\tshannonDiveristy");
 		
 		for( int x=1; x < topSplits.length; x++)
 			writer.write("\t" + topSplits[x]);
@@ -53,12 +81,17 @@ public class AddMetadata
 
 			if( codes[2].startsWith("G"))
 			{
+				Integer birthGroup = birthMap.get(codes[2]);
+				
+				if( birthGroup == null)
+					throw new Exception("No");
+				
 				PCR_DataParser pcr = pcrMap.get(codes[2]);
 				
 				if( pcr == null || ! pcr.getGroup().equals(codes[2]))
 					throw new Exception("No");
 				
-				writer.write(splits[0] + "\t" + codes[2] + "\t" + pcr.getL_crispatus() + "\t" + pcr.getL_iners() + 
+				writer.write(splits[0] + "\t" + codes[2] + "\t" + birthGroup +  "\t" + pcr.getL_crispatus() + "\t" + pcr.getL_iners() + 
 						"\t" + pcr.getBglobulin() + "\t" + originalWrapper.getNumberSequences(splits[0]) + "\t" +
 								originalWrapper.getShannonEntropy(splits[0]));
 				
