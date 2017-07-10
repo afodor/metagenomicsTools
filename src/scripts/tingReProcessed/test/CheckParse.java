90978986e3dee8676a00f975c290bbc4b0fed4e6
Anthony Fodor <anthony.fodor@gmail.com>
2017-07-10 12:54:31 -0400

    towards taxa check...

--- src/scripts/tingReProcessed/test/CheckParse.java
+++ src/scripts/tingReProcessed/test/CheckParse.java
@@ -3,8 +3,10 @@ package scripts.tingReProcessed.test;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.LinkedHashMap;
+import java.util.List;
 
 import utils.ConfigReader;
 
@@ -20,7 +22,65 @@ public class CheckParse
 		
 		confirmPivot(7, 8);
 
+		List<String> names =getNames();
+		
+		List<String> pivotNames = getNamesFromPivot();
+		
+		for( int x=0; x < pivotNames.size(); x++)
+			System.out.println(pivotNames.get(x) + " " + getSkipNum(names, pivotNames.get(x)));
+		
+		
 		System.out.println("pass");
+	}
+	
+	
+	
+	private static List<String> getNames() throws Exception
+	{
+		List<String> list = new ArrayList<String>();
+		
+		BufferedReader reader= new BufferedReader(new FileReader(new File(
+				ConfigReader.getTingDir() + File.separator + "may_2017_rerun" + File.separator + 
+					"20170512_Casp11_DSS_5groups_16S_DeNovo_NoPhiX_NoPrimerSeq_L6.txt")));
+		
+		for(String s= reader.readLine(); s != null; s= reader.readLine())
+		{
+			list.add( s.trim().length() > 0 ? s.split("\t")[0] : "");
+			
+		}
+			
+		reader.close();
+		
+		return list;
+	}
+	
+
+	private static List<String> getNamesFromPivot() throws Exception
+	{
+		List<String> list = new ArrayList<String>();
+		
+		BufferedReader reader = new BufferedReader(new FileReader(new File(
+				ConfigReader.getTingDir() + File.separator + "may_2017_rerun" + File.separator + 
+				"otuAsColumnsLogNorm_rerunPlusMetadata_L6.txt")));
+		
+		String[] splits=reader.readLine().split("\t");
+		
+		for(String s : splits)
+			list.add(s);
+		
+		reader.close();
+		
+		return list;
+	}
+	
+	
+	private static int getSkipNum(List<String> names, String name) throws Exception
+	{
+		for( int x=0; x < names.size(); x++)
+			if( names.get(x).equals(name) )
+				return x-2;
+		
+		return -1;
 	}
 	
 	private static void confirmPivot(int skipNumber, int columnNumber) throws Exception
