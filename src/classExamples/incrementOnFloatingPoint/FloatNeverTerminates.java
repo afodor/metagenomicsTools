6de1d87f8e0b22b917f9ff40b07e1a68e3ab6918
Anthony Fodor <anthony.fodor@gmail.com>
2017-09-10 10:18:19 -0400

    float never terminates

---
+++ src/classExamples/incrementOnFloatingPoint/FloatNeverTerminates.java
@@ -0,0 +1,14 @@
+package classExamples.incrementOnFloatingPoint;
+
+public class FloatNeverTerminates
+{
+	public static void main(String[] args)
+	{
+		for( float f= 2147383743; f < 2147383744; f++)
+		{
+			
+		}
+		
+		System.out.println("We never get here");
+	}
+}
