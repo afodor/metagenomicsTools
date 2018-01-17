167ca244f5428a0baa3381396545b43b6ff865ab
Anthony Fodor <anthony.fodor@gmail.com>
2018-01-17 18:00:13 -0500

    add meta map....

--- src/scripts/emilyJan2018/AddMetadata.java
+++ src/scripts/emilyJan2018/AddMetadata.java
@@ -3,6 +3,7 @@ package scripts.emilyJan2018;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
+import java.util.HashMap;
 
 import utils.ConfigReader;
 
@@ -31,6 +32,38 @@ public class AddMetadata
 		
 		if( ! top2.equals(top3))
 			throw new Exception("Logic error");
+		
+		HashMap<String, String> map = new HashMap<>();
+		addToMap(meta1, map, "AN703");
+		addToMap(meta2, map, "AN40");
+		addToMap(meta3, map,"AN34");
+	}
+	
+	private static void addToMap(File inFile, HashMap<String, String> map, String suffix) throws Exception
+	{
+		BufferedReader reader = new BufferedReader(new FileReader(inFile));
+		
+		reader.readLine();
+		
+		for(String s= reader.readLine(); s != null && s.trim().length() > 0 ; s= reader.readLine())
+		{
+			s = s.replaceAll("\"", "");
+			String[] splits =s.split("\t");
+			
+			String key = splits[0] + "_" + suffix;
+			
+			if( map.containsKey(key))
+				throw new Exception("Duplciate key " +  key);
+			
+			map.put(key, s);
+		}
+		
+		reader.close();
+	}
+	
+	private static void writeAMeta(File inFile, File outFile, String firstLine ) throws Exception
+	{
+		
 	}
 	
 	private static String getFirstLine(File file) throws Exception
