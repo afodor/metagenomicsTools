51dd35921d7ee355227b8222e64cacd409c6a5f6
Anthony Fodor <anthony.fodor@gmail.com>
2018-02-07 15:47:28 -0500

    attempt to merge meta...

--- src/scripts/FarnazFeb2018/MergeMetadata.java
+++ src/scripts/FarnazFeb2018/MergeMetadata.java
@@ -1,8 +1,10 @@
 package scripts.FarnazFeb2018;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.util.HashMap;
 
 import utils.ConfigReader;
@@ -17,6 +19,58 @@ public class MergeMetadata
 		String firstLine = readFirstLine(metaFile);
 		HashMap<Integer, String> metaMap = getMetaMap(metaFile);
 		System.out.println(metaMap.size());
+		
+		File inFile = new File(ConfigReader.getFarnazFeb2018Directory() + File.separator + 
+					"otu_table_mc2_w_tax_L6.txt");
+		
+		File outFile = new File(ConfigReader.getFarnazFeb2018Directory() + File.separator + 
+				"otu_table_mc2_w_tax_L6WithMeta.txt");
+	
+		addColumns(inFile, firstLine, metaMap, outFile);
+	}
+	
+	private static void addColumns(File inFile, String firstLine,HashMap<Integer, String> metaMap, 
+				File outFile) throws Exception
+	{
+		BufferedReader reader = new BufferedReader(new FileReader(inFile));
+		
+		BufferedWriter writer = new BufferedWriter(new FileWriter(outFile));
+		
+		writer.write("sampleID");
+		
+		String[] topSplits = firstLine.split("\t");
+		
+		for( int x=1; x < topSplits.length; x++)
+			writer.write("\t" + topSplits[x]);
+		
+		topSplits = reader.readLine().split("\t");
+		
+		for( int x=1; x < topSplits.length; x++)
+			writer.write("\t" + topSplits[x]);
+		
+		writer.write("\n");
+		
+		for(String s= reader.readLine(); s != null; s= reader.readLine())
+		{
+			String[] dataSplits=  s.split("\t");
+			
+			int key = Integer.parseInt(dataSplits[0]);
+			writer.write("" + key);
+			
+			String metaLine = metaMap.get(key);
+			String[] metaSplits = metaLine.split("\t");
+			
+			for( int x=1; x < metaSplits.length; x++)
+				writer.write("\t" + metaSplits[x]);
+			
+			for( int x=1; x < dataSplits.length; x++)
+				writer.write("\t" + dataSplits[x]);
+			
+			writer.write("\n");
+		}
+		
+		writer.flush(); writer.close();
+		reader.close();
 	}
 	
 	private static HashMap<Integer, String> getMetaMap(File file) throws Exception
