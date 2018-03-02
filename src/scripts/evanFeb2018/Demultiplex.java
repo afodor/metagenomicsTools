9a945a96a9aa3287e5d4ae334daa17d204085359
Anthony Fodor <anthony.fodor@gmail.com>
2018-03-02 15:12:45 -0500

    awaiting batch 2...

--- src/scripts/evanFeb2018/Demultiplex.java
+++ src/scripts/evanFeb2018/Demultiplex.java
@@ -1,41 +1,69 @@
 package scripts.evanFeb2018;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.util.HashMap;
+import java.util.HashSet;
 
 import parsers.FastQ;
 import utils.ConfigReader;
 
 public class Demultiplex
 {
-
 	public static void main(String[] args) throws Exception
 	{
 		File mappingFile1 = new File(ConfigReader.getEvanFeb2018Dir() + File.separator + 
 				"Batch_1_2015" + File.separator + "batch_1_2015_mapping.txt");
 		
-		HashMap<String, String> barcode1 = getBarcodeToSampleMap(mappingFile1);
-
-		for(String s : barcode1.keySet())
-			System.out.println(s +  " "+ barcode1.get(s));
+		File seqFile1= 
+				new File(
+						ConfigReader.getEvanFeb2018Dir() + File.separator + 
+						"Batch_1_2015" + File.separator + "R_2015_03_31_14_16_19_user_Nikki.fastq");
 		
-		BufferedReader fastq1Reader = new BufferedReader(new FileReader(new File(
-				ConfigReader.getEvanFeb2018Dir() + File.separator + 
-				"Batch_1_2015" + File.separator + "R_2015_03_31_14_16_19_user_Nikki.fastq")));
+		demultiplex(mappingFile1, seqFile1, 1);
+		
+	}
+	
+	private static void demultiplex(File mapFile, File sequenceFile, int batchNumber) throws Exception
+	{
+		HashMap<String, String> barcodeToSampleMap= getBarcodeToSampleMap(mapFile);
+
+		HashMap<String, BufferedWriter> writerMap = new HashMap<>();		
+		
+		for(String s : barcodeToSampleMap.values())
+		{
+			BufferedWriter writer = new BufferedWriter(new FileWriter(new File(
+				ConfigReader.getEvanFeb2018Dir() + File.separator + "demultiplexFastaA" +
+						File.separator + s + "_" + batchNumber 
+						+ ".fasta" 
+						)));
+			
+			writerMap.put(s,writer);
+		}
+		
+		BufferedReader fastqReader = new BufferedReader(new FileReader(sequenceFile));
 		
 		long numFound =0;
 		long numTested =0;
 		
-		for( FastQ fq = FastQ.readOneOrNull(fastq1Reader); fq != null; fq = FastQ.readOneOrNull(fastq1Reader))
+		for( FastQ fq = FastQ.readOneOrNull(fastqReader); fq != null; fq = FastQ.readOneOrNull(fastqReader))
 		{
 			boolean gotOne =false;
 			
-			for(String s : barcode1.keySet())
+			for(String s : barcodeToSampleMap.keySet())
 				if( fq.getSequence().startsWith(s))
+				{
 					gotOne = true;
-			
+					
+					BufferedWriter writer = writerMap.get(barcodeToSampleMap.get(s));
+					
+					writer.write(">" + fq.getHeader() + "\n");
+					writer.write( fq.getSequence() + "\n");
+				}
+					
 			numTested++;
 			
 			if( gotOne)
@@ -44,11 +72,19 @@ public class Demultiplex
 			if( numTested % 10000 == 0 )
 				System.out.println(numFound + " " + numTested);
 		}
+		
+		fastqReader.close();
+		
+		for( BufferedWriter w : writerMap.values())
+		{
+			w.flush(); w.close();
+		}
 	}
 	
 	private static HashMap<String, String> getBarcodeToSampleMap(File inFile) throws Exception
 	{
 		HashMap<String, String> map = new HashMap<>();
+		HashSet<String> vals = new HashSet<>();
 		
 		BufferedReader reader = new BufferedReader(new FileReader(inFile));
 		
@@ -64,10 +100,14 @@ public class Demultiplex
 				throw new Exception("No");
 			
 			map.put(splits[1], splits[0]);
+			
+			if( vals.contains(splits[0]))
+				throw new Exception("No");
+			
+			vals.add(splits[0]);
 		}
 		
 		return map;
 	}
-	
 }
 
