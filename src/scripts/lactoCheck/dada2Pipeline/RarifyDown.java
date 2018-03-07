c54632baee0cfbb0128acd74cffacbdac981edbb
Anthony Fodor <anthony.fodor@gmail.com>
2018-03-06 20:19:48 -0500

    code to rarify down...

--- src/scripts/lactoCheck/dada2Pipeline/RarifyDown.java
+++ src/scripts/lactoCheck/dada2Pipeline/RarifyDown.java
@@ -1,14 +1,27 @@
 package scripts.lactoCheck.dada2Pipeline;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Random;
+import java.util.zip.GZIPInputStream;
 
+import parsers.FastQ;
 import utils.ConfigReader;
 
 public class RarifyDown
 {
+	private static final Random RANDOM = new Random(42);
+	
 	public static void main(String[] args) throws Exception
 	{
 		HashMap<String, Integer> map = getDepthMap();
@@ -20,6 +33,75 @@ public class RarifyDown
 				rareNumber =Math.min(rareNumber, i);
 		
 		System.out.println(rareNumber);
+	
+		File fastqDir = new File(ConfigReader.getLactoCheckDir() + File.separator + 
+				"fastqDemultiplexed");
+		
+		File fastqOutDir = new File(ConfigReader.getLactoCheckDir() + File.separator + 
+				"fastqDemultiplexedRarified");
+		
+		for(String s : map.keySet())
+		{
+			int readDepth = map.get(s);
+			
+			if(readDepth > 50)
+			{
+				System.out.println(s + " " + readDepth);
+				HashSet<Integer> set = getIncludeSet(readDepth, rareNumber);
+				
+				int numWritten =0;
+				
+				BufferedReader reader =  new BufferedReader(new InputStreamReader( 
+						new GZIPInputStream( new FileInputStream( fastqDir + File.separator + s ) ) ));
+				
+				BufferedWriter writer = new BufferedWriter(new FileWriter(new File(
+						fastqOutDir.getAbsolutePath() + File.separator + 
+						s.replace(".gz", ""))));
+				
+				int seqIndex =0;
+				for( FastQ fq= FastQ.readOneOrNull(reader); fq != null; fq = FastQ.readOneOrNull(reader))
+				{
+					if( set.contains(seqIndex))
+					{
+						set.remove(seqIndex);
+						numWritten++;
+						fq.writeToFile(writer);
+					}
+					
+					
+					seqIndex++;
+				}
+				
+				writer.flush(); writer.close();
+				
+				reader.close();
+				
+				if( numWritten != rareNumber)
+					throw new Exception("No");
+				
+				if( ! set.isEmpty())
+					throw new Exception("No");
+			
+			}
+		}
+	
+	}
+	
+	private static HashSet<Integer> getIncludeSet(int depth, int setSize )
+	{
+		List<Integer> list= new ArrayList<>();
+		
+		for( int x=0; x < depth; x++)
+			list.add(x);
+		
+		Collections.shuffle(list, RANDOM);
+		
+		HashSet<Integer> set= new HashSet<>();
+		
+		for( int x=0; x < setSize;x++)
+			set.add(list.get(x));
+		
+		return set;
 	}
 	
 	private static HashMap<String, Integer> getDepthMap() throws Exception
