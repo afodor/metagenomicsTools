b13ec4f1edc54ffd2737f7e7fe6de639dde3a8b2
Anthony Fodor <anthony.fodor@gmail.com>
2018-03-08 13:55:54 -0500

    got best blast hits...

--- src/scripts/lactoCheck/dada2Pipeline/WriteTopBlastHits.java
+++ src/scripts/lactoCheck/dada2Pipeline/WriteTopBlastHits.java
@@ -1,8 +1,12 @@
 package scripts.lactoCheck.dada2Pipeline;
 
+import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileWriter;
 import java.util.HashMap;
 import java.util.HashSet;
+import java.util.LinkedHashMap;
+import java.util.LinkedHashSet;
 import java.util.Set;
 
 import parsers.FastaSequence;
@@ -17,9 +21,8 @@ public class WriteTopBlastHits
 		HashMap<String, HitScores> queryMap = 
 		HitScores.getTopHitsAsQueryMap(ConfigReader.getLactoCheckDir() + File.separator + 
 				"allTogetherFastaToSilvaByBlast.txt");
-	
 		
-		HashSet<String> targets = new HashSet<>();
+		HashSet<String> targets = new LinkedHashSet<>();
 		
 		for(String s : queryMap.keySet())
 			targets.add(queryMap.get(s).getTargetId());
@@ -28,11 +31,30 @@ public class WriteTopBlastHits
 		
 		for(String s : headers.keySet())
 			System.out.println(s + " " + headers.get(s));
+		
+		BufferedWriter writer = new BufferedWriter(new FileWriter(new File(
+				ConfigReader.getLactoCheckDir() + File.separator + "silvaToDada2TopHits.txt")));
+		
+		writer.write("queryID\ttargetID\talignmentLength\tpercentIdentity\tnumMismatches\teScore\theaderLine\n");
+		
+		for(String s : queryMap.keySet())
+		{
+			HitScores hs = queryMap.get(s);
+			writer.write(hs.getQueryId() + "\t");
+			writer.write(hs.getTargetId()+"\t");
+			writer.write(hs.getAlignmentLength() + "\t");
+			writer.write(hs.getPercentIdentity() + "\t");
+			writer.write(hs.getNumMismatches() + "\t");
+			writer.write(hs.getEScore() + "\t");
+			writer.write(headers.get(hs.getTargetId()) + "\n");
+		}
+		
+		writer.flush();  writer.close();
 	}
 	
 	private static HashMap<String, String> getSilvaHeaderLines(Set<String> queryIDS) throws Exception
 	{
-		HashMap<String, String> map = new HashMap<>();
+		HashMap<String, String> map = new LinkedHashMap<>();
 		
 		FastaSequenceOneAtATime fsoat = new FastaSequenceOneAtATime(
 				"C:\\silvaDatabase\\SILVA_132_SSURef_Nr99_tax_silva.fasta");
@@ -46,6 +68,8 @@ public class WriteTopBlastHits
 				map.put(key, fs.getHeader());
 			}
 		}
+		
+		fsoat.close();
 		
 		return map;
 	}
