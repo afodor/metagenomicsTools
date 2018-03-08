7e93ffc573c9d80e0b21d16a23420136f483fbd8
Anthony Fodor <anthony.fodor@gmail.com>
2018-03-08 09:59:10 -0500

    all together pipeline

--- src/scripts/lactoCheck/dada2Pipeline/PivotAllToOTUTable.java
+++ src/scripts/lactoCheck/dada2Pipeline/PivotAllToOTUTable.java
@@ -1,8 +1,10 @@
 package scripts.lactoCheck.dada2Pipeline;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.StringTokenizer;
@@ -24,6 +26,43 @@ public class PivotAllToOTUTable
 		
 		for(String s: header)
 			System.out.println(s);
+		
+		BufferedWriter writer = new BufferedWriter( new FileWriter(new File(
+				ConfigReader.getLactoCheckDir() + File.separator + 
+					"dada2AllTogetherPivoted.txt")) );
+		
+		writer.write("sample");
+		
+		for( int x=0; x < header.size(); x++)
+		{
+			writer.write("\t" + "taxa_" + (x+1));
+		}
+		
+		writer.write("\n");
+		
+		for(String s= reader.readLine(); s != null; s= reader.readLine())
+		{
+			s = s.replaceAll("\"", "");
+			
+			StringTokenizer sToken = new StringTokenizer(s);
+			
+			while(sToken.hasMoreTokens())
+				writer.write(sToken.nextToken() + (sToken.hasMoreTokens() ? "\t" : "\n") );
+		}
+		
+		writer.flush();  writer.close();
+		
+		BufferedWriter writerFasta = new BufferedWriter(new FileWriter(new File(
+				ConfigReader.getLactoCheckDir() + File.separator + 
+				"allTogetherFasta.txt")));
+		
+		for( int x=0; x < header.size(); x++)
+		{
+			writerFasta.write(">taxa_" + (x+1) + "\n");
+			writerFasta.write(header.get(x) + "\n");
+		}
+		
+		writerFasta.flush();  writerFasta.close();
 	}
 	
 	private static List<String> getSequenceHeaders(String s) throws Exception
