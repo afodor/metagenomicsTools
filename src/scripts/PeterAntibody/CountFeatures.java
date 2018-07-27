159543c2a8ddc01c5e609db979b8321a19c9ede8
afodor <afodor@CCI7NR4PN2WWS.dyn.uncc.edu>
2018-07-26 21:58:47 -0400

    need to know how to handle mismatches...

--- src/scripts/PeterAntibody/CountFeatures.java
+++ src/scripts/PeterAntibody/CountFeatures.java
@@ -1,8 +1,11 @@
 package scripts.PeterAntibody;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.StringTokenizer;
 
 import utils.ConfigReader;
 
@@ -23,6 +26,7 @@ public class CountFeatures
 		map.put("human_kappa_germline_chothia.txt", "human_kappa_germline");  
 		map.put("imgt_mouse_lc_chothia.txt", "imgt_mouse");
 		map.put("therapeutic_lc_chothia.txt", "therapeutic");
+		map.put("mouse_kappa_germline_chothia.txt","mouse_germline" );
 		
 		return map;
 	}
@@ -38,12 +42,15 @@ public class CountFeatures
 	 * */
 	private static HashMap< String, HashMap<String,HashMap<String,Character>>>  getMap() throws Exception
 	{
+		int count =0;
 		HashMap<String, String> fileToCatMap = getFileNameToCategoryMap();
 		
 		HashMap< String, HashMap<String,HashMap<String,Character>>> map = new HashMap<>();
 		
-		String[] files = new File(ConfigReader.getPeterAntibodyDirectory() + File.separator + 
-					"oneAtATime" + File.separator + "inputFiles").list();
+		File topDir = new File(ConfigReader.getPeterAntibodyDirectory() + File.separator + 
+				"oneAtATime" + File.separator + "inputFiles");
+		
+		String[] files = topDir.list();
 		
 		for( String name : files)
 		{
@@ -55,11 +62,57 @@ public class CountFeatures
 			
 			HashMap<String,HashMap<String,Character>> map1 = map.get(assignment);
 			
-			if( assignment == null)
+			if( map1 == null)
 			{
 				map1 =new HashMap<String,HashMap<String,Character>>();
 				map.put(assignment, map1);
 			}
+			
+			HashMap<String,Character> seqMap = null;
+			
+			BufferedReader reader = new BufferedReader(new FileReader(new File(topDir.getAbsolutePath()
+					+ File.separator + name)));
+			
+			for(String s= reader.readLine(); s != null && s.trim().length() > 0; s= reader.readLine())
+			{
+				if( s.startsWith(">"))
+				{
+					String key = s.substring(1, s.lastIndexOf("|"));
+					System.out.println(key);
+					
+					seqMap = map1.get(key);
+					
+					if( seqMap == null)
+					{
+						seqMap = new HashMap<String,Character>();
+						map1.put(key, seqMap);
+					}
+				}
+				else
+				{
+					StringTokenizer sToken =new StringTokenizer(s);
+					
+					if( sToken.countTokens() != 2)
+						throw new Exception("No " + s);
+					
+					String key = new String(sToken.nextToken());
+				
+					String charString = sToken.nextToken();
+					if( charString.length() != 1)
+						throw new Exception("No " + s);
+					
+					if( seqMap.containsKey(key) && seqMap.get(key) != charString.charAt(0))
+					{
+						System.out.println("Duplicate mismatch" + key + " " + seqMap.get(key) + " " + charString.charAt(0));
+						count++;
+					}
+						
+					seqMap.put(key, charString.charAt(0));
+				}
+			}
+			
+			reader.close();
+			System.out.println("Duplicate " + count);
 		}
 		
 		return map;
