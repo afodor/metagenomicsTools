f402b1053f4b6e42ba212ba561a7057b27601d3f
afodor <afodor@CCI7NR4PN2WWS.dyn.uncc.edu>
2018-07-29 18:52:18 -0400

    towards writing taxa map...

--- src/scripts/laura/ierProject/MergeQiimeToMeta.java
+++ src/scripts/laura/ierProject/MergeQiimeToMeta.java
@@ -4,6 +4,7 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 import utils.ConfigReader;
@@ -19,7 +20,42 @@ public class MergeQiimeToMeta
 		
 	
 		List<String> sampleId = getSampleIDs(relativeAbundnaceFile);
+		HashMap<String, List<Double>> taxaMap = getAsMap(relativeAbundnaceFile);
 	}
+	
+	// outer key is taxa
+	private static HashMap<String, List<Double>> getAsMap(File file) throws Exception
+	{
+		HashMap<String, List<Double>> map =new HashMap<>();
+		
+		BufferedReader reader = new BufferedReader(new FileReader(file));
+		
+		reader.readLine();
+		
+		for(String s= reader.readLine(); s != null; s= reader.readLine())
+		{
+			String[] splits= s.split("\t");
+			
+			String key = new String(splits[0]);
+			
+			if(map.containsKey(key))
+				throw new Exception("No");
+			
+			List<Double> list= new ArrayList<>();
+			
+			map.put(key,list );
+			
+			for( int x=1; x < splits.length; x++)
+			{
+				list.add(Double.parseDouble(splits[x]));
+			}
+		}
+		
+		reader.close();
+		
+		return map;
+	}
+	
 	
 	private static List<String> getSampleIDs(File inFile) throws Exception
 	{
