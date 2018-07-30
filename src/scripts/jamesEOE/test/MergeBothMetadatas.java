dc905d14d9a78561bd847e1f07915dbd92335c56
afodor <afodor@CCI7NR4PN2WWS.dyn.uncc.edu>
2018-07-30 16:42:58 -0400

    Need to log normalize

--- src/scripts/jamesEOE/test/MergeBothMetadatas.java
+++ src/scripts/jamesEOE/test/MergeBothMetadatas.java
@@ -1,8 +1,10 @@
 package scripts.jamesEOE.test;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.util.HashMap;
 
 import parsers.NewRDPNode;
@@ -11,7 +13,7 @@ import utils.ConfigReader;
 
 public class MergeBothMetadatas
 {
-	
+
 	public static void main(String[] args) throws Exception
 	{
 		for( int x=1; x < NewRDPParserFileLine.TAXA_ARRAY.length; x++)
@@ -31,12 +33,22 @@ public class MergeBothMetadatas
 		
 		HashMap<String, Integer> eMap = EvanMetadataParser.getEvanCaseControlMap();
 		
+		BufferedWriter writer = new BufferedWriter( new FileWriter(new File(
+			ConfigReader.getJamesEoeDirectory() + File.separator + "test" + 
+				File.separator + "AF_merged_" + level + ".txt")));
+		
+		writer.write("id\tcaseControl\tactiveInactive\tsource");
 		
 		BufferedReader reader = new BufferedReader(new FileReader(new File(
 			ConfigReader.getJamesEoeDirectory() + File.separator + "test" + 
 				File.separator + level+ ".tsv")));
 		
-		reader.readLine();
+		String[] firstLine = reader.readLine().split("\t");
+		
+		for( int x=1; x < firstLine.length; x++)
+			writer.write("\t" + firstLine[x]);
+		
+		writer.write("\n");
 		
 		for(String s = reader.readLine(); s != null; s= reader.readLine())
 		{
@@ -44,22 +56,38 @@ public class MergeBothMetadatas
 			
 			String id = splits[0];
 			
+			writer.write(id + "\t" );
+			
 			if( bMap.containsKey(id))
 			{
+				BenitezMetadataParser bmp = bMap.get(id);
 				bMap.remove(id);
+				writer.write(bmp.getStudyGroup() + "\t" + bmp.getStatus() + "\tBenitez");
 			}
 			else if( eMap.containsKey(id))
 			{
+				Integer caseControl = eMap.get(id);
+				
+				writer.write(caseControl + "\tNA\tEvan");
+				
 				eMap.remove(id);
 			}
 			else
 			{
+				writer.write("NA\tNA\tNA");
 				System.out.println("Could not find " + id);
 			}
+			
+			for( int x=1; x < splits.length; x++)
+				writer.write("\t" + splits[x]);
+			
+			writer.write("\n");
 		}
 		
 		System.out.println("Bmap size after =  " + bMap.size());
 		System.out.println("Emap size after = " + eMap.size());
 		reader.close();
+		writer.flush();  writer.close();
+		
 	}
 }
