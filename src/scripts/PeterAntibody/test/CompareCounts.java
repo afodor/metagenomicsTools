2918084507a67ae595481dde79c73312579bc023
afodor <afodor@CCI7NR4PN2WWS.dyn.uncc.edu>
2018-08-26 21:38:09 -0400

    check count code running faster and passing

--- src/scripts/PeterAntibody/test/CompareCounts.java
+++ src/scripts/PeterAntibody/test/CompareCounts.java
@@ -11,8 +11,54 @@ import utils.ConfigReader;
 
 public class CompareCounts
 {
+	// outker key is file name
+	// inner key is position@aaChar
+	private static HashMap<String, HashMap<String,Integer>> getCountMap() throws Exception
+	{
+		HashMap<String, HashMap<String,Integer>>  map = new HashMap<>();
+		
+		HashMap<String, String> fileToCatMap = CountFeatures.getFileNameToCategoryMap();
+		
+		for(String fileName : fileToCatMap.keySet() )
+		{
+			System.out.println(fileName);
+			File inFile =new File(ConfigReader.getPeterAntibodyDirectory() + File.separator + 
+					"oneAtATime" + File.separator +   "inputFiles" + File.separator + "Aug_1" + 
+							File.separator + fileName);
+			
+			HashMap<String,Integer> innerMap = new HashMap<>();
+			map.put(fileName, innerMap);
+		
+			BufferedReader reader = new BufferedReader(new FileReader(inFile));
+			
+			for(String s= reader.readLine(); s != null; s= reader.readLine())
+			{
+				StringTokenizer sToken = new StringTokenizer(s);
+				
+				if( sToken.countTokens() == 2)
+				{
+					String key = sToken.nextToken() + "@" + Character.toUpperCase(sToken.nextToken().charAt(0));
+					
+					Integer count = innerMap.get(key);
+					
+					if(count == null)
+						count =0;
+					
+					count++;
+					
+					innerMap.put(key, count);
+				}
+			}
+		
+		}
+		
+		
+		return map;
+	}
+	
 	public static void main(String[] args) throws Exception
 	{
+		HashMap<String, HashMap<String,Integer>> countMap =getCountMap();
 		HashMap<String, String> fileToCatMap = CountFeatures.getFileNameToCategoryMap();
 		
 		BufferedReader reader = new BufferedReader(new FileReader(new File(
@@ -31,48 +77,29 @@ public class CompareCounts
 			int observed = 0;
 			for(String fileName : fileToCatMap.keySet() )
 			{
+				HashMap<String,Integer> innerMap = countMap.get(fileName);
+				
 				if( fileToCatMap.get(fileName).equals(classificaiton))
 				{
 					File inFile =new File(ConfigReader.getPeterAntibodyDirectory() + File.separator + 
 							"oneAtATime" + File.separator +   "inputFiles" + File.separator + "Aug_1" + 
 									File.separator + fileName);
-					observed += getObserved(inFile, position, aa);
+					
+					Integer aNum = innerMap.get(position + "@" + aa );
+					if( aNum==null)
+						aNum=0;
+					observed += aNum;
 				}
 			}
-			
-			System.out.println(classificaiton + " " + position + "  " + aa + " " + observed );
 			
 			if( observed != expected)
 				throw new Exception("Fail " + observed + " " + expected);
 			
+
+			System.out.println(classificaiton + " " + position + "  " + aa + " " + observed + " " + expected );
+			
 		}
 		
 		System.out.println("Pass");
-	}
-	
-	private static int getObserved(File inFile, String position, char aaChar)
-		throws Exception
-	{
-		BufferedReader reader = new BufferedReader(new FileReader(inFile));
-		
-		int observed =0;
-		
-		for(String s= reader.readLine(); s != null; s= reader.readLine())
-		{
-			StringTokenizer sToken = new StringTokenizer(s);
-			
-			if( sToken.countTokens() >= 2)
-			{
-				if( sToken.nextToken().equals(position))
-				{
-					char aChar =sToken.nextToken().charAt(0);
-					
-					if( Character.toUpperCase(aChar)==aaChar)
-						observed++;
-				}
-			}
-		}
-		
-		return observed;
 	}
 }
