e61d7e28625c5fa4932fee02c66672c569f2b8ee
afodor <afodor@CCI7NR4PN2WWS.dyn.uncc.edu>
2018-09-02 11:50:35 -0400

    add parsing of date line

--- src/scripts/hansenAlleleData/PivotOut.java
+++ src/scripts/hansenAlleleData/PivotOut.java
@@ -16,10 +16,12 @@ public class PivotOut
 		File inFile = new File(ConfigReader.getHansenAlleleDirectory() + File.separator + 
 				"ecoli.txt");
 		
-		HashMap<Integer, String> myMap =getWtKoMap(inFile);
+		HashMap<Integer, String> myWTMap =getWtKoMap(inFile);
+
+		HashMap<Integer, String> myDateMap =getDateMap(inFile);
 		
-		for(Integer i : myMap.keySet())
-			System.out.println(i + " " + myMap.get(i));
+		for(Integer i : myWTMap.keySet())
+			System.out.println(i + " " + myWTMap.get(i) + " " + myDateMap.get(i));
 	}
 	
 	private static HashMap<Integer,String> getWtKoMap(File f) throws Exception
@@ -43,6 +45,42 @@ public class PivotOut
 			
 			map.put(x, aString);
 			x++;
+		}
+		
+		reader.close();
+		
+		return map;
+	}
+	
+
+	private static HashMap<Integer,String> getDateMap(File f) throws Exception
+	{
+		HashMap<Integer,String> map = new LinkedHashMap();
+		
+		BufferedReader reader = new BufferedReader(new FileReader(f));
+		reader.readLine();
+		
+		int x=0;
+		
+		TabReader tReader =new TabReader(reader.readLine());
+		
+		String aString = "NA";
+		
+		while(tReader.hasMore())
+		{
+			String aVal = tReader.nextToken();
+			
+			if( aVal.trim().length() > 0)
+				aString = aVal;
+			
+			map.put(x, aString.replace("Day ", ""));
+			x++;
+		}
+		
+		for( Integer i : map.keySet())
+		{
+			if( map.get(i).indexOf("/") != -1)
+				map.put(i, map.get(i+1));
 		}
 		
 		reader.close();
