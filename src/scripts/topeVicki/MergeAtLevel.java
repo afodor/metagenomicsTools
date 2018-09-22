dcea57248179b12acc15f1afdae1e7869735198a
afodor <afodor@CCI7NR4PN2WWS.dyn.uncc.edu>
2018-09-22 17:49:25 -0400

    still need to write file..

--- src/scripts/topeVicki/MergeAtLevel.java
+++ src/scripts/topeVicki/MergeAtLevel.java
@@ -9,16 +9,30 @@ import java.util.List;
 import java.util.Map;
 import java.util.StringTokenizer;
 
+import utils.ConfigReader;
+
 public class MergeAtLevel
 {
-	public static void main(String[] args)
+	public static void main(String[] args) throws Exception
 	{
+		String[] levels = { "p", "c","o","f","g" };
 		
+		for(String level : levels)
+		{
+			mergeAtLevel(level);
+		}
 	}
 	
 	private static Map<String, Map<String,Integer>> mergeAtLevel(String level ) throws Exception
 	{
+		System.out.println(level);
 		Map<String, Map<String,Integer>> map = new HashMap<>();
+		
+		File file1 = new File(ConfigReader.getTopeVickiDir() + File.separator + "raw1.txt");
+		File file2 = new File(ConfigReader.getTopeVickiDir() + File.separator + "raw2.txt");
+		
+		addToMap(map, file1, level);
+		addToMap(map, file2, level);
 		
 		return map;
 	}
@@ -35,7 +49,26 @@ public class MergeAtLevel
 		{
 			String taxa = getTaxa(s, level);
 			
-			
+			if( taxa != null)
+			{
+				String[] splits = s.split("\t");
+				
+				if( splits.length != topSplits.length)
+					throw new Exception("No");
+				
+				for( int x=1; x < splits.length-1; x++)
+				{
+					Map<String,Integer> innerMap = map.get(topSplits[x]);
+					
+					if( innerMap == null)
+					{
+						innerMap = new HashMap<>();
+						map.put(topSplits[x], innerMap);						
+					}
+					
+					innerMap.put(taxa, Integer.parseInt(splits[x]));
+				}
+			}
 		}
 		
 		reader.close();
@@ -50,19 +83,22 @@ public class MergeAtLevel
 		
 		while(sToken.hasMoreTokens())
 		{
-			String taxa =sToken.nextToken();
+			String taxa =sToken.nextToken().trim();
+			
+			if( taxa.equals("Unassigned"))
+				return null;
 			
 			if( taxa.startsWith(level + "__"))
 			{
 				taxa = taxa.replace(level + "__", "" ).trim();
 				
-				if( taxa.length() == 0 )
+				if( taxa.length() == 0 || taxa.equals("Unassigned") )
 					return null;
 				
 				return taxa;
 			}
 		}
 		
-		throw new Exception("Could not find " + level + " " +last);
+		return null;
 	}
 }
