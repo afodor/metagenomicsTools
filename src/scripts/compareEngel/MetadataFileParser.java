70ee4abda0bb905516e4dd454f8b3b431bd8fe07
Anthony Fodor <anthony.fodor@gmail.com>
2019-06-07 17:54:23 -0400

    towards meta merge

--- src/scripts/compareEngel/MetadataFileParser.java
+++ src/scripts/compareEngel/MetadataFileParser.java
@@ -10,21 +10,66 @@ import utils.ConfigReader;
 
 public class MetadataFileParser
 {
-	
-	
-	/*
-	private final float eupct;  // european ancestry
-	private final float yripct;  // african ancestry
+	private final String sampleID;
+	private final Float eupct;  // european ancestry
+	private final Float yripct;  // african ancestry
 	private final int c_mrace; // maternal race
 	private final int c_case2; // preterm/term
-	*/
 	
+	public String getSampleID()
+	{
+		return sampleID;
+	}
+
+	public Float getEupct()
+	{
+		return eupct;
+	}
+
+	public Float getYripct()
+	{
+		return yripct;
+	}
+
+	public int getC_mrace()
+	{
+		return c_mrace;
+	}
+
+	public int getC_case2()
+	{
+		return c_case2;
+	}
+
 	public static void main(String[] args) throws Exception
 	{
-		getMetaMap();
+		HashMap<String, MetadataFileParser> map =  getMetaMap();
+		
+		for(String s: map.keySet())
+			System.out.println(s + " " + map.get(s).sampleID + " " + map.get(s).c_case2);
+		
 	}
 	
 	private final static String FILE_NAME = "PIN0108_MicrobCov_NoSNP_20190607.txt";
+	
+	private MetadataFileParser(String s,HashMap<String,Integer> topLineMap)
+	{
+		String[] splits = s.split("\t");
+		
+		this.sampleID = splits[0];
+		this.eupct = getFloatOrNull(splits[topLineMap.get("EUpct")]);
+		this.yripct = getFloatOrNull(splits[topLineMap.get("YRIpct")]);
+		this.c_mrace= Integer.parseInt(splits[topLineMap.get("C_MRACE")]);
+		this.c_case2= Integer.parseInt(splits[topLineMap.get("C_CASE2")]);
+	}
+	
+	private static Float getFloatOrNull( String s )
+	{
+		if( s.length() >0)
+			return Float.parseFloat(s);
+		
+		return null;
+	}
 	
 	public static HashMap<String, MetadataFileParser> getMetaMap() throws Exception
 	{
@@ -32,8 +77,24 @@ public class MetadataFileParser
 		
 		HashMap<String,Integer> topLineMap= getColumnIndexMap();
 		
-		for(String s : topLineMap.keySet())
-			System.out.println(s + " " + topLineMap.get(s));
+		
+		BufferedReader reader = new BufferedReader(new FileReader(new File(
+			ConfigReader.getEngelCheckDir() + File.separator + 
+						FILE_NAME)));
+		
+		reader.readLine();
+		
+		for(String s= reader.readLine(); s != null; s= reader.readLine())
+		{
+			MetadataFileParser mfp = new MetadataFileParser(s, topLineMap);
+			
+			if( map.containsKey(mfp.sampleID))
+				throw new Exception("Duplicate "  + mfp.sampleID);
+			
+			map.put(mfp.sampleID, mfp);
+		}
+		
+		reader.close();
 		
 		return map;
 	}
