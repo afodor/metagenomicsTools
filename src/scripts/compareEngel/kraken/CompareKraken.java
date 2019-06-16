1c64eeb034e2064f9002c26b09f2bdd7ae76a020
afodor <afodor@CCI7NR4PN2WWS.dyn.uncc.edu>
2019-06-16 13:18:52 -0400

    ready to check...

--- src/scripts/compareEngel/kraken/CompareKraken.java
+++ src/scripts/compareEngel/kraken/CompareKraken.java
@@ -5,6 +5,8 @@ import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.StringTokenizer;
 
 import javax.annotation.processing.Filer;
 import javax.lang.model.element.Element;
@@ -28,13 +30,33 @@ public class CompareKraken
 			
 			OtuWrapper wrapper = new OtuWrapper(ConfigReader.getEngelCheckDir() + File.separator + 
 					"racialDisparityKraken2_2019Jun03_taxaCount_" + level + ".tsv");
+			
+			for(String sample : wrapper.getSampleNames())
+			{
+				
+				String sampleName = new StringTokenizer(sample, "-").nextToken();
+				
+				File fileCheck = new File(ConfigReader.getEngelCheckDir() + File.separator + "krakenCheck" + 
+										File.separator + sampleName + "_reported.tsv");
+				
+				if( fileCheck.exists())
+				{
+					HashMap<String, Long> map = getExpectedAtLevel(fileCheck, "" + level.charAt(0));
+					//System.out.println("Found " + sampleName);
+				}
+				else
+				{
+					// some control samples that should not have been included in original sequences
+					System.out.println("Could not find " + sampleName);
+				}			
+			}
 		}
 		
 	}
 	
 	private static HashMap<String, Long> getExpectedAtLevel(File file, String level) throws Exception
 	{
-		HashMap<String, Long> map = new HashMap<>();
+		HashMap<String, Long> map = new LinkedHashMap<>();
 		
 		BufferedReader reader = new BufferedReader(new FileReader(file));
 		
