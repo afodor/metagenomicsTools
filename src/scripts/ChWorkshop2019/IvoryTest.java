8e92723166dbec7d8c94c46286f9cee22bde991f
afodor <afodor@CCI7NR4PN2WWS.dyn.uncc.edu>
2019-10-14 13:54:37 -0400

    second round of testing

--- src/scripts/ChWorkshop2019/IvoryTest.java
+++ src/scripts/ChWorkshop2019/IvoryTest.java
@@ -14,13 +14,65 @@ public class IvoryTest
 	public static void main(String[] args) throws Exception
 	{
 		File inFile = new File( "C:\\chWorkshop\\190709-DORSEY\\03_countsTables_txtFormat\\190709-DORSEY-txt\\taxa-table.txt");
-		
-		
+		File ivoryFile =new File("C:\\chWorkshop\\190709-DORSEY\\04_statistics\\190709-DORSEY_normalizedCounts.txt");
 		
 		HashMap<String, HashMap<String,Long>> map = getCounts(inFile);
+		HashMap<String, HashMap<String,Double>> ivoryMap = parseIvory(ivoryFile);
+		
+		List<String> aList = new ArrayList<>(map.keySet());
+		List<String> anotherList = new ArrayList<>(ivoryMap.keySet());
+		
+		System.out.println(aList.size());
+		System.out.println(anotherList.size());
+		
+		if( ! aList.equals(anotherList))
+			System.out.println("No");
 	}
 	
+	private static HashMap<String, HashMap<String,Double>> parseIvory(File inFile ) throws Exception
+	{
+		HashMap<String, HashMap<String,Double>> map = new HashMap<>();
+		
+		BufferedReader reader = new BufferedReader(new FileReader(inFile));
+		
+		String topLine = reader.readLine();
+		
+		List<String> taxa  = new ArrayList<>();
+		
+		String[] topSplits = topLine.split("\t");
+		
+		for(int x=1; x < topSplits.length;x++)
+		{
+			taxa.add(topSplits[x]);
+		}
+		
+		for(String s= reader.readLine(); s != null; s= reader.readLine())
+		{
+			String[] splits =s.split("\t");
+			
+			String sample= splits[0];
+			
+			HashMap<String,Double> innerMap = new HashMap<>();
+			map.put(sample, innerMap);
+			
+			int index=1;
+			for(String taxon : taxa)
+			{
+				if(innerMap.containsKey(taxon))
+					throw new Exception("No");
+				
+				innerMap.put(taxon,  Double.parseDouble(splits[index]));
+				
+				index++;
+			}
+		}
+		
+		return map;
+	}
+	
+	
 	// outer key is sample;  inner key is taxa
+	
 	private static HashMap<String, HashMap<String,Long>> getCounts(File inFile) throws Exception
 	{
 		HashMap<String, HashMap<String,Long>> map = new HashMap<>();
