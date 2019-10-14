9c0cd3db70b5c541d1fb55559fbf37e5e0dd5a7b
afodor <afodor@CCI7NR4PN2WWS.dyn.uncc.edu>
2019-10-14 14:32:49 -0400

    pass through log

--- src/scripts/ChWorkshop2019/IvoryTest.java
+++ src/scripts/ChWorkshop2019/IvoryTest.java
@@ -11,6 +11,7 @@ import java.util.List;
 
 import javax.annotation.processing.Filer;
 
+import scripts.lactoCheck.Normalize;
 import utils.Avevar;
 
 public class IvoryTest
@@ -28,7 +29,62 @@ public class IvoryTest
 		System.out.println(totalMean);
 		
 		checkNormalization(map, ivoryMap, totalMean);
+		
+		File ivoryLog = new File("C:\\chWorkshop\\190709-DORSEY\\04_statistics\\190709-DORSEY_log10NormalizedCounts_withMeta.txt");
+		
+		HashMap<String, HashMap<String,Double>> ivoryLogNorm = parseIvoryLogNorm(ivoryLog);
+		checkLog(ivoryMap, ivoryLogNorm);
+		
+		System.out.println("Global pass");
 	}
+	
+	
+
+	private static void checkLog(HashMap<String, HashMap<String,Double>> map ,
+			HashMap<String, HashMap<String,Double>> logMap) throws Exception
+	{
+		List<String> aList = new ArrayList<>(map.keySet());
+		List<String> anotherList = new ArrayList<>(logMap.keySet());
+		
+		System.out.println(aList.size());
+		System.out.println(anotherList.size());
+
+		Collections.sort(aList);
+		Collections.sort(anotherList);
+		
+		if( ! aList.equals(anotherList))
+			throw new Exception("No");
+		
+		for(String sample: aList)
+		{
+			HashMap<String, Double> mapInner= map.get(sample);
+			HashMap<String, Double> logInner = logMap.get(sample);
+			
+			List<String> bList = new ArrayList<>(mapInner.keySet());
+			List<String> cList = new ArrayList<>(logInner.keySet());
+			
+			Collections.sort(bList);
+			Collections.sort(cList);
+			
+			if( ! bList.equals(cList))
+				throw new Exception("No");
+			
+			for( String s : mapInner.keySet() )
+			{
+				Double mapVal = logInner.get(s);
+				
+				Double expectedVal = Math.log10(mapInner.get(s));
+				
+				if( Math.abs(mapVal- expectedVal) > 0.0001)
+					throw new Exception("No " + mapVal+ " " + expectedVal + " " + sample + " "+ s);
+				
+				//System.out.println(mapVal +  " " + expectedVal);
+			}
+			
+		}
+	}
+
+	
 	
 	private static void checkNormalization(HashMap<String, HashMap<String,Long>> map ,
 			HashMap<String, HashMap<String,Double>> ivoryMap, double totalMean ) throws Exception
@@ -142,6 +198,47 @@ public class IvoryTest
 		return map;
 	}
 	
+	
+	private static HashMap<String, HashMap<String,Double>> parseIvoryLogNorm(File inFile ) throws Exception
+	{
+		HashMap<String, HashMap<String,Double>> map = new LinkedHashMap<>();
+		
+		BufferedReader reader = new BufferedReader(new FileReader(inFile));
+		
+		String topLine = reader.readLine();
+		
+		List<String> taxa  = new ArrayList<>();
+		
+		String[] topSplits = topLine.split("\t");
+		
+		for(int x=53; x < topSplits.length;x++)
+		{
+			taxa.add(topSplits[x]);
+		}
+		
+		for(String s= reader.readLine(); s != null; s= reader.readLine())
+		{
+			String[] splits =s.split("\t");
+			
+			String sample= splits[0];
+			
+			HashMap<String,Double> innerMap = new LinkedHashMap<>();
+			map.put(sample, innerMap);
+			
+			int index=53;
+			for(String taxon : taxa)
+			{
+				if(innerMap.containsKey(taxon))
+					throw new Exception("No");
+				
+				innerMap.put(taxon,  Double.parseDouble(splits[index]));
+				
+				index++;
+			}
+		}
+		
+		return map;
+	}
 	
 	// outer key is sample;  inner key is taxa
 	
