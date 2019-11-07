36c4b27e202b8f4398c5ea0fa9b0298428ddc97c
afodor <afodor@CCI7NR4PN2WWS.dyn.uncc.edu>
2019-11-07 13:43:30 -0500

    finding duplicate

--- src/scripts/ChWorkshop2019/TaxaMerger.java
+++ src/scripts/ChWorkshop2019/TaxaMerger.java
@@ -21,9 +21,13 @@ public class TaxaMerger
 	
 	public static void main(String[] args) throws Exception
 	{
+
+		File inFile = new File( "C:\\chWorkshop\\190709-DORSEY\\03_countsTables_txtFormat\\190709-DORSEY-txt\\taxa-table.txt");
+		
+		
 		for(String t : TAXA_LEVELS)
 		{
-			HashMap<String, HashMap<String,Integer> > map  = parseAtLevel(t);
+			HashMap<String, HashMap<String,Integer> > map  = parseAtLevel(t, inFile);
 			
 			/*
 			for(String s : map.keySet())
@@ -36,8 +40,71 @@ public class TaxaMerger
 			}
 			*/
 			writeFile(map, t);
+			writeNonUniqueLeftStrings(map, t, inFile);
 			System.out.println(t);
 		}
+		
+	}
+	
+	private static void writeNonUniqueLeftStrings( HashMap<String, HashMap<String,Integer> > map, String taxonomicLevel,
+			File inFile)
+		throws Exception
+	{
+		HashSet<String> otus = new HashSet();
+		
+		for(String s : map.keySet())
+			for(String s2 : map.get(s).keySet())
+				otus.add(s2);
+		
+		List<String> otuList =new ArrayList<>(otus);
+		Collections.sort(otuList);
+		
+		// key is otu id; value is everything to the left of that OTU id
+		HashMap<String, HashSet<String>> duplicateMap = new LinkedHashMap<>();
+		
+		BufferedReader reader = new BufferedReader(new FileReader(inFile));
+		
+		reader.readLine();
+		
+		for(String s= reader.readLine(); s != null; s= reader.readLine())
+		{
+			String[] splits =s.split("\t");
+			
+			String taxaString = splits[splits.length-2];
+			
+			String taxaID = getTaxaIDOrNull(taxaString, taxonomicLevel);
+			
+			if( taxaID != null)
+			{
+				HashSet<String> set = duplicateMap.get(taxaID);
+				
+				if( set == null)
+				{
+					set = new HashSet<>();
+					duplicateMap.put(taxaID, set);
+				}
+				
+				String leftString = taxaString.substring(0, taxaString.indexOf(taxonomicLevel + "__"));
+				set.add(leftString);
+				
+			}
+		}
+		
+		reader.close();
+		
+		BufferedWriter writer = new BufferedWriter(new FileWriter(new File(
+				"C:\\chWorkshop\\190709-DORSEY\\03_countsTables_txtFormat\\190709-DORSEY-txt\\" + 
+						  taxonomicLevel + "_nonUniqueLeftString.txt")));
+		
+		writer.write("taxa\tleftString\n");
+		
+		for(String s : duplicateMap.keySet())
+		{
+			if(duplicateMap.get(s).size() > 1 )
+				writer.write( s + "\t" + duplicateMap.get(s) + "\n");
+		}
+		
+		writer.flush();  writer.close();
 		
 	}
 	
@@ -94,11 +161,9 @@ public class TaxaMerger
 	 * the outer key is sampleID; the inner key is taxaId
 	 * 
 	 * */
-	private static HashMap<String, HashMap<String,Integer> > parseAtLevel( String taxaLeve) throws Exception
+	private static HashMap<String, HashMap<String,Integer> > parseAtLevel( String taxaLevel, File inFile) throws Exception
 	{
 		HashMap<String, HashMap<String,Integer> > map = new LinkedHashMap();
-		
-		File inFile = new File( "C:\\chWorkshop\\190709-DORSEY\\03_countsTables_txtFormat\\190709-DORSEY-txt\\taxa-table.txt");
 		
 		BufferedReader reader = new BufferedReader(new FileReader(inFile));
 		
@@ -115,7 +180,7 @@ public class TaxaMerger
 				throw new Exception();
 			
 			String taxaString = splits[splits.length-2];
-			String taxaKey = getTaxaIDOrNull(taxaString, taxaLeve);
+			String taxaKey = getTaxaIDOrNull(taxaString, taxaLevel);
 			if( taxaKey != null)
 			{
 
@@ -138,6 +203,7 @@ public class TaxaMerger
 			}
 		}
 		
+		reader.close();
 		return map;
 	}
 	
