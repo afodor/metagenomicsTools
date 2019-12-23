083098769f212d9443ef8372222dbe6c76d4011f
Anthony Fodor <anthony.fodor@gmail.com>
2019-12-23 10:03:35 -0500

    merging in

--- src/scripts/jamesEOE/test/MergeBothDatasetPValues.java
+++ src/scripts/jamesEOE/test/MergeBothDatasetPValues.java
@@ -5,7 +5,6 @@ import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileReader;
 import java.io.FileWriter;
-import java.io.IOException;
 import java.util.HashMap;
 
 public class MergeBothDatasetPValues
@@ -14,6 +13,7 @@ public class MergeBothDatasetPValues
 	{
 		Double evanPValue = null;
 		Double benitezPValue = null;
+		Double qiimePValue = null;
 	}
 	
 	public static void main(String[] args) throws Exception
@@ -21,7 +21,34 @@ public class MergeBothDatasetPValues
 		HashMap<String, Holder> map = new HashMap<>();
 		addToMap(map, new File("C:\\JamesEOE\\test\\genusaf_FirstModelsEvan.txt"), true);
 		addToMap(map, new File("C:\\JamesEOE\\test\\genusaf_FirstModelsBenitez.txt"), false);
+		addQiimeToMap(new File("C:\\JamesEOE\\Qiime_EoE_genus_Case_Control_models.tsv"), map);
 		writeResults(map);
+	}
+	
+	private static void addQiimeToMap(File file,HashMap<String, Holder> map ) throws Exception
+	{
+		BufferedReader reader = new BufferedReader(new FileReader(file));
+		
+		reader.readLine();
+		
+		for(String s= reader.readLine(); s != null; s = reader.readLine())
+		{
+			String[] splits =s.split("\t");
+			
+			String key = splits[0].replaceAll("\"", "").trim();
+			Holder h = map.get(key);
+			
+			if( h == null)
+			{
+				h = new Holder();
+				map.put(key, h);
+			}
+			
+			if( h.qiimePValue != null)
+				throw new Exception("No");
+			
+			h.qiimePValue = Double.parseDouble(splits[1]);
+		}
 	}
 	
 	private static String getValOrNA( Double d  )
@@ -37,7 +64,7 @@ public class MergeBothDatasetPValues
 	{
 		BufferedWriter writer =new BufferedWriter(new FileWriter(new File("C:\\JamesEOE\\test\\genusMerged.txt")));
 		
-		writer.write("taxa\tevanP\tBenitezP\n");
+		writer.write("taxa\tevanP\tBenitezP\tqiimeEvanP\n");
 		
 		for(String s: map.keySet())
 		{
@@ -46,7 +73,8 @@ public class MergeBothDatasetPValues
 			writer.write(s + "\t");
 			
 			writer.write( getValOrNA(h.evanPValue) + "\t" );
-			writer.write( getValOrNA(h.benitezPValue) + "\n" );
+			writer.write( getValOrNA(h.benitezPValue) + "\t" );
+			writer.write(getValOrNA(h.qiimePValue) + "\n");
 		}
 		
 		
@@ -63,17 +91,19 @@ public class MergeBothDatasetPValues
 		{
 			String[] splits = s.split("\t");
 			
-			Holder h = map.get(splits[1]);
+			String key = splits[1].trim().replaceAll("\"", "");
+			Holder h = map.get(key);
 			
-			double pValue = Math.log10( Double.parseDouble(splits[0]));
+			double pValue = Double.parseDouble(splits[0]);
+			//double pValue = Math.log10( Double.parseDouble(splits[0]));
 			
-			if( Double.parseDouble(splits[5]) > Double.parseDouble(splits[4]) )
-				pValue = -pValue;
+			//if( Double.parseDouble(splits[5]) > Double.parseDouble(splits[4]) )
+				//pValue = -pValue;
 			
 			if( h== null)
 			{
 				h = new Holder();
-				map.put(splits[1], h);
+				map.put(key, h);
 			}
 			
 			if( isEvan )
