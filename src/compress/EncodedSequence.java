aed769b6b4621f4df542f5a1d606f75ddd84bafe
Anthony Fodor <anthony.fodor@gmail.com>
2020-10-18 22:46:13 -0400

    bug fixes with .equals

--- src/compress/EncodedSequence.java
+++ src/compress/EncodedSequence.java
@@ -17,6 +17,16 @@ public class EncodedSequence
 		this.encodedSequence = EncodeACGT.encode(dnaSeq);
 	}
 	
+	public int getOriginalLength()
+	{
+		return originalLength;
+	}
+	
+	public String getEncodedSequence()
+	{
+		return encodedSequence;
+	}
+	
 	public String decode() 
 	{
 		return EncodeACGT.decode(encodedSequence, originalLength);
@@ -32,29 +42,48 @@ public class EncodedSequence
 	public boolean equals(Object obj)
 	{
 		EncodedSequence other = (EncodedSequence) obj;
-		return this.encodedSequence.equals(other.encodedSequence);
+		return this.encodedSequence.equals(other.encodedSequence) && this.originalLength == other.originalLength;
 	}
 	
 	@Override
 	public int hashCode()
 	{
-		return this.encodedSequence.hashCode();
+		return this.encodedSequence.hashCode() + this.originalLength;
 	}
 	
 	public static void main(String[] args) throws Exception
 	{
+
+		EncodedSequence esZero = new EncodedSequence("");
+		
+		// should be 0
+		System.out.println(esZero.decode().length());
+		
 		HashSet<EncodedSequence> set= new LinkedHashSet<EncodedSequence>();
 		
 		String s1 = "ACCCTTTTAACCCCCCCCCCCGGGTTTAAA";
 		String s2 = "ACTGACGT";
+		String s3 = "A";
+		String s4 = "AA";
 		
+		EncodedSequence esA = new EncodedSequence(s3);
+		EncodedSequence esAA = new EncodedSequence(s4);
+		
+		// should be "A" "AA"
+		System.out.println(esA.toString() + " " + esAA.toString());
+		
+		
+		//should be false
+		System.out.println(esA.equals(esAA));
 		
 		set.add(new EncodedSequence(s1));
 		set.add(new EncodedSequence(s1));
 		set.add(new EncodedSequence(s2));
 		set.add(new EncodedSequence(s2));
+		set.add(esA);	
+		set.add(esAA);
 		
-		// should be 2
+		// should be 4
 		System.out.println(set.size());
 		
 		// should be true
