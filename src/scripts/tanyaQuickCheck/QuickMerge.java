1d4af884644eb3d1448e416a631337c2306264ef
Anthony Fodor <anthony.fodor@gmail.com>
2021-03-03 17:37:02 -0500

    getting ready to merger

--- src/scripts/tanyaQuickCheck/QuickMerge.java
+++ src/scripts/tanyaQuickCheck/QuickMerge.java
@@ -1,33 +1,101 @@
 package scripts.tanyaQuickCheck;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.util.HashMap;
 import java.util.LinkedHashMap;
 
+import parsers.OtuWrapper;
 import utils.TabReader;
 
 public class QuickMerge
 {
-	public static String[] LEVELS = {"phylum", "class", "order", "family", "species"};
+	public static String[] LEVELS = {"phylum"};
+	
+	
+	//public static String[] LEVELS = {"phylum", "class", "order", "family", "species"};
 	
 	public static void main(String[] args) throws Exception
 	{
-	
-		getMetaboliteLines( new File( "C:\\tanyaQuickRep\\pos_pre.txt"));
+		File preFile =  new File( "C:\\tanyaQuickRep\\pos_pre.txt");
 		
-		/*
+		HashMap<String, String> metaboliteLines =  getMetaboliteLines(preFile);
+		
+		
 		for(String level : LEVELS)
 		{
 			System.out.println(level);
 			OtuWrapper wrapper = new OtuWrapper("C:\\tanyaQuickRep\\tanya_kraken_"+ level +  ".txt");
 			
-			wrapper.writeLoggedDataWithTaxaAsColumns( new File( "C:\\tanyaQuickRep\\tanya_kraken_"+ level +  "logNorm.txt"));
+			File loggedFile =  new File( "C:\\tanyaQuickRep\\tanya_kraken_"+ level +  "logNorm.txt");
+			
+			wrapper.writeLoggedDataWithTaxaAsColumns(loggedFile);
+			
+			writeMergedFile(loggedFile, metaboliteLines, preFile, level, "pre");
 		}
-		*/
+		
 	}
 	
+	private static String getTopLine(File file) throws Exception
+	{
+		BufferedReader reader =new BufferedReader(new FileReader(file));
+	
+		String s= reader.readLine();
+		
+		reader.close();
+		
+		return s;
+	}
+	
+	private static void writeMergedFile( File loggedFile, HashMap<String, String> metaboliteLines , File metaboliteFile, String level, String prePostString )
+		throws Exception
+	{
+		String[] topLineMetabolites = getTopLine(metaboliteFile).split("\t");
+		
+		BufferedReader reader = new BufferedReader(new FileReader(loggedFile));
+		
+		BufferedWriter  writer = new BufferedWriter(new FileWriter( new File("C:\\tanyaQuickRep\\" + level + "_logged_" + prePostString + ".txt")));
+		
+		writer.write("id");
+		
+		String[] firstTaxaLine = reader.readLine().split("\t");
+		
+		for( int x=1; x < firstTaxaLine.length; x++)
+			writer.write("\t" + firstTaxaLine[x]);
+		
+		for( int x=1; x < topLineMetabolites.length; x++)
+			writer.write("\t" + topLineMetabolites[x]);
+		
+		writer.write("\n");
+		
+
+		int found =0;
+		for(String s = reader.readLine(); s != null; s= reader.readLine())
+		{
+			String[] splits = s.split("\t");
+			
+			String metaboliteLine = metaboliteLines.get(splits[0]);
+			
+		
+			if( metaboliteLine == null)
+			{
+				System.out.println("Could not find " + splits[0]);
+			}
+			else
+			{
+				System.out.println("Found" + splits[0]);
+				found++;
+			}
+		}
+		
+		System.out.println("Found " + found);
+		writer.close();
+		
+		reader.close();
+	}
 	
 	/*
 	 * Key is going to be the microbiome id; 
@@ -56,17 +124,19 @@ public class QuickMerge
 			
 			if( microbeID == null) 
 			{
-				System.out.println("Could not find " +key );
+				//System.out.println("Could not find " +key );
 			}
 			else
 			{
-				System.out.println("Found " + microbeID);
+				///System.out.println("Found " + microbeID);
 				numFound++;
+				
+				map.put(microbeID, s);
 			}
 				
 		}
 		
-		System.out.println("Found " + numFound);
+		//System.out.println("Found " + numFound);
 		reader.close();
 		
 		return map;
