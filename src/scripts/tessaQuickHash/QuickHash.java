c4144fc944995875b81006558b275f6c4e24bf75
Anthony Fodor <anthony.fodor@gmail.com>
2021-06-10 16:57:15 -0400

    have patient records

--- src/scripts/tessaQuickHash/QuickHash.java
+++ src/scripts/tessaQuickHash/QuickHash.java
@@ -4,6 +4,7 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.util.HashMap;
 import java.util.List;
+import java.util.StringTokenizer;
 
 public class QuickHash
 {
@@ -19,10 +20,35 @@ public class QuickHash
 		
 		BufferedReader reader = new BufferedReader(new FileReader("c:\\Temp\\apple.txt"));
 		
+		boolean isNewRecord = true;
+		
 		for(String s= reader.readLine(); s != null; s= reader.readLine())
 		{
-			System.out.println(s);
+			s = s.replaceAll("\"", "");
+			StringTokenizer sToken = new StringTokenizer(s);
+			sToken.nextToken();
+			
+			if( isNewRecord )
+			{
+				Integer subjectRecord = Integer.parseInt(sToken.nextToken());
+				
+				if( sToken.hasMoreTokens())
+					throw new Exception("Parsing error");
+				
+				System.out.println(subjectRecord);
+				
+				isNewRecord =false;
+			}
+			else
+			{
+				String nextToken = sToken.nextToken();
+				
+				if( nextToken.equals("break"))
+					isNewRecord =true;
+				
+			}
 		}
+		
 		
 		reader.close();
 		return map;
