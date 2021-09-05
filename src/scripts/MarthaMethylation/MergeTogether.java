f79fdb44000a49449feb094f5e9231485a9c29c8
afodor <afodor@DESKTOP-T44GFRG>
2021-09-05 15:53:20 -0400

    ready for statistical testing

---
+++ src/scripts/MarthaMethylation/MergeTogether.java
@@ -0,0 +1,74 @@
+package scripts.MarthaMethylation;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.util.HashMap;
+
+public class MergeTogether
+{
+	public static String PATHWAY_PLUS_META = "C:\\MarthaMethylation\\shanCountTables\\humann2LogNormPlusMeta.txt";
+	
+	public static void main(String[] args) throws Exception
+	{
+		HashMap<String, MgDefScoreParserLine>  map = MgDefScoreParserLine.getMgScoreMap();
+		HashMap<String, ShanMetadataParserLine> metaMap = ShanMetadataParserLine.getMetaMap();
+		
+		BufferedReader reader = new BufferedReader(new FileReader(new File(
+				MetaMergePathways.PATHWAY_FILE_PIVOTED_LOG_NORM
+				)));
+		
+		BufferedWriter writer =new BufferedWriter(new FileWriter(new File(PATHWAY_PLUS_META)));
+		
+		String[] topSplits = reader.readLine().split("\t");
+		
+		writer.write(topSplits[0] + "\tsubjectID\tBS_pred_methy\tBS_pred_methy_post\tmc_pred_methy\tmc_pred_methy_post");
+		
+		for( int x=1; x < topSplits.length; x++)
+			writer.write("\t" + topSplits[x]);
+		
+		writer.write("\n");
+		
+		for(String s = reader.readLine(); s != null; s = reader.readLine())
+		{
+			String[] splits = s.split("\t");
+			
+			writer.write(splits[0]);
+			
+			ShanMetadataParserLine smpl = metaMap.get(splits[0]);
+			
+			MgDefScoreParserLine mdspl = map.get(smpl.getStudyID());
+			
+			writer.write("\t" + smpl.getStudyID());
+			
+			if( mdspl == null)
+			{
+				writer.write("\tNA\tNA\tNA\tNA");
+			}
+			else
+			{
+				writer.write("\t" + getNAOrVal(mdspl.getBS_pred_methy()) +"\t" 
+							+ getNAOrVal( mdspl.getBS_pred_methy_post()) + "\t" + 
+							getNAOrVal(	mdspl.getMc_pred_methy())  + "\t" +
+							getNAOrVal(	mdspl.getMc_pred_methy_post()));
+			}
+			
+			for( int x=1; x < splits.length; x++)
+				writer.write("\t" + splits[x]);
+			
+			writer.write("\n");
+		}
+		
+		writer.flush();  writer.close();
+	}
+	
+	private static String getNAOrVal(Double d)
+	{
+		if( d== null)
+			return "NA";
+		
+		return d.toString();
+	}
+}
