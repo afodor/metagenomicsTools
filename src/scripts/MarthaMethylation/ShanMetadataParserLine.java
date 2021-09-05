6fd4279f2131eec03e0e0709f9249e7f68beaad9
afodor <afodor@DESKTOP-T44GFRG>
2021-09-05 12:26:09 -0400

    need mg parser...

--- src/scripts/MarthaMethylation/ShanMetadataParserLine.java
+++ src/scripts/MarthaMethylation/ShanMetadataParserLine.java
@@ -11,6 +11,31 @@ public class ShanMetadataParserLine
 	private final String sampleType;
 	private final String visit;
 	
+	public String getStudyID()
+	{
+		return studyID;
+	}
+
+	public String getSampleID()
+	{
+		return sampleID;
+	}
+
+	public String getSampleType()
+	{
+		return sampleType;
+	}
+
+	public String getVisit()
+	{
+		return visit;
+	}
+
+	public static String getMetadataFilePath()
+	{
+		return METADATA_FILE_PATH;
+	}
+
 	public static final String METADATA_FILE_PATH = "C:\\MarthaMethylation\\shanCountTables\\metadata_all.txt";
 	
 	private ShanMetadataParserLine(String s)
@@ -23,6 +48,14 @@ public class ShanMetadataParserLine
 		this.visit = splits[6];
 	}
 	
+	public static void main(String[] args) throws Exception
+	{
+		HashMap<String, ShanMetadataParserLine> metaMap = getMetaMap();
+		
+		for(String  s: metaMap.keySet())
+			System.out.println(s + " "+ metaMap.get(s).studyID);
+	}
+	
 	public static HashMap<String, ShanMetadataParserLine> getMetaMap() throws Exception
 	{
 		HashMap<String, ShanMetadataParserLine> map = new HashMap<>();
@@ -33,6 +66,13 @@ public class ShanMetadataParserLine
 		
 		for(String s= reader.readLine(); s != null; s= reader.readLine())
 		{
+			String[] splits = s.split("\t");
+			String sampleId = splits[2];
+			
+			if( map.containsKey(sampleId))
+				throw new Exception("Duplicate " + sampleId);
+			
+			map.put(sampleId, new ShanMetadataParserLine(s));
 			
 		}
 		
