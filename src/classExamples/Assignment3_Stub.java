5bd4fb246f097a74e437a1d203196c84f46dda56
Anthony Fodor <anthony.fodor@gmail.com>
2021-09-14 20:43:58 -0400

    stub for assignment 3

---
+++ src/classExamples/Assignment3_Stub.java
@@ -0,0 +1,36 @@
+package classExamples;
+
+public class Assignment3_Stub
+{
+	public static String generateRandomSequence(char[] alphabet, float[] weights, int length)
+	{
+		return null;
+	}
+	
+	
+	public static void main(String[] args) 
+	{
+		float[] dnaWeights = { .3f, .3f, .2f, .2f };
+		char[] dnaChars = { 'A', 'C', 'G', 'T'  };
+		
+		// a random DNA 30 mer
+		System.out.println(generateRandomSequence(dnaChars, dnaWeights,30));
+		
+		// background rate of residues from https://www.science.org/doi/abs/10.1126/science.286.5438.295
+		float proteinBackground[] =
+			{0.072658f, 0.024692f, 0.050007f, 0.061087f,
+		        0.041774f, 0.071589f, 0.023392f, 0.052691f, 0.063923f,
+		        0.089093f, 0.023150f, 0.042931f, 0.052228f, 0.039871f,
+		        0.052012f, 0.073087f, 0.055606f, 0.063321f, 0.012720f,
+		        0.032955f}; 
+			
+
+		char[] proteinResidues = 
+				new char[] { 'A', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'K', 'L', 'M', 'N', 'P', 'Q', 'R', 'S', 'T',
+							 'V', 'W', 'Y' };
+		
+		// a random protein with 30 residues
+		System.out.println(generateRandomSequence(proteinResidues, proteinBackground, 30));
+		
+	}
+}
