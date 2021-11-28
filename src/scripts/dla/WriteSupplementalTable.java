d5bca3dce4d139d2cb13353235bf78ac68370b63
Anthony Fodor <anthony.fodor@gmail.com>
2021-11-27 22:54:15 -0500

    towards ratios

--- src/scripts/dla/WriteSupplementalTable.java
+++ src/scripts/dla/WriteSupplementalTable.java
@@ -5,7 +5,9 @@ import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileReader;
 import java.io.FileWriter;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 
 public class WriteSupplementalTable
 {
@@ -14,10 +16,76 @@ public class WriteSupplementalTable
 		writeUnsortedTable();
 	}
 	
+	private static double getRatio(List<MetadataFileLine> list, String s, boolean vanderbilt)
+		throws Exception
+	{
+		List<Integer> numeratorIndex = new ArrayList<>();
+	
+		String[] splits = s.split("\t");
+		
+		if( splits.length != list.size())
+			throw new Exception("Parsing error " + splits.length + " " + list.size() + "\n" + s);
+		
+		for( int x=1; x < list.size(); x++)
+		{
+			if( vanderbilt && list.get(x).getPatient().equals("Vanderbilt-patient") )
+				numeratorIndex.add(x);
+			else if( ! vanderbilt && list.get(x).getPatient().equals("UNC-patient") )
+				numeratorIndex.add(x);
+			
+		}
+		
+		double numeratorSum =0;
+		
+		for( Integer i : numeratorIndex)
+		{
+			numeratorSum += Double.parseDouble(splits[i]);
+		}
+		
+		double numeratorAvg = numeratorSum / numeratorIndex.size();
+		
+		double denom = getExactlyOneVal(list, s, vanderbilt);
+		
+		if (denom == 0 )
+			return 0;
+		
+		double ratio = numeratorAvg/denom;
+		
+		if( ratio >0 &&  ratio < 1)
+			ratio = -1/ratio;
+			
+		return ratio;
+	}
+	
+	private static double getExactlyOneVal( List<MetadataFileLine> list, String s , boolean vanderbilt ) throws Exception
+	{
+		
+		Integer index = null;
+		
+		for( int x=1; x < list.size(); x++)
+		{
+			if( vanderbilt && list.get(x).getPatient().equals("Vanderbilt-donor") )
+				index = x;
+			else if( ! vanderbilt && list.get(x).getPatient().equals("UNC-donor") )
+				index = x;
+			
+		}
+		
+		if( index == null)
+			throw new Exception("Could not find ");
+		
+		String[] splits = s.split("\t");
+		
+		return Double.parseDouble(splits[index]);
+	}
+	
 	@SuppressWarnings("resource")
 	private static void writeUnsortedTable() throws Exception
 	{
 		HashMap<String, MetadataFileLine> map = MetadataFileLine.getMetaMap();
+		
+		List<MetadataFileLine> metaList = new ArrayList<>();
+		metaList.add(null);
 		
 		BufferedReader reader = new BufferedReader(new FileReader(new File(
 				"C:\\DLA_Analyses2021-main\\input\\hum"
@@ -42,10 +110,12 @@ public class WriteSupplementalTable
 			if( mfl== null)
 				throw new Exception("Could not find " + s);
 			
+			metaList.add(mfl);
+			
 			writer.write("\t" + mfl.getPatient() + "_" + mfl.getTimepoint());
 		}
 		
-		writer.write("\n");
+		writer.write("\tvanderbiltRatio\tuncRatio\taverageRatio\n");
 		
 		for(String s= reader.readLine(); s != null && s.trim().length() > 0; s= reader.readLine())
 		{
@@ -58,7 +128,14 @@ public class WriteSupplementalTable
 			for( int x=1; x < splits.length; x++)
 				writer.write("\t" + splits[x]);
 			
-			writer.write("\n");
+			double vanderbiltRatio = getRatio(metaList, s, true);
+			double chapelHillRatio = getRatio(metaList, s, false);
+			
+			writer.write("\t" + vanderbiltRatio + "\t" + chapelHillRatio + "\t");
+			
+			double averageRatio = ( vanderbiltRatio + chapelHillRatio ) / 2;
+			
+			writer.write(averageRatio + "\n");
 		}
 		
 		writer.flush();  writer.close();
