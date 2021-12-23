04691128af2a7200596368e440a886d0f81133e1
Anthony Fodor <anthony.fodor@gmail.com>
2021-12-23 14:12:29 -0500

    ready for models

--- src/scripts/AliBariatricSurgery/AddOneMonthDates.java
+++ src/scripts/AliBariatricSurgery/AddOneMonthDates.java
@@ -1,8 +1,10 @@
 package scripts.AliBariatricSurgery;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.Set;
@@ -10,11 +12,93 @@ import java.util.Set;
 public class AddOneMonthDates
 {
 	public static final File ALI_TOP_DIR = new File("C:\\bariatricSurgery_Daisy\\fromAli_Dec15_2021\\countTables");
+	public static final File FILE_OUTPUT_DIR = new File("C:\\bariatricSurgery_Daisy\\fromAli_Dec15_2021\\AF_Out");
+	
+	public static final int[] TIMEPOINTS = { 0,1,6,12,18,24 };
 	
 	public static void main(String[] args) throws Exception
 	{
 		 HashMap<String, HashMap<Integer,Double>> map = getPatientWeightMap();
 		 System.out.println(map);
+		 
+
+		String[] filesToParse = ALI_TOP_DIR.list();
+			
+		for(String s : filesToParse)
+			if( s.endsWith(".tsv"))
+			{
+				File inFile = new File( ALI_TOP_DIR + File.separator + s );
+				File outFile = new File( FILE_OUTPUT_DIR + File.separator + s.replace(".tsv", "") +"withPercentChange.txt" );
+				addMetadata(map, inFile, outFile);
+			}
+	}
+	
+	private static String getPercentChange( HashMap<Integer,Double> map , Integer timepoint2, Integer timepoint1 ) throws Exception
+	{
+		if( map == null)
+			return "NA";
+		
+		Double val1 = map.get(timepoint1);
+		Double val2 = map.get(timepoint2);
+		
+		if( val1 == null || val2 == null)
+			return "NA";
+		
+		double percentChange = 100.0 * (val2 - val1 ) / val1;
+		
+		return "" + percentChange;
+	}
+	
+	private static void addMetadata( HashMap<String, HashMap<Integer,Double>> map , File inFile, File outFile) throws Exception
+	{
+		System.out.println(inFile.getAbsolutePath());
+		BufferedReader reader = new BufferedReader(new FileReader(inFile));
+		
+		BufferedWriter writer = new BufferedWriter(new FileWriter(outFile));
+		
+		String[] topSplits = reader.readLine().split("\t");
+		
+		for(int x=0; x < 6; x++)
+			writer.write(topSplits[x] + "\t");
+		
+		writer.write("timepointInt");
+		
+		for( int x=1; x < TIMEPOINTS.length; x++)
+			writer.write("\tpercentChange_" + TIMEPOINTS[x] + "_0");
+		
+		for( int x=2; x < TIMEPOINTS.length; x++)
+			writer.write("\tpercentChange_" + TIMEPOINTS[x] + "_1");
+
+		for( int x=7; x < topSplits.length; x++)
+			writer.write("\t" + topSplits[x]);
+		
+		writer.write("\n");
+		
+		for( String s= reader.readLine(); s != null; s = reader.readLine() )
+		{
+			String[] splits = s.split("\t");
+			
+			for( int x=0; x <6; x++)
+				writer.write("\t" + splits[x]);
+			
+			HashMap<Integer, Double> innerMap = map.get(splits[1]);
+			
+			for( int x=1; x < TIMEPOINTS.length; x++)
+				writer.write("\t" + getPercentChange(innerMap, TIMEPOINTS[x] , 0));
+			
+			for( int x=2; x < TIMEPOINTS.length; x++)
+				writer.write("\t" + getPercentChange(innerMap, TIMEPOINTS[x] , 1));
+			
+			for( int x=7; x < topSplits.length; x++)
+				writer.write("\t" + splits[x]);
+			
+			writer.write("\n");
+
+		}
+		
+		
+		writer.flush();  writer.close();
+		reader.close();
 	}
 	
 	// outerkey is patient ID;  inner key is timepoint;  inner value is weight
