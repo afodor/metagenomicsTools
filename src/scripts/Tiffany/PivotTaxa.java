8cd535cc50efabf8a1380322052fd5bcde9d7b35
Anthony Fodor <anthony.fodor@gmail.com>
2022-02-20 21:09:40 -0500

    pivot

--- src/scripts/Tiffany/PivotTaxa.java
+++ src/scripts/Tiffany/PivotTaxa.java
@@ -14,12 +14,70 @@ public class PivotTaxa
 {
 	private static final File IN_FILE = new File( "C:\\tiffany\\normalized_otu_fil.txt");
 	private static final File OUT_FILE = new File("C:\\tiffany\\pivot_otu_fil.txt");
+	private static final File OUT_FILE_META = new File("C:\\tiffany\\pivot_otu_fil_Meta.txt");
+	private static final File META_FILE = new File("C:\\\\tiffany\\\\SWAB IDs by Race.txt");
 	
 	public static void main(String[] args) throws Exception
 	{
 		HashMap<String, List<Double>> map =getMap();
 		writeFile( map);
 		
+		HashMap<String, Integer> metaMap = getMetaMap();
+		
+		BufferedWriter writer = new BufferedWriter(new FileWriter(OUT_FILE_META));
+		
+		BufferedReader reader = new BufferedReader(new FileReader(OUT_FILE));
+		
+		writer.write("sample\trace");
+		
+		String[] splits = reader.readLine().split("\t");
+		
+		for( int x=1; x < splits.length; x++)
+			writer.write("\t" + splits[x]);
+		
+		writer.write("\n");
+		
+		for(String s= reader.readLine(); s != null; s= reader.readLine())
+		{
+			splits = s.split("\t");
+			
+			writer.write(splits[0] + "\t" + metaMap.get(splits[0]) );
+			
+			for(int x=1; x < splits.length; x++)
+				writer.write("\t" + splits[x]);
+			
+			writer.write("\n");
+		}
+		
+		reader.close();
+		writer.close();
+	}
+	
+	private static HashMap<String, Integer> getMetaMap() throws Exception
+	{
+		HashMap<String, Integer> map = new HashMap<>();
+		
+		@SuppressWarnings("resource")
+		BufferedReader reader = new BufferedReader(new FileReader(META_FILE));
+		
+		reader.readLine();
+		
+		for(String s = reader.readLine(); s != null; s= reader.readLine())
+		{
+			String[] splits =s.split("\t");
+			
+			if( splits.length != 2)
+				throw new Exception("No");
+			
+			if( map.containsKey(splits[0]))
+				throw new Exception("No");
+			
+			map.put(splits[0], Integer.parseInt(splits[1]));
+		}
+		
+		reader.close();
+		
+		return map;
 	}
 	
 	private static void writeFile( HashMap<String, List<Double>> map ) throws Exception
