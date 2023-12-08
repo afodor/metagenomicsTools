fde20bc722d8f038aa66bc3291679aebcacee3cb
Anthony Fodor <anthony.fodor@gmail.com>
2023-12-08 10:30:28 -0500

    just need to write

--- src/scripts/KeRarify/QuickCollectorCurve.java
+++ src/scripts/KeRarify/QuickCollectorCurve.java
@@ -2,7 +2,10 @@ package scripts.KeRarify;
 
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashMap;
+import java.util.HashSet;
+import java.util.LinkedHashMap;
 import java.util.List;
 
 import parsers.OtuWrapper;
@@ -12,10 +15,20 @@ public class QuickCollectorCurve
 	public static void main(String[] args) throws Exception
 	{
 		OtuWrapper wrapper = new OtuWrapper("C:\\keRarify\\amr_transposed.txt");
-
+		
+		HashMap<String, HashMap<Integer, Integer>> collapsedMap = 
+				getCollapsedMap(wrapper);
+		
+		System.out.println(collapsedMap);
+		
+	}
+	
+	private static HashMap<String, HashMap<Integer, Integer>> getCollapsedMap(OtuWrapper wrapper) 
+		throws Exception
+	{
 		//outer key is the sample name
 		// inner map - key is rarefication depth; value is the taxa count at that depth
-		HashMap<String, HashMap<Integer, Integer>> map = new HashMap<>();
+		HashMap<String, HashMap<Integer, Integer>> map = new LinkedHashMap<>();
 		
 		for( int x=0; x < wrapper.getSampleNames().size(); x++)
 		{
@@ -24,16 +37,25 @@ public class QuickCollectorCurve
 			if( map.containsKey(sampleName))
 				throw new Exception("Duplicate key");
 			
-			HashMap<Integer, Integer> innerMap = new HashMap<>();
+			HashMap<Integer, Integer> innerMap = new LinkedHashMap<>();
 			map.put(sampleName, innerMap);
 			
 			List<Integer> countList = getCountsList(wrapper, x);
 			
-			System.out.println(countList);
-			System.exit(1);
-
+			//System.out.println(countList);
+			
+			Collections.shuffle(countList);
+			HashSet<Integer> taxaSeen = new HashSet<>();
+			
+			for( int y=0; y < countList.size(); y++ )
+			{
+				taxaSeen.add(countList.get(y));
+				innerMap.put(y, taxaSeen.size());
+			}
 			
 		}
+		
+		return map;
 	}
 	
 	private static List<Integer> getCountsList(OtuWrapper wrapper, int taxaIndex) throws Exception
