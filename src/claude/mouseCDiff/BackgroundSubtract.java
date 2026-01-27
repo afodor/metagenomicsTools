cdf91c9a98414aabd24f2ba2ee88d29975d21a32
admin2 <admin2@AX8_Max>
2026-01-26 19:46:39 -0500

    in progress

--- src/claude/mouseCDiff/BackgroundSubtract.java
+++ src/claude/mouseCDiff/BackgroundSubtract.java
@@ -20,11 +20,13 @@ public class BackgroundSubtract
 	
 	public static void main(String[] args) throws Exception
 	{
+		HashMap<String, Double> lookupMap = getLookupMap();
+		
 		OtuWrapper initialData = new OtuWrapper( new File( 
 			"C:\\claudeSummary\\simonCrossRho\\kraken_max1\\kraken_max1_counts_table_transposed.tsv") );
 		
 		System.out.println(initialData.getOtuNames());
-		
+				
 		for( int x=0; x < initialData.getSampleNames().size(); x++)
 		{
 			HashMap<String, Double> vals = new HashMap<String, Double>();
@@ -54,6 +56,50 @@ public class BackgroundSubtract
 			
 		}
 		
+	}
+	
+	// key is sourceGenome@targetGenome
+	private static HashMap<String, Double> getLookupMap() throws Exception
+	{
+		HashMap<String, Double> map = new LinkedHashMap<String, Double>();
+		
+		BufferedReader reader = new BufferedReader(new FileReader(new File(
+				"C:\\claudeSummary\\simonCrossRho\\kraken_max1\\relative_abundance_1879x_predicted.tsv"	)));
+		
+		StringTokenizer sToken = new StringTokenizer(reader.readLine(), "\t");
+		List<String> topNames = new ArrayList<String>();
+		
+		sToken.nextToken();
+		
+		while(sToken.hasMoreTokens())
+			topNames.add(sToken.nextToken());
+		
+		for(String s= reader.readLine(); s!= null; s= reader.readLine())
+		{
+			sToken = new StringTokenizer(s, "\t");
+			
+			String firstToken = sToken.nextToken();
+			
+			int x=0;
+			double sum =0;
+			
+			while( sToken.hasMoreTokens())
+			{
+				double val = Double.parseDouble(sToken.nextToken());
+				
+				sum += val;
+				
+				map.put(firstToken + "@" + topNames.get(x), val);
+				x++;
+			}
+			
+			if (sum > 0 )
+				if(Math.abs(1.0-sum) > 0.0000001)
+					throw new Exception("Parsing error " + sum + " "+ firstToken ); ;
+				
+		}
+		
+		return map;
 	}
 	
 }
